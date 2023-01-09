48ed7d0fd76c561af49ddf3ceea7a9295d586e79
Kristian <krisvl5000@gmail.com>
2023-01-09 17:51:52 +0200

    Added Folder Nested Loops Exercise

---
+++ 01. C# Basics/Nested Loops Exercise/Sum Prime Non Prime/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _01._Hello_Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = int.Parse(Console.ReadLine());
+            int second = int.Parse(Console.ReadLine());
+
+            for (int i = first; i <= second; i++)
+            {
+                string currentNum = i.ToString();
+                int oddSum = 0;
+                int evenSum = 0;
+
+                for (int j = 0; j < currentNum.Length; j++)
+                {
+                    int currentDigit = int.Parse(currentNum[j].ToString());
+
+                    if (j % 2 == 0)
+                    {
+                        evenSum += currentDigit;
+                    }
+                    else
+                    {
+                        oddSum += currentDigit;
+                    }
+                }
+                if (oddSum == evenSum)
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
