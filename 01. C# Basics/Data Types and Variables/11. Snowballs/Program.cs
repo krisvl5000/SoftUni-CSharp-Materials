38ca3700cc184f7542950c158ac51785e6006778
Kristian <krisvl5000@gmail.com>
2023-01-09 17:49:37 +0200

    Added Folder Data Types and Variables

---
+++ 01. C# Basics/Data Types and Variables/11. Snowballs/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using System.Numerics;
+
+namespace _01._Hello_Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger highestValue = 0;
+
+
+            int finalSnow = 0;
+            int finalTime = 0;
+            int finalQuality = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int snowballSnow = int.Parse(Console.ReadLine());
+                int snowballTime = int.Parse(Console.ReadLine());
+                int snowballQuality = int.Parse(Console.ReadLine());
+
+                BigInteger snowballValue = BigInteger.Pow
+                    (snowballSnow / snowballTime, snowballQuality);
+
+                if (snowballValue > highestValue)
+                {
+                    highestValue = snowballValue;
+
+                    finalQuality = snowballQuality;
+                    finalTime = snowballTime;
+                    finalSnow = snowballSnow;
+                }
+
+            }
+
+            Console.WriteLine($"{finalSnow} : {finalTime}" +
+                $" = {highestValue} ({finalQuality})");
+
+            
+        }
+    }
+}
