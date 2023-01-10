d3a334e718c27b974940539746121049d36124e9
Kristian <krisvl5000@gmail.com>
2023-01-10 20:19:15 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Lab/2. Stack Sum/Program.cs
+++ 03. C# Advanced/Stacks and Queues Lab/2. Stack Sum/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +8,68 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int[] intArr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            Stack<int> stack = new Stack<int>();
 
+            foreach (var item in intArr)
+            {
+                stack.Push(item);
+            }
+
+            int sum = 0;
+
+            string[] input = Console.ReadLine().Split();
+
+            while (true)
+            {
+                string command = input[0].ToLower();
+
+                if (command == "end")
+                {
+                    break;
+                }
+
+                if (command == "add")
+                {
+                    int firstNum = int.Parse(input[1]);
+                    int secondNum = int.Parse(input[2]);
+
+                    stack.Push(firstNum);
+                    stack.Push(secondNum);
+                }
+                else if (command == "remove")
+                {
+                    int numToPop = int.Parse(input[1]);
+
+                    if (stack.Count >= numToPop)
+                    {
+                        int counter = 0;
+                        while (counter < numToPop)
+                        {
+                            stack.Pop();
+                            counter++;
+                        }
+                    }
+                }
+
+                input = Console.ReadLine().Split();
+            }
+
+            int remainingSum = 0;
+
+            if (stack.Count > 0)
+            {
+                while (stack.Count > 0)
+                {
+                    remainingSum += stack.Pop();
+                }
+
+                Console.WriteLine($"Sum: {remainingSum}");
+            }
+            else
+            {
+                Console.WriteLine("Sum: 0");
+            }
 
         }
     }
