7d1d2fecc3fd5bc30afcfe29eb186fab8e152840
Kristian <krisvl5000@gmail.com>
2023-01-10 21:50:28 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Lab/3. Simple Calculator/Program.cs
+++ 03. C# Advanced/Stacks and Queues Lab/3. Simple Calculator/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +8,42 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            string[] input = Console.ReadLine().Split();
 
+            Stack<string> stack = new Stack<string>();
+
+            for (int i = input.Length - 1; i >= 0; i--)
+            {
+                stack.Push(input[i]);
+            }
+
+            int result = 0;
+
+            bool operatorPlus = true;
+
+            while (stack.Count != 0)
+            {
+                string item = stack.Pop();
+
+                if (item == "+")
+                {
+                    operatorPlus = true;
+                }
+                else if (item == "-")
+                {
+                    operatorPlus = false;
+                }
+                else if (operatorPlus)
+                {
+                    result += int.Parse(item);
+                }
+                else if (!operatorPlus)
+                {
+                    result -= int.Parse(item);
+                }
+            }
+
+            Console.WriteLine(result);
 
         }
     }
