a89da5af8e5f05aacf4fab9c8409ecc989b5b5d8
Kristian <krisvl5000@gmail.com>
2023-01-10 20:58:35 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Lab/4. Matching Brackets/Program.cs
+++ 03. C# Advanced/Stacks and Queues Lab/4. Matching Brackets/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +8,25 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            string expression = Console.ReadLine();
+            Stack<int> stack = new Stack<int>();
 
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if (expression[i] == '(')
+                {
+                    stack.Push(i);
+                }
+
+                if (expression[i] == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int endIndex = i;
+
+                    Console.WriteLine(expression.Substring(startIndex, 
+                        endIndex - startIndex + 1));
+                }
+            }
 
         }
     }
