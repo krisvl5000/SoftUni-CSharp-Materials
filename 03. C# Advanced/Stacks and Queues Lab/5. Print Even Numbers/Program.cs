ff40ed3ed0343af6a2dffd65eb1c2e63685d4b04
Kristian <krisvl5000@gmail.com>
2023-01-11 09:34:57 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Lab/5. Print Even Numbers/Program.cs
+++ 03. C# Advanced/Stacks and Queues Lab/5. Print Even Numbers/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +8,19 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
+            Queue<int> queue = new Queue<int>();
+
+            foreach (var item in input)
+            {
+                if (item % 2 == 0)
+                {
+                    queue.Enqueue(item);
+                }
+            }
+
+            Console.WriteLine(String.Join(", ", queue));
 
         }
     }
