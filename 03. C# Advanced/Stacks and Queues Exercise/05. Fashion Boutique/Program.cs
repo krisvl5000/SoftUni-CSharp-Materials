fecba20b5b0495f7f70dd9cb73a633a038df1a9f
Kristian <krisvl5000@gmail.com>
2023-01-11 14:47:49 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Exercise/05. Fashion Boutique/Program.cs
+++ 03. C# Advanced/Stacks and Queues Exercise/05. Fashion Boutique/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +8,37 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int[] clothes = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
+            int capacity = int.Parse(Console.ReadLine());
+
+            Stack<int> stack = new Stack<int>();
+
+            int rackNumber = 1;
+
+            foreach (var item in clothes)
+            {
+                stack.Push(item);
+            }
+
+            int currentBox = 0;
+
+            foreach (var item in clothes)
+            {
+                if (currentBox + stack.Peek() <= capacity)
+                {
+                    currentBox += stack.Pop();
+                }
+                else
+                {
+                    currentBox = 0;
+                    rackNumber++;
+
+                    currentBox += stack.Pop();
+                }
+            }
+
+            Console.WriteLine(rackNumber);
 
         }
     }
