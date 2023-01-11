02753494d94c6f5254bf65626a68e9c0e1eddfc0
Kristian <krisvl5000@gmail.com>
2023-01-11 17:46:09 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Exercise/07. Truck Tour/Program.cs
+++ 03. C# Advanced/Stacks and Queues Exercise/07. Truck Tour/Program.cs
@@ -1,13 +1,41 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
-namespace _01._Hello_Softuni
+internal class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int numberOfPumps = int.Parse(Console.ReadLine());
+        Queue<string> queue = new Queue<string>();
+        for (int i = 0; i < numberOfPumps; i++)
         {
-
-
+            queue.Enqueue(Console.ReadLine());
         }
+        for (int index = 0; index < numberOfPumps; index++)
+        {
+            if (CanCompleteFullCircle(queue))
+            {
+                Console.WriteLine(index);
+                break;
+            }
+            queue.Enqueue(queue.Dequeue());
+        }
+    }
+
+    private static bool CanCompleteFullCircle(Queue<string> queue)
+    {
+        int fuel = 0;
+        bool canCompleteCirle = true;
+        for (int i = 0; i < queue.Count; i++)
+        {
+            int petrolAmount = int.Parse(queue.Peek().Split().First());
+            int distance = int.Parse(queue.Peek().Split().Last());
+            fuel += petrolAmount - distance;
+            if (fuel < 0)
+                canCompleteCirle = false;
+            queue.Enqueue(queue.Dequeue());
+        }
+        return canCompleteCirle;
     }
 }
