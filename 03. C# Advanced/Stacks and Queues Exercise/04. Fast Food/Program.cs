6431738ec38318dc2452fc9414791988fcf5de74
Kristian <krisvl5000@gmail.com>
2023-01-11 14:35:53 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Exercise/04. Fast Food/Program.cs
+++ 03. C# Advanced/Stacks and Queues Exercise/04. Fast Food/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +8,49 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int quantity = int.Parse(Console.ReadLine());
 
+            int[] orders = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            var queue = new Queue<int>();
+
+            foreach (var item in orders)
+            {
+                queue.Enqueue(item);
+            }
+
+            int biggestOrder = queue.Peek();
+
+            foreach (var item in orders)
+            {
+                if (item > biggestOrder)
+                {
+                    biggestOrder = item;
+                }
+            }
+
+            Console.WriteLine(biggestOrder);
+
+            while (queue.Count > 0)
+            {
+                if (quantity - queue.Peek() >= 0)
+                {
+                    quantity -= queue.Dequeue();
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine("Orders complete");
+            }
+            else
+            {
+                Console.WriteLine($"Orders left: {String.Join(" ", queue)}");
+            }
 
         }
     }
