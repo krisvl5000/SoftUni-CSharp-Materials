644ac4d52de0edaa666229d260501eed1d1fd583
Kristian <krisvl5000@gmail.com>
2023-01-11 20:57:06 +0200

    Update Program.cs

--- 03. C# Advanced/Multidimensional Arrays Lab/2. Sum Matrix Columns/Program.cs
+++ 03. C# Advanced/Multidimensional Arrays Lab/2. Sum Matrix Columns/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +7,34 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int[] dimentions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
 
+            int rows = dimentions[0];
+            int cols = dimentions[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            for (int col = 0; col < cols; col++)
+            {
+                int sum = 0;
+
+                for (int row = 0; row < rows; row++)
+                {
+                    sum += matrix[row, col];
+                }
+
+                Console.WriteLine(sum);
+            }
 
         }
     }
