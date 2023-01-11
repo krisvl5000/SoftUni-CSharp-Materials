e26d493dfcf008426cf5510a90af67d59eb60dba
Kristian <krisvl5000@gmail.com>
2023-01-11 10:48:46 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Lab/8. Traffic Jam/Program.cs
+++ 03. C# Advanced/Stacks and Queues Lab/8. Traffic Jam/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +8,37 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            string input = Console.ReadLine();
+
+            var queue = new Queue<string>();
+            
+            int counter = 0;
+            int totalCarsPassed = 0;
+
+            while (input != "end")
+            {
+                if (input == "green")
+                {
+                    while (queue.Count != 0 && counter < n)
+                    {
+                        Console.WriteLine($"{queue.Dequeue()} passed!");
+                        totalCarsPassed++;
+                        counter++;
+                    }
+
+                    counter = 0;
+
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                queue.Enqueue(input);
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{totalCarsPassed} cars passed the crossroads.");
 
         }
     }
