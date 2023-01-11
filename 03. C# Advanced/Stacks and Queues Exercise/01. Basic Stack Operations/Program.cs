074ce5486b4af34ff0829ce4c882d96d99ac5e43
Kristian <krisvl5000@gmail.com>
2023-01-11 11:29:08 +0200

    Adding Solutions

--- 03. C# Advanced/Stacks and Queues Exercise/01. Basic Stack Operations/Program.cs
+++ 03. C# Advanced/Stacks and Queues Exercise/01. Basic Stack Operations/Program.cs
@@ -1,2 +1,63 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace _01._Hello_Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stackArgs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int itemsToPush = stackArgs[0];
+            int itemsToPop = stackArgs[1];
+            int itemToLookFor = stackArgs[2];
+
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Stack<int> stack = new Stack<int>();
+
+            foreach (var item in arr)
+            {
+                stack.Push(item);
+            }
+
+            for (int i = 0; i < itemsToPop; i++)
+            {
+                if (stack.Count > 0)
+                {
+                    stack.Pop();
+                }
+            }
+
+            if (stack.Count > 0)
+            {
+                if (stack.Contains(itemToLookFor))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    int minNum = stack.Peek();
+
+                    foreach (var item in stack)
+                    {
+                        if (item < minNum)
+                        {
+                            minNum = item;
+                        }
+                    }
+
+                    Console.WriteLine(minNum);
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
+            
+
+        }
+    }
+}
