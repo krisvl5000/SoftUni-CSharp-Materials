57b8ecbfae0475a74cb9c1a9e4f4d96a8ee82b6e
Kristian <krisvl5000@gmail.com>
2023-01-11 15:12:25 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Exercise/06. Songs Queue/Program.cs
+++ 03. C# Advanced/Stacks and Queues Exercise/06. Songs Queue/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +8,50 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            string[] queueArgs = Console.ReadLine().Split(", ");
 
+            Queue<string> queue = new Queue<string>(queueArgs);
+
+            string[] input = Console.ReadLine().Split();
+
+            while (queue.Count > 0)
+            {
+                string command = input[0];
+
+                if (command == "Play")
+                {
+                    queue.Dequeue();
+                }
+                else if (command == "Add")
+                {
+                    string songRequested = string.Join(" ", input);
+                    string song = songRequested.Remove(0, 4);
+                    
+                    if (!queue.Contains(song))
+                    {
+                        queue.Enqueue(song);
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{song} is already contained!");
+                    }
+
+                }
+                else if (command == "Show")
+                {
+                    Console.WriteLine(String.Join(", ", queue));
+                }
+
+
+                if (queue.Count == 0)
+                {
+                    break;
+                }
+
+                input = Console.ReadLine().Split();
+            }
+
+            Console.WriteLine("No more songs!");
 
         }
     }
