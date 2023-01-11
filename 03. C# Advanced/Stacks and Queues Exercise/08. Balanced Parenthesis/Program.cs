54d156515dbb53b5786d1282eb0be64a9d01daf2
Kristian <krisvl5000@gmail.com>
2023-01-11 18:15:48 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Exercise/08. Balanced Parenthesis/Program.cs
+++ 03. C# Advanced/Stacks and Queues Exercise/08. Balanced Parenthesis/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +8,53 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
 
+            Stack<char> parentheses = new Stack<char>();
+
+            bool isBalanced = true;
+
+            Dictionary<char, char> parDefinitions = new Dictionary<char, char>()
+            {
+                { '}', '{' }, { ')', '(' }, {']', '[' }
+            };
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char symbol = input[i];
+
+                if (parDefinitions.Any(x => x.Value == symbol))
+                {
+                    parentheses.Push(symbol);
+                }
+                else if (parDefinitions.Any(x => x.Key == symbol))
+                {
+                    if (parentheses.Count > 0 && parDefinitions[symbol] 
+                        == parentheses.Peek())
+                    {
+                        parentheses.Pop();
+                    }
+                    else
+                    {
+                        isBalanced = false;
+                        break;
+                    }
+                }
+            }
+
+            if (parentheses.Count > 0)
+            {
+                isBalanced = false;
+            }
+
+            if (isBalanced)
+            {
+                Console.WriteLine("YES");
+            }
+            else
+            {
+                Console.WriteLine("NO");
+            }
 
         }
     }
