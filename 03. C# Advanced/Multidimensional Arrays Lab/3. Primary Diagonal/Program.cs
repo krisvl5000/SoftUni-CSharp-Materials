8970e51903937dd5f293f5f974e875b592c35ab1
Kristian <krisvl5000@gmail.com>
2023-01-11 21:31:51 +0200

    Update Program.cs

--- 03. C# Advanced/Multidimensional Arrays Lab/3. Primary Diagonal/Program.cs
+++ 03. C# Advanced/Multidimensional Arrays Lab/3. Primary Diagonal/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +7,35 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            int[,] matrix = new int[n,n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            int sum = 0;
+            int colCounter = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = colCounter; col < n; col++)
+                {
+                    sum += matrix[row, col];
+                    break;
+
+                }
+                colCounter++;
+            }
+
+            Console.WriteLine(sum);
 
         }
     }
