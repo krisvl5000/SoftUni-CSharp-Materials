8515d71d659a4ea5e6a05d8dec3a619c8c6dcf9b
Kristian <krisvl5000@gmail.com>
2023-01-11 15:39:05 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Exercise/09. Simple Text Editor/Program.cs
+++ 03. C# Advanced/Stacks and Queues Exercise/09. Simple Text Editor/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +8,43 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            string text = "";
+
+            Stack<string> stack = new Stack<string>();
+            stack.Push(text);
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine().Split();
+
+                string command = input[0];
+
+                if (command == "1")
+                {
+                    string stringToAppend = input[1];
+                    text += stringToAppend;
+                    stack.Push(text);
+                }
+                else if (command == "2")
+                {
+                    int countToErase = int.Parse(input[1]);
+
+                    text = text.Remove(text.Length - countToErase); // might need changing
+                    stack.Push(text);
+                }
+                else if (command == "3")
+                {
+                    int index = int.Parse(input[1]);
+                    Console.WriteLine(text[index - 1]);
+                }
+                else if (command == "4")
+                {
+                    stack.Pop();
+                    text = stack.Peek();
+                }
+            }
 
         }
     }
