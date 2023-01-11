bb93a5b650e82b6fd5ee3ff1825eb2fdc24307da
Kristian <krisvl5000@gmail.com>
2023-01-11 11:33:43 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Exercise/02. Basic Queue Operations/Program.cs
+++ 03. C# Advanced/Stacks and Queues Exercise/02. Basic Queue Operations/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,6 +8,54 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int[] stackArgs = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            int itemsToEnqueue = stackArgs[0];
+            int itemsToDequeue = stackArgs[1];
+            int itemToLookFor = stackArgs[2];
+
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            foreach (var item in arr)
+            {
+                queue.Enqueue(item);
+            }
+
+            for (int i = 0; i < itemsToDequeue; i++)
+            {
+                if (queue.Count > 0)
+                {
+                    queue.Dequeue();
+                }
+            }
+
+            if (queue.Count > 0)
+            {
+                if (queue.Contains(itemToLookFor))
+                {
+                    Console.WriteLine("true");
+                }
+                else
+                {
+                    int minNum = queue.Peek();
+
+                    foreach (var item in queue)
+                    {
+                        if (item < minNum)
+                        {
+                            minNum = item;
+                        }
+                    }
+
+                    Console.WriteLine(minNum);
+                }
+            }
+            else
+            {
+                Console.WriteLine(0);
+            }
 
 
         }
