ea588ba85b87216463c925da68008566b5f66ea3
Kristian <krisvl5000@gmail.com>
2023-01-11 10:19:32 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Lab/7. Hot Potato/Program.cs
+++ 03. C# Advanced/Stacks and Queues Lab/7. Hot Potato/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,8 +8,41 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            string[] people = Console.ReadLine().Split();
 
+            Queue<string> queue = new Queue<string>();
 
+            int n = int.Parse(Console.ReadLine());
+
+            foreach (var item in people)
+            {
+                queue.Enqueue(item);
+            }
+
+            int counter = 1;
+            while (queue.Count != 1)
+            {
+                if (n == 1)
+                {
+                    while (queue.Count != 1)
+                    {
+                        Console.WriteLine($"Removed {queue.Dequeue()}"); 
+                    }
+                }
+
+                queue.Enqueue(queue.First());
+                queue.Dequeue();
+
+                counter++;
+
+                if (counter == n)
+                {
+                    Console.WriteLine($"Removed {queue.Dequeue()}");
+                    counter = 1;
+                }
+            }
+
+            Console.WriteLine($"Last is {queue.Peek()}");
         }
     }
 }
