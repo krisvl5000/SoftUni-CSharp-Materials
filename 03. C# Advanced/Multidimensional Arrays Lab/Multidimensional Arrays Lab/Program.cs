fa794e2f5c76d8c2aac3e7a9c227ab310392a411
Kristian <krisvl5000@gmail.com>
2023-01-11 20:47:00 +0200

    Adding Layout

--- 03. C# Advanced/Multidimensional Arrays Lab/Multidimensional Arrays Lab/Program.cs
+++ 03. C# Advanced/Multidimensional Arrays Lab/Multidimensional Arrays Lab/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+namespace _01._Hello_Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] dimentions = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int rows = dimentions[0];
+            int cols = dimentions[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            int sum = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                int[] arr = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = arr[col];
+
+                    sum += matrix[row, col];
+                }
+            }
+
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+
+        }
+    }
+}
