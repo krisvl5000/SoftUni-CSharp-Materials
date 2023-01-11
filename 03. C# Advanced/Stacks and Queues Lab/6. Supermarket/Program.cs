9f8dba904986e88fd24f9cd13b44a65800168224
Kristian <krisvl5000@gmail.com>
2023-01-11 09:43:31 +0200

    Update Program.cs

--- 03. C# Advanced/Stacks and Queues Lab/6. Supermarket/Program.cs
+++ 03. C# Advanced/Stacks and Queues Lab/6. Supermarket/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,8 +8,28 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
 
+            Queue<string> queue = new Queue<string>();
 
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    while (queue.Count != 0)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
         }
     }
 }
