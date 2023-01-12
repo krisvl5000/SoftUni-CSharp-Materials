388965b6cf59396a361bb53bcf44afc1d6c243e0
Kristian <krisvl5000@gmail.com>
2023-01-12 10:19:50 +0200

    Update Program.cs

--- 03. C# Advanced/Multidimensional Arrays Lab/4. Symbol in Matrix/Program.cs
+++ 03. C# Advanced/Multidimensional Arrays Lab/4. Symbol in Matrix/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +7,41 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] arr = Console.ReadLine().ToCharArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            char thingToLookFor = char.Parse(Console.ReadLine());
+
+            bool isFound = false;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] == thingToLookFor)
+                    {
+                        Console.WriteLine($"({row}, {col})");
+                        isFound = true;
+                        return;
+                    }
+                }
+            }
+
+            if (!isFound)
+            {
+                Console.WriteLine($"{thingToLookFor} does not occur in the matrix");
+            }
 
         }
     }
