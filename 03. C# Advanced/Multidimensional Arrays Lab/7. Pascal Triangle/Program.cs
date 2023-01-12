82de8e8cf59d1df37ba767079898a96c26a2a08f
Kristian <krisvl5000@gmail.com>
2023-01-12 11:24:38 +0200

    Update Program.cs

--- 03. C# Advanced/Multidimensional Arrays Lab/7. Pascal Triangle/Program.cs
+++ 03. C# Advanced/Multidimensional Arrays Lab/7. Pascal Triangle/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01._Hello_Softuni
 {
@@ -6,8 +7,32 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int size = int.Parse(Console.ReadLine());
+            long[][] matrix = new long[size][];
+            matrix[0] = new long[1] { 1 };
 
+            for (int row = 1; row < size; row++)
+            {
+                matrix[row] = new long[row + 1];
+                matrix[row][0] = 1;
 
+                for (int col = 1; col < row; col++)
+                {
+                    matrix[row][col] = matrix[row - 1][col - 1] + matrix[row - 1][col];
+                }
+
+                matrix[row][row] = 1;
+            }
+
+            for (int row = 0; row < matrix.Length; row++)
+            {
+                for (int col = 0; col < matrix[row].Length; col++)
+                {
+                    Console.Write(matrix[row][col] + " ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
