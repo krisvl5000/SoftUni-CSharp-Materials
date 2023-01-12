7e3ded354bb879df19cb9b5c5a6d899d6551399a
Kristian <krisvl5000@gmail.com>
2023-01-12 14:54:46 +0200

    Update Program.cs

--- 03. C# Advanced/Multidimensional Arrays Exercise/2. Squares in Matrix/Program.cs
+++ 03. C# Advanced/Multidimensional Arrays Exercise/2. Squares in Matrix/Program.cs
@@ -1,50 +1,4 @@
-﻿//using System;
-//using System.Linq;
-
-//namespace _01._Hello_Softuni
-//{
-//    internal class Program
-//    {
-//        static void Main(string[] args)
-//        {
-//            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
-
-//            int rows = input[0];
-//            int cols = input[1];
-//            int equalsCount = 0;
-
-//            char[,] matrix = new char[rows, cols];
-
-//            for (int row = 0; row < rows; row++)
-//            {
-//                char[] userLine = Console.ReadLine().Split().Select(char.Parse).ToArray();
-
-//                for (int col = 0; col < cols; col++)
-//                {
-//                    matrix[row, col] = userLine[col];
-//                }
-//            }
-
-//            for (int row = 0; row < rows - 1; row++)
-//            {
-//                for (int col = 0; col < cols - 1; col++)
-//                {
-//                    if (matrix[row, col] == matrix[row, col + 1] &&
-//                        matrix[row, col] == matrix[row + 1, col] &&
-//                        matrix[row, col] == matrix[row + 1, col + 1])
-//                    {
-//                        equalsCount++;
-//                    }
-//                }
-//            }
-
-//            Console.WriteLine(equalsCount);
-
-//        }
-//    }
-//}
-
-using System;
+﻿using System;
 using System.Linq;
 
 namespace _01._Hello_Softuni
@@ -66,10 +20,26 @@ namespace _01._Hello_Softuni
 
                 for (int col = 0; col < cols; col++)
                 {
-                    
+                    matrix[row, col] = arr[col];
                 }
             }
 
+            int squareCounter = 0;
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row + 1, col] &&
+                        matrix[row, col] == matrix[row, col + 1] &&
+                        matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        squareCounter++;
+                    }
+                }
+            }
+
+            Console.WriteLine(squareCounter);
         }
     }
 }
