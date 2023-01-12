bab312285331ea61e02458995a8a64ed30a52958
Kristian <krisvl5000@gmail.com>
2023-01-12 12:25:30 +0200

    Update Program.cs

--- 03. C# Advanced/Multidimensional Arrays Exercise/Multidimensional Arrays Exercise/Program.cs
+++ 03. C# Advanced/Multidimensional Arrays Exercise/Multidimensional Arrays Exercise/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +7,31 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            int[,] matrix = new int[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = arr[col];
+                }
+            }
+
+            int leftToRightSum = 0;
+            int rightToLeftSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                leftToRightSum += matrix[i, i];
+                rightToLeftSum += matrix[i, n - i - 1];
+            }
+
+            int totalSum = Math.Abs(leftToRightSum - rightToLeftSum);
+            Console.WriteLine(totalSum);
 
         }
     }
