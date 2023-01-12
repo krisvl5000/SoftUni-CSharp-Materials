81757de1137373a3eee7e8c0a333422786ea2c71
Kristian <krisvl5000@gmail.com>
2023-01-12 14:50:17 +0200

    Fixed issues

---
+++ 03. C# Advanced/Multidimensional Arrays Exercise/11. Additional Exercise/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _01._Hello_Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            for (int i = n; i >= 0; i--)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    if (j >= i)
+                    {
+                        Console.Write('#');
+                    }
+                    else
+                    {
+                        Console.Write(' ');
+                    }
+                }
+
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
--- 03. C# Advanced/Multidimensional Arrays Exercise/2. Squares in Matrix/Program.cs
+++ 03. C# Advanced/Multidimensional Arrays Exercise/2. Squares in Matrix/Program.cs
@@ -1,4 +1,51 @@
-﻿using System;
+﻿//using System;
+//using System.Linq;
+
+//namespace _01._Hello_Softuni
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+
+//            int rows = input[0];
+//            int cols = input[1];
+//            int equalsCount = 0;
+
+//            char[,] matrix = new char[rows, cols];
+
+//            for (int row = 0; row < rows; row++)
+//            {
+//                char[] userLine = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+//                for (int col = 0; col < cols; col++)
+//                {
+//                    matrix[row, col] = userLine[col];
+//                }
+//            }
+
+//            for (int row = 0; row < rows - 1; row++)
+//            {
+//                for (int col = 0; col < cols - 1; col++)
+//                {
+//                    if (matrix[row, col] == matrix[row, col + 1] &&
+//                        matrix[row, col] == matrix[row + 1, col] &&
+//                        matrix[row, col] == matrix[row + 1, col + 1])
+//                    {
+//                        equalsCount++;
+//                    }
+//                }
+//            }
+
+//            Console.WriteLine(equalsCount);
+
+//        }
+//    }
+//}
+
+using System;
+using System.Linq;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +53,22 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
+            int rows = input[0];
+            int cols = input[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] arr = Console.ReadLine().Split().Select(char.Parse).ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    
+                }
+            }
 
         }
     }
