ae3b9c2c047f235644ba76bb415adeda0cfe6a5a
Kristian <krisvl5000@gmail.com>
2023-01-13 12:59:26 +0200

    Update Program.cs

--- 03. C# Advanced/Multidimensional Arrays Exercise/6. Jagged Array Manipulator/Program.cs
+++ 03. C# Advanced/Multidimensional Arrays Exercise/6. Jagged Array Manipulator/Program.cs
@@ -1,13 +1,85 @@
 ﻿using System;
+using System.Linq;
 
-namespace _01._Hello_Softuni
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            
+        int n = int.Parse(Console.ReadLine());
 
+        int[][] matrix = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            matrix[i] = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        }
+
+        for (int i = 0; i < matrix.Length - 1; i++)
+        {
+            if (matrix[i].Length == matrix[i + 1].Length)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] *= 2;
+                    matrix[i + 1][j] *= 2;
+                }
+            }
+            else
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    matrix[i][j] /= 2;
+                }
+                for (int j = 0; j < matrix[i + 1].Length; j++)
+                {
+                    matrix[i + 1][j] /= 2;
+                }
+            }
+        }
+
+        while (true)
+        {
+            string[] command = Console.ReadLine().Split();
+            if (command[0] == "End")
+            {
+                break;
+            }
+
+            int row = int.Parse(command[1]);
+            int col = int.Parse(command[2]);
+            int value = int.Parse(command[3]);
+
+            if (command[0] == "Add")
+            {
+                if (AreIndexesValid(row, col, matrix))
+                {
+                    matrix[row][col] += value;
+                }
+            }
+            else if (command[0] == "Subtract")
+            {
+                if (AreIndexesValid(row, col, matrix))
+                {
+                    matrix[row][col] -= value;
+                }
+            }
+        }
+
+        foreach (int[] row in matrix)
+        {
+            Console.WriteLine(string.Join(" ", row));
+        }
+    }
+
+    static bool AreIndexesValid(int row, int col, int[][] matrix)
+    {
+        if (row >= 0 && row < matrix.GetLength(0) &&
+            col >= 0 && col < matrix[row].Length)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
         }
     }
 }
