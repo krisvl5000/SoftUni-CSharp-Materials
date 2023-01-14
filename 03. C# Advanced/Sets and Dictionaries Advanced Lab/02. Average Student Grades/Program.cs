1d4aa3010e3597bd86d533f1637502cd062fc652
Kristian <krisvl5000@gmail.com>
2023-01-14 11:01:26 +0200

    Update Program.cs

--- 03. C# Advanced/Sets and Dictionaries Advanced Lab/02. Average Student Grades/Program.cs
+++ 03. C# Advanced/Sets and Dictionaries Advanced Lab/02. Average Student Grades/Program.cs
@@ -6,7 +6,40 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            var dict = new Dictionary<string, List<decimal>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = input[0];
+                decimal grade = decimal.Parse(input[1]);
+
+                if (!dict.ContainsKey(name))
+                {
+                    dict[name] = new List<decimal>();
+                    dict[name].Add(grade);
+                }
+                else
+                {
+                    dict[name].Add(grade);
+                }
+            }
+
+            foreach (var student in dict)
+            {
+                Console.Write($"{student.Key} -> ");
+
+                foreach (var grade in student.Value)
+                {
+                    Console.Write($"{grade:F2} ");
+                }
+
+                Console.WriteLine($"(avg: {student.Value.Average():F2})");
+            }
 
         }
     }
