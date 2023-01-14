7e2e8472daa0f34b82d3ec1b1196687095aa0b7e
Kristian <krisvl5000@gmail.com>
2023-01-14 15:54:57 +0200

    Update Program.cs

--- 03. C# Advanced/Sets and Dictionaries Advanced Exercise/03. Periodic Table/Program.cs
+++ 03. C# Advanced/Sets and Dictionaries Advanced Exercise/03. Periodic Table/Program.cs
@@ -6,7 +6,24 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var item in input)
+                {
+                    set.Add(item);
+                }
+            }
+
+            set = set.OrderBy(x => x).ToHashSet();
+
+            Console.WriteLine(String.Join(" ", set));
 
         }
     }
