f8c43071427f206870316a96cd59dc5406c038b1
Kristian <krisvl5000@gmail.com>
2023-01-14 14:34:48 +0200

    Update Program.cs

--- 03. C# Advanced/Sets and Dictionaries Advanced Lab/06. Record Unique Names/Program.cs
+++ 03. C# Advanced/Sets and Dictionaries Advanced Lab/06. Record Unique Names/Program.cs
@@ -6,7 +6,21 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            HashSet<string> set = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string name = Console.ReadLine();
+
+                set.Add(name);
+            }
+
+            foreach (var item in set)
+            {
+                Console.WriteLine(item);
+            }
 
         }
     }
