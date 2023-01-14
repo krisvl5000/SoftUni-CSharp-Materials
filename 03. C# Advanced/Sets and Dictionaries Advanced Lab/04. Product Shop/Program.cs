242929358fadecf89b98d48c17a30d01adbdc63a
Kristian <krisvl5000@gmail.com>
2023-01-14 12:06:56 +0200

    Update Program.cs

--- 03. C# Advanced/Sets and Dictionaries Advanced Lab/04. Product Shop/Program.cs
+++ 03. C# Advanced/Sets and Dictionaries Advanced Lab/04. Product Shop/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +7,39 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            var dict = new Dictionary<string, Dictionary<string, double>>();
 
+            string[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "Revision")
+            {
+                string shop = input[0];
+                string product = input[1];
+                double price = double.Parse(input[2]);
+
+                if (!dict.ContainsKey(shop))
+                {
+                    dict.Add(shop, new Dictionary<string, double>());
+                }
+
+                dict[shop].Add(product, price);
+
+                input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            dict = dict.OrderBy(x => x.Key).ToDictionary(x => x.Key, x => x.Value);
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine(item.Key + "->");
+
+                foreach (var kvp in item.Value)
+                {
+                    Console.WriteLine($"Product: {kvp.Key}, Price: {kvp.Value}");
+                }
+            }
 
         }
     }
