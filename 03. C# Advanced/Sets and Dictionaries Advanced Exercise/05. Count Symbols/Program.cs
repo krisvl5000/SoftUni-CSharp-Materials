911269a2c23eff1c9b34dbcbd191627c936a4bb6
Kristian <krisvl5000@gmail.com>
2023-01-14 17:24:15 +0200

    Update Program.cs

--- 03. C# Advanced/Sets and Dictionaries Advanced Exercise/05. Count Symbols/Program.cs
+++ 03. C# Advanced/Sets and Dictionaries Advanced Exercise/05. Count Symbols/Program.cs
@@ -6,7 +6,24 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            string input = Console.ReadLine();
 
+            var dict = new SortedDictionary<char, int>();
+
+            foreach (var ch in input)
+            {
+                if (!dict.ContainsKey(ch))
+                {
+                    dict.Add(ch, 0);
+                }
+                
+                dict[ch]++;
+            }
+
+            foreach (var item in dict)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value} time/s");
+            }
 
         }
     }
