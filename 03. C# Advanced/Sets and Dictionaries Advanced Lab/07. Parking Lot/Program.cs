1f2ee575ddc878d669e9d4a6bddcd9fb0f2dc26f
Kristian <krisvl5000@gmail.com>
2023-01-14 14:43:01 +0200

    Update Program.cs

--- 03. C# Advanced/Sets and Dictionaries Advanced Lab/07. Parking Lot/Program.cs
+++ 03. C# Advanced/Sets and Dictionaries Advanced Lab/07. Parking Lot/Program.cs
@@ -6,6 +6,41 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            HashSet<string> set = new HashSet<string>();
+
+            string[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "END")
+            {
+                string direction = input[0];
+                string plate = input[1];
+
+                if (direction == "IN")
+                {
+                    set.Add(plate);
+                }
+                else if (direction == "OUT")
+                {
+                    set.Remove(plate);
+                }
+
+                input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (set.Count > 0)
+            {
+                foreach (var item in set)
+                {
+                    Console.WriteLine(item);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+
 
 
         }
