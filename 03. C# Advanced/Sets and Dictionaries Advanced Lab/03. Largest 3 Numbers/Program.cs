86c9e30f1bbf63e047ba6ef984000400bbc80000
Kristian <krisvl5000@gmail.com>
2023-01-14 11:16:36 +0200

    Update Program.cs

--- 03. C# Advanced/Sets and Dictionaries Advanced Lab/03. Largest 3 Numbers/Program.cs
+++ 03. C# Advanced/Sets and Dictionaries Advanced Lab/03. Largest 3 Numbers/Program.cs
@@ -6,7 +6,24 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
+            arr = arr.OrderByDescending(x => x).ToArray();
+
+            if (arr.Length > 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(arr[i] + " ");
+                }
+            }
+            else
+            {
+                foreach (var item in arr)
+                {
+                    Console.Write(item + " ");
+                }
+            }
 
         }
     }
