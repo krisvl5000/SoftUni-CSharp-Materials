38b19cbc60520f503f67ad36c9205d9f5b9ec0e8
Kristian <krisvl5000@gmail.com>
2023-01-14 14:30:45 +0200

    Update Program.cs

--- 03. C# Advanced/Sets and Dictionaries Advanced Lab/05. Cities by Continent and Country/Program.cs
+++ 03. C# Advanced/Sets and Dictionaries Advanced Lab/05. Cities by Continent and Country/Program.cs
@@ -6,7 +6,42 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            var dict = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string continent = input[0];
+                string country = input[1];
+                string city = input[2];
+
+                if (!dict.ContainsKey(continent))
+                {
+                    dict.Add(continent, new Dictionary<string, List<string>>());
+                }
+
+                if (!dict[continent].ContainsKey(country))
+                {
+                    dict[continent].Add(country, new List<string>());
+                }
+
+                dict[continent][country].Add(city);
+            }
+
+            foreach (var continent in dict)
+            {
+                Console.WriteLine($"{continent.Key}:");
+
+                foreach (var country in continent.Value)
+                {
+                    Console.WriteLine($"  {country.Key} -> " +
+                        $"{String.Join(", ", country.Value)}");
+                }
+            }
 
         }
     }
