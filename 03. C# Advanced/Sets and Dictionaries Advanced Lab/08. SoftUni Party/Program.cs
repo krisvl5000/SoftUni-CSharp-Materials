2bfa5b7bb0e11ad79df552381a5759fcc04b3699
Kristian <krisvl5000@gmail.com>
2023-01-14 14:56:15 +0200

    Update Program.cs

--- 03. C# Advanced/Sets and Dictionaries Advanced Lab/08. SoftUni Party/Program.cs
+++ 03. C# Advanced/Sets and Dictionaries Advanced Lab/08. SoftUni Party/Program.cs
@@ -6,7 +6,67 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            HashSet<string> vipSet = new HashSet<string>();
+            HashSet<string> regSet = new HashSet<string>();
 
+            string input = Console.ReadLine();
+
+            while (true)
+            {
+                if (input == "PARTY")
+                {
+                    string peopleWhoCame = Console.ReadLine();
+
+                    while (true)
+                    {
+                        if (peopleWhoCame == "END")
+                        {
+                            input = "END";
+                            break;
+                        }
+
+                        if (vipSet.Contains(peopleWhoCame))
+                        {
+                            vipSet.Remove(peopleWhoCame);
+                        }
+                        else if (regSet.Contains(peopleWhoCame))
+                        {
+                            regSet.Remove(peopleWhoCame);
+                        }
+
+                        peopleWhoCame = Console.ReadLine();
+                    }
+                }
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                if (char.IsDigit(input.First()))
+                {
+                    vipSet.Add(input);
+                }
+                else
+                {
+                    regSet.Add(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(vipSet.Count + regSet.Count);
+
+            foreach (var item in vipSet)
+            {
+                Console.WriteLine(item);
+            }
+
+            foreach (var item in regSet)
+            {
+                Console.WriteLine(item);
+            }
+            
 
         }
     }
