4842a0f7e461cd9ea51c67a460b8d488fd98bf9a
Kristian <krisvl5000@gmail.com>
2023-01-14 15:50:59 +0200

    Update Program.cs

--- 03. C# Advanced/Sets and Dictionaries Advanced Exercise/02. Sets of Elements/Program.cs
+++ 03. C# Advanced/Sets and Dictionaries Advanced Exercise/02. Sets of Elements/Program.cs
@@ -6,7 +6,35 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int[] sizes = Console.ReadLine().Split().Select(int.Parse).ToArray();
 
+            int firstSet = sizes[0];
+            int secondSet = sizes[1];
+
+            HashSet<int> set1 = new HashSet<int>();
+            HashSet<int> set2 = new HashSet<int>();
+
+            for (int i = 0; i < firstSet; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                set1.Add(input);
+            }
+
+            for (int i = 0; i < secondSet; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                set2.Add(input);
+            }
+
+            foreach (var item in set1)
+            {
+                if (set2.Contains(item))
+                {
+                    Console.Write(item + " ");
+                }
+            }
 
         }
     }
