eb64b8f4cc797a3da0dac06c2c83cae535f34d2f
Kristian <krisvl5000@gmail.com>
2023-01-14 17:19:37 +0200

    Update Program.cs

--- 03. C# Advanced/Sets and Dictionaries Advanced Exercise/04. Even Times/Program.cs
+++ 03. C# Advanced/Sets and Dictionaries Advanced Exercise/04. Even Times/Program.cs
@@ -6,7 +6,24 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            var dict = new Dictionary<int, int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                if (!dict.ContainsKey(input))
+                {
+                    dict.Add(input, 0);
+                }
+
+                dict[input]++;
+            }
+
+            Console.WriteLine(dict.Where(x => x.Value % 2 == 0)
+                .Select(y => y.Key).First());
 
         }
     }
