bbdbd738f15b8c241b354b725f49a667dc646e86
Kristian <krisvl5000@gmail.com>
2023-01-16 15:15:39 +0200

    Update Program.cs

--- 03. C# Advanced/Defining Classes Lab/03. Car Constructors/Program.cs
+++ 03. C# Advanced/Defining Classes Lab/03. Car Constructors/Program.cs
@@ -6,8 +6,21 @@ namespace CarManufacturer
     {
         static void Main(string[] args)
         {
+            Car defaultCar = new Car("BMW", "X3", 2006, 100000, 5000);
+            Console.WriteLine(defaultCar.WhoAmI());
 
+            Car car = new Car();
+            car.Model = "A5";
+            car.Make = "Audi";
+            car.Year = 1995;
+            car.FuelQuantity = 100;
+            car.FuelConsumption = 2;
 
+            for (int i = 0; i < 5; i++)
+            {
+                car.Drive(30);
+                Console.WriteLine(car.WhoAmI());
+            }
         }
     }
 }
