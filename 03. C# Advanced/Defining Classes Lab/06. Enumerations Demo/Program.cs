3f4d4832cfe6e3bbf8076ab375ccdfce302245fc
Kristian <krisvl5000@gmail.com>
2023-01-16 19:40:06 +0200

    Adding New Solution Enumerations Demo

---
+++ 03. C# Advanced/Defining Classes Lab/06. Enumerations Demo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01._Hello_Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
