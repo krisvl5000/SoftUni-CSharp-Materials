e5c63354c53bfdec10d8bd987e5c47c16c500cdc
Kristian <krisvl5000@gmail.com>
2023-01-16 10:15:01 +0200

    Added New Changes To The Car Class

--- 03. C# Advanced/Defining Classes Lab/01. Car/Car.cs
+++ 03. C# Advanced/Defining Classes Lab/01. Car/Car.cs
@@ -11,9 +11,30 @@ namespace CarManufacturer
         string make;
         string model;
         int year;
+        double fuelQuantity;
+        double fuelConsumption;
 
         public string Make { get { return this.make; } set { this.make = value; } }
         public string Model { get { return this.model; } set { this.model = value; } }
         public int Year { get { return this.year; } set { this.year = value; } }
+        public double FuelQuantity { get { return this.fuelQuantity; } set { this.fuelQuantity = value; } }
+        public double FuelConsumption { get { return this.fuelConsumption; } set { this.fuelConsumption = value; } }
+
+        public void Drive(double distance)
+        {
+            if (fuelQuantity - distance * fuelConsumption > 0)
+            {
+                fuelQuantity -= distance * fuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform the trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}";
+        }
     }
 }
--- 03. C# Advanced/Defining Classes Lab/01. Car/Program.cs
+++ 03. C# Advanced/Defining Classes Lab/01. Car/Program.cs
@@ -8,8 +8,16 @@ namespace CarManufacturer
         {
             Car car = new Car();
             car.Model = "A5";
+            car.Make = "Audi";
+            car.Year = 1995;
+            car.FuelQuantity = 100;
+            car.FuelConsumption = 2;
 
-            Console.WriteLine(car.Model);
+            for (int i = 0; i < 5; i++)
+            {
+                car.Drive(30);
+                Console.WriteLine(car.WhoAmI());
+            }
         }
     }
 }
