39db9edb62202975c01f899bbea69c3871002038
Kristian <krisvl5000@gmail.com>
2023-01-16 10:19:21 +0200

    Adding Second Solution

--- 03. C# Advanced/Defining Classes Lab/02. Car Extension/Program.cs
+++ 03. C# Advanced/Defining Classes Lab/02. Car Extension/Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 
-namespace _01._Hello_Softuni
+namespace CarManufacturer
 {
-    internal class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
+            Car car = new Car();
+            car.Model = "A5";
+            car.Make = "Audi";
+            car.Year = 1995;
+            car.FuelQuantity = 100;
+            car.FuelConsumption = 2;
 
-
+            for (int i = 0; i < 5; i++)
+            {
+                car.Drive(30);
+                Console.WriteLine(car.WhoAmI());
+            }
         }
     }
 }
