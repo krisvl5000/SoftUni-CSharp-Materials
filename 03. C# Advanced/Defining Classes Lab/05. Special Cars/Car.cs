3b7ca411ddcf910468fa6e8f0b843b6424fd4fcf
Kristian <krisvl5000@gmail.com>
2023-01-16 16:34:15 +0200

    Added The Classes

---
+++ 03. C# Advanced/Defining Classes Lab/05. Special Cars/Car.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Car
+    {
+        Engine engine;
+        Tire[] tire;
+        string make;
+        string model;
+        int year;
+        double fuelQuantity;
+        double fuelConsumption;
+
+        public Car()
+        {
+            Make = "VW";
+            Model = "Golf";
+            Year = 2025;
+            FuelQuantity = 200;
+            FuelConsumption = 10;
+        }
+
+        public Car(string make, string model, int year) : this()
+        {
+            Make = make;
+            Model = model;
+            Year = year;
+        }
+
+        public Car(string make, string model, int year,
+            double fuelQuantity, double fuelConsumption) : this(make, model, year)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption;
+        }
+
+        public Car(string make, string model, int year,
+           double fuelQuantity, double fuelConsumption, Engine engine, Tire[] tire) : 
+            this(make, model, year, fuelConsumption, fuelQuantity)
+        {
+            Engine = engine;
+            Tire = tire;
+        }
+
+        public string Make { get { return this.make; } set { this.make = value; } }
+        public string Model { get { return this.model; } set { this.model = value; } }
+        public int Year { get { return this.year; } set { this.year = value; } }
+        public double FuelQuantity { get { return this.fuelQuantity; } set { this.fuelQuantity = value; } }
+        public double FuelConsumption { get { return this.fuelConsumption; } set { this.fuelConsumption = value; } }
+        public Engine Engine { get; set; }
+        public Tire[] Tire { get; set; }
+
+        public void Drive(double distance)
+        {
+            if (fuelQuantity - distance * fuelConsumption > 0)
+            {
+                fuelQuantity -= distance * fuelConsumption;
+            }
+            else
+            {
+                Console.WriteLine("Not enough fuel to perform the trip!");
+            }
+        }
+
+        public string WhoAmI()
+        {
+            return $"Make: {this.Make}\nModel: {this.Model}\nYear: {this.Year}\nFuel: {this.FuelQuantity:F2}";
+        }
+    }
+}
---
+++ 03. C# Advanced/Defining Classes Lab/05. Special Cars/Tire.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    public class Tire
+    {
+        private int year;
+        private double pressure;
+
+        public Tire(int year, double pressure)
+        {
+            Year = year;
+            Pressure = pressure;
+        }
+
+        public int Year { get; set; }
+
+        public double Pressure { get; set; }
+    }
+}
