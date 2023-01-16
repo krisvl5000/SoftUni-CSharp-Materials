5328da52468c53b68b744eeb69f87eca0c00d7fe
Kristian <krisvl5000@gmail.com>
2023-01-16 14:59:46 +0200

    Update Engine.cs

--- 03. C# Advanced/Defining Classes Lab/04. Car Engine And Tires/Engine.cs
+++ 03. C# Advanced/Defining Classes Lab/04. Car Engine And Tires/Engine.cs
@@ -8,5 +8,19 @@ namespace _04._Car_Engine_And_Tires
 {
     internal class Engine
     {
+        private int horsePower;
+        private double cubicCapacity;
+
+        public Engine(int horsePower, double cubicCapacity)
+        {
+            HorsePower = horsePower;
+            CubicCapacity = cubicCapacity;
+        }
+
+        public int HorsePower { get; set; }
+
+        public double CubicCapacity { get; set; }
+
+
     }
 }
