fe104781b229e44c3ac9b2cccc1ab16a2581b6a0
Kristian <krisvl5000@gmail.com>
2023-01-16 15:21:32 +0200

    Update Program.cs

--- 03. C# Advanced/Defining Classes Lab/04. Car Engine And Tires/Program.cs
+++ 03. C# Advanced/Defining Classes Lab/04. Car Engine And Tires/Program.cs
@@ -6,29 +6,16 @@ namespace CarManufacturer
     {
         static void Main(string[] args)
         {
-            Tire[] tires = new Tire[]
+            Tire[] tires = new Tire[4]
             {
-                new Tire(1995, 1.6),
-                new Tire(1995, 1.6),
-                new Tire(1995, 1.9),
-                new Tire(1995, 1.9)
+                new Tire(1, 2.5),
+                new Tire(1, 2.1),
+                new Tire(2, 0.5),
+                new Tire(2, 2.3)
             };
 
-            Car defaultCar = new Car("BMW", "X3", 2006, 100000, 5000);
-            Console.WriteLine(defaultCar.WhoAmI());
-
-            Car car = new Car();
-            car.Model = "A5";
-            car.Make = "Audi";
-            car.Year = 1995;
-            car.FuelQuantity = 100;
-            car.FuelConsumption = 2;
-
-            for (int i = 0; i < 5; i++)
-            {
-                car.Drive(30);
-                Console.WriteLine(car.WhoAmI());
-            }
+            Engine engine = new Engine(560, 6300);
+            Car car = new Car("Lamborghini", "Urus", 2010, 250, 9, engine, tires);
         }
     }
 }
