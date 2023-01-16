14b189ef7563b7f9d45a5bde636b7d2603244a8f
Kristian <krisvl5000@gmail.com>
2023-01-16 14:57:35 +0200

    Create Engine.cs

---
+++ 03. C# Advanced/Defining Classes Lab/04. Car Engine And Tires/Engine.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04._Car_Engine_And_Tires
+{
+    internal class Engine
+    {
+    }
+}
