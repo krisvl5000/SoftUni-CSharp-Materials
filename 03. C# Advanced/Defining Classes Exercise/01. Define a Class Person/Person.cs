3987697581b263559b056b3bcbff4f8b921f4ca6
Kristian <krisvl5000@gmail.com>
2023-01-17 10:22:00 +0200

    Create Person.cs

---
+++ 03. C# Advanced/Defining Classes Exercise/01. Define a Class Person/Person.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._Define_a_Class_Person
+{
+    internal class Person
+    {
+    }
+}
