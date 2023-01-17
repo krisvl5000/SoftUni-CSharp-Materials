0d9477006634667e614bbe9286c0c69e940a7315
Kristian <krisvl5000@gmail.com>
2023-01-17 20:37:53 +0200

    Update Program.cs

--- 03. C# Advanced/Exam Preparation/Exam Preparation/Program.cs
+++ 03. C# Advanced/Exam Preparation/Exam Preparation/Program.cs
@@ -1,13 +1,52 @@
 ﻿using System;
+using System.Linq;
 
-namespace _01._Hello_Softuni
+class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        int maxCaffeine = 300;
+        int currentCaffeine = 0;
+        int caffeineReduction = 30;
+
+        int[] caffeine = Console.ReadLine()
+            .Split(", ")
+            .Select(int.Parse)
+            .ToArray();
+
+        int[] drinks = Console.ReadLine()
+            .Split(", ")
+            .Select(int.Parse)
+            .ToArray();
+
+        while (caffeine.Length > 0 && drinks.Length > 0)
         {
-
-
+            int caffeineInDrink = caffeine[caffeine.Length - 1] * drinks[0];
+            if (currentCaffeine + caffeineInDrink <= maxCaffeine)
+            {
+                currentCaffeine += caffeineInDrink;
+                caffeine = caffeine.Take(caffeine.Length - 1).ToArray();
+                drinks = drinks.Skip(1).ToArray();
+            }
+            else
+            {
+                currentCaffeine -= caffeineReduction;
+                if (currentCaffeine < 0)
+                {
+                    currentCaffeine = 0;
+                }
+                drinks = drinks.Skip(1).Concat(drinks.Take(1)).ToArray();
+            }
         }
+
+        if (drinks.Length > 0)
+        {
+            Console.WriteLine("Drinks left: " + string.Join(", ", drinks));
+        }
+        else
+        {
+            Console.WriteLine("At least Stamat wasn't exceeding the maximum caffeine.");
+        }
+        Console.WriteLine("Stamat is going to sleep with " + currentCaffeine + " mg caffeine.");
     }
 }
