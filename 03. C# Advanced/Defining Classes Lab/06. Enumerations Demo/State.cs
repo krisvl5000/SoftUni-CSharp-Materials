53d1f08258fe1d5fb13e90a0f4c950c946193225
Kristian <krisvl5000@gmail.com>
2023-01-17 09:13:49 +0200

    Create State.cs

---
+++ 03. C# Advanced/Defining Classes Lab/06. Enumerations Demo/State.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06._Enumerations_Demo
+{
+    internal class State
+    {
+    }
+}
