0bdfd267309aeb9903bcffe6ae310c1ac69ad52d
Kristian <krisvl5000@gmail.com>
2023-01-18 11:09:45 +0200

    Added Templates

--- 03. C# Advanced/Generics Lab/Generic List Explanation/ObjectList.cs
+++ 03. C# Advanced/Generics Lab/Generic List Explanation/ObjectList.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Generic_List_Explanation
+namespace Test
 {
-    internal class ObjectList
+    public class ObjectList
     {
+
     }
 }
--- 03. C# Advanced/Generics Lab/Generic List Explanation/Program.cs
+++ 03. C# Advanced/Generics Lab/Generic List Explanation/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Test
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
