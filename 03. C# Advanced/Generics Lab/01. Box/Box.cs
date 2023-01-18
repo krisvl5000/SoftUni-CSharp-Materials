64e4c0969320b8edb25842f648c929af9cee42e0
Kristian <krisvl5000@gmail.com>
2023-01-18 12:23:00 +0200

    Wrote the Box logic and changed namespace in main class

---
+++ 03. C# Advanced/Generics Lab/01. Box/Box.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    public class Box<T>
+    {
+        private List<T> data;
+
+        public Box()
+        {
+            this.data = data;
+        }
+
+        public void Add(T item)
+        {
+            this.data.Add(item);
+        }
+
+        public T Remove()
+        {
+            if (this.data.Count == 0)
+            {
+                throw new InvalidOperationException("The box is empty.");
+            }
+            var lastElement = this.data[this.data.Count - 1];
+            this.data.RemoveAt(this.data.Count - 1);
+            return lastElement;
+        }
+    }
+}
