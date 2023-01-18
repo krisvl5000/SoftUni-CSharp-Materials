6d5b550ebd55b3730c74f4e60cd2fb4dbb8522f8
Kristian <krisvl5000@gmail.com>
2023-01-18 11:36:08 +0200

    Update Program.cs

--- 03. C# Advanced/Generics Lab/Generic List Explanation/Program.cs
+++ 03. C# Advanced/Generics Lab/Generic List Explanation/Program.cs
@@ -6,7 +6,14 @@ namespace Test
     {
         static void Main(string[] args)
         {
+            var objectList = new ObjectList<int>(16);
 
+            objectList.Add(1000);
+            objectList.Add(2000);
+
+            var first = (int)objectList.Get(1);
+
+            Console.WriteLine(first);
 
         }
     }
