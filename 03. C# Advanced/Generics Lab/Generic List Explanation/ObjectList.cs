562974986b634af59f70181c1f3ef3240d89c178
Kristian <krisvl5000@gmail.com>
2023-01-18 11:13:56 +0200

    Update ObjectList.cs

--- 03. C# Advanced/Generics Lab/Generic List Explanation/ObjectList.cs
+++ 03. C# Advanced/Generics Lab/Generic List Explanation/ObjectList.cs
@@ -6,8 +6,25 @@ using System.Threading.Tasks;
 
 namespace Test
 {
-    public class ObjectList
+    public class ObjectList<T>
     {
+        private int currentIndex;
+        private T[] data;
 
+        public ObjectList(int capacity)
+        {
+            this.data = new T[capacity];
+        }
+
+        public void Add(T value)
+        {
+            this.data[this.currentIndex] = value;
+            this.currentIndex++;
+        }
+
+        public T Get(int index)
+        {
+            return this.data[index];
+        }
     }
 }
