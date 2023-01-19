cc0d4f7b8312f71811a61345f4e6da7b628ba9cc
Kristian <krisvl5000@gmail.com>
2023-01-19 19:37:14 +0200

    Update Program.cs

--- 03. C# Advanced/Defining Classes Exercise/04. Opinion Poll/Program.cs
+++ 03. C# Advanced/Defining Classes Exercise/04. Opinion Poll/Program.cs
@@ -7,12 +7,35 @@ using DefiningClasses;
 
 namespace DefiningClasses
 {
-    public class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            Family family = new Family();
 
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            List<Person> filteredPeople = family.List
+                .Where(x => x.Age > 30)
+                .OrderBy(x => x.Name)
+                .ToList();
+
+            foreach (var item in filteredPeople)
+            {
+                Console.WriteLine($"{item.Name} - {item.Age}");
+            }
         }
     }
 }
