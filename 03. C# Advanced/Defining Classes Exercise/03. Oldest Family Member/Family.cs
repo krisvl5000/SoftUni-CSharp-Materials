1f28939cc49d91728b0d655e98d4202dc841cf0b
Kristian <krisvl5000@gmail.com>
2023-01-19 19:09:40 +0200

    Update Family.cs

--- 03. C# Advanced/Defining Classes Exercise/03. Oldest Family Member/Family.cs
+++ 03. C# Advanced/Defining Classes Exercise/03. Oldest Family Member/Family.cs
@@ -4,9 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03._Oldest_Family_Member
+namespace DefiningClasses
 {
-    internal class Family
+    public class Family
     {
+        private List<Person> list;
+
+        public List<Person> List { get; set; } = new List<Person>();
+
+        public void AddMember(Person member)
+        {
+            List.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return list.OrderBy(x => x.Age).First();
+        }
     }
 }
