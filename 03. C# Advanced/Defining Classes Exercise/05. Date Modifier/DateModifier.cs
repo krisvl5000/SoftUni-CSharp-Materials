5e6135420d9cca0f0bdf2f2859d75a7c4c6fed69
Kristian <krisvl5000@gmail.com>
2023-01-19 19:40:58 +0200

    Update DateModifier.cs

--- 03. C# Advanced/Defining Classes Exercise/05. Date Modifier/DateModifier.cs
+++ 03. C# Advanced/Defining Classes Exercise/05. Date Modifier/DateModifier.cs
@@ -4,9 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _05._Date_Modifier
+namespace DateModifier
 {
-    internal class DateModifier
+    public class DateModifier
     {
+
     }
 }
