9b9f233d892ce4efb94147048fcd2064868f126c
Kristian <krisvl5000@gmail.com>
2023-01-19 19:37:12 +0200

    Update Person.cs

--- 03. C# Advanced/Defining Classes Exercise/04. Opinion Poll/Person.cs
+++ 03. C# Advanced/Defining Classes Exercise/04. Opinion Poll/Person.cs
@@ -30,5 +30,10 @@ namespace DefiningClasses
         public string Name { get; set; }
 
         public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} - {this.Age}";
+        }
     }
 }
