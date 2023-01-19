71761fb1067377c4127c3e0afab668ced94f301d
Kristian <krisvl5000@gmail.com>
2023-01-19 12:06:58 +0200

    Update Box.cs

--- 03. C# Advanced/Generics Exerciese/Generics Exerciese/Box.cs
+++ 03. C# Advanced/Generics Exerciese/Generics Exerciese/Box.cs
@@ -4,20 +4,20 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Generics_Exerciese
+namespace Box
 {
     public class Box<T>
     {
-        public T Value { get; set; }
-
         public Box(T value)
         {
             Value = value;
         }
 
+        public T Value { get; }
+
         public override string ToString()
         {
-            return Value.ToString();
+            return $"{typeof(T)}: {Value}";
         }
     }
 }
