812b05eb01eac68bd8b0c56a752b637ea58bcb2c
Kristian <krisvl5000@gmail.com>
2023-01-19 19:20:41 +0200

    Create Family.cs

---
+++ 03. C# Advanced/Defining Classes Exercise/04. Opinion Poll/Family.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        private List<Person> list;
+
+        public List<Person> List { get; set; } = new List<Person>();
+
+        public void AddMember(Person member)
+        {
+            List.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return List.OrderByDescending(x => x.Age).First();
+        }
+    }
+}
