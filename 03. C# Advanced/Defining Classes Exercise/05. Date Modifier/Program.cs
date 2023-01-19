b5aa58595a4ea7f4bf381cccdb728885d21b2b0f
Kristian <krisvl5000@gmail.com>
2023-01-19 19:54:14 +0200

    Update Program.cs

--- 03. C# Advanced/Defining Classes Exercise/05. Date Modifier/Program.cs
+++ 03. C# Advanced/Defining Classes Exercise/05. Date Modifier/Program.cs
@@ -1,13 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using DateModifier;
 
-namespace DateModifier
+namespace _01._Hello_Softuni
 {
-    public class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
