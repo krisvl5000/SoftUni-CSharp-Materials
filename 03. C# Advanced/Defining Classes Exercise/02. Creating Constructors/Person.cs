c3f0f051f4b015b258b22e157a97158ae156046d
Kristian <krisvl5000@gmail.com>
2023-01-19 18:59:11 +0200

    Create Person.cs

---
+++ 03. C# Advanced/Defining Classes Exercise/02. Creating Constructors/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
