afd4109625937cd537f1bf2ddf7900819916e8bf
Kristian <krisvl5000@gmail.com>
2023-01-19 19:02:10 +0200

    Update Person.cs

--- 03. C# Advanced/Defining Classes Exercise/02. Creating Constructors/Person.cs
+++ 03. C# Advanced/Defining Classes Exercise/02. Creating Constructors/Person.cs
@@ -11,10 +11,20 @@ namespace DefiningClasses
         private string name;
         private int age;
 
-        public Person(string name, int age)
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age) : this(age)
         {
             Name = name;
-            Age = age;
         }
 
         public string Name { get; set; }
