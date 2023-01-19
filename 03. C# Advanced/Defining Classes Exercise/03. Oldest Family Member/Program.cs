668ba2fcccdd9e8850b5b72d5318b04bda90fa70
Kristian <krisvl5000@gmail.com>
2023-01-19 19:14:30 +0200

    Update Program.cs

--- 03. C# Advanced/Defining Classes Exercise/03. Oldest Family Member/Program.cs
+++ 03. C# Advanced/Defining Classes Exercise/03. Oldest Family Member/Program.cs
@@ -11,7 +11,25 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
 
+            Family family = new Family();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string name = input[0];
+                int age = int.Parse(input[1]);
+
+                Person person = new Person(name, age);
+                family.AddMember(person);
+            }
+
+            Person oldestMember = family.GetOldestMember();
+
+            Console.WriteLine($"{oldestMember.Name} {oldestMember.Age}");
 
         }
     }
