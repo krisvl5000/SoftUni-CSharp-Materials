35f5cde6b6741b1cd1782dfe3c98f334cc8c8af6
Kristian <krisvl5000@gmail.com>
2023-01-19 19:04:55 +0200

    Upgrading the Class

---
+++ 03. C# Advanced/Defining Classes Exercise/03. Oldest Family Member/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age) : this(age)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+    }
+}
