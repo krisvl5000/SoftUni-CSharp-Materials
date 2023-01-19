fc767ae1d946a698dd18d4808bcd9545a5d18f73
Kristian <krisvl5000@gmail.com>
2023-01-19 18:03:59 +0200

    Update Program.cs

--- 03. C# Advanced/Defining Classes Exercise/01. Define a Class Person/Program.cs
+++ 03. C# Advanced/Defining Classes Exercise/01. Define a Class Person/Program.cs
@@ -11,15 +11,15 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-            Person peter = new Person();
+            Person peter = new Person("Peter", 20);
             peter.Name = "Peter";
             peter.Age = 20;
 
-            Person george = new Person();
+            Person george = new Person("George", 18);
             george.Name = "George";
             george.Age = 18;
 
-            Person jose = new Person();
+            Person jose = new Person("Jose", 43);
             jose.Name = "Jose";
             jose.Age = 43;
         }
