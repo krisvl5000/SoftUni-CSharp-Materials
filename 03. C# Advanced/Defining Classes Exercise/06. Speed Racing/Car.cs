c26c3b8c942238d21fcd9d21ff5c9830f62385a2
Kristian <krisvl5000@gmail.com>
2023-01-20 09:00:24 +0200

    Update Car.cs

--- 03. C# Advanced/Defining Classes Exercise/06. Speed Racing/Car.cs
+++ 03. C# Advanced/Defining Classes Exercise/06. Speed Racing/Car.cs
@@ -8,6 +8,17 @@ namespace DefiningClasses
 {
     public class Car
     {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+        public string Model { get; set; }
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        public double TravelledDistance { get; set; }
 
     }
 }
