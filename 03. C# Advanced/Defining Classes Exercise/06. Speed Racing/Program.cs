f8be804af7d79d0a0e1f598744a08339a08fbe3d
Kristian <krisvl5000@gmail.com>
2023-01-20 09:23:03 +0200

    Update Program.cs

--- 03. C# Advanced/Defining Classes Exercise/06. Speed Racing/Program.cs
+++ 03. C# Advanced/Defining Classes Exercise/06. Speed Racing/Program.cs
@@ -11,7 +11,54 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
+            List<Car> list = new List<Car>();
 
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] carArgs = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string model = carArgs[0];
+                double fuelAmount = double.Parse(carArgs[1]);
+                double fuelConsumptionPerKm = double.Parse(carArgs[2]);
+
+                Car car = new Car(model, fuelAmount, fuelConsumptionPerKm);
+                list.Add(car);
+            }
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            while (input[0] != "End")
+            {
+                //string command = input[0];
+                string carModel = input[1];
+                int distanceToDrive = int.Parse(input[2]);
+
+                Car carToDrive = list.Find(x => x.Model == carModel);
+
+                if (carToDrive.DoesItHaveEnoughFuel(carToDrive, distanceToDrive))
+                {
+                    carToDrive.FuelAmount -= 
+                        carToDrive.FuelConsumptionPerKilometer * distanceToDrive;
+
+                    carToDrive.TravelledDistance += distanceToDrive;
+                }
+                else
+                {
+                    Console.WriteLine("Insuffiecient fuel for the drive");
+                }
+
+                input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            foreach (var car in list)
+            {
+                Console.WriteLine(car);
+            }
 
         }
     }
