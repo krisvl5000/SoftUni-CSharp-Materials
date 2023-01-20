eeca5648a784f45a4924e145a85f5f567ceb119e
Kristian <krisvl5000@gmail.com>
2023-01-20 15:08:52 +0200

    Create Car.cs

---
+++ 03. C# Advanced/Defining Classes Exercise/08. Car Salesman/Car.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+
+namespace DefiningClasses
+{
+    public class Car
+    {
+        private string model;
+        private Engine engine;
+        private int weight;
+        private string color;
+
+        public string Model { get { return model; } set { model = value; } }
+        public Engine Engine { get { return engine; } set { engine = value; } }
+        public int Weight { get { return weight; } set { weight = value; } }
+        public string Color { get { return color; } set { color = value; } }
+
+        public Car(string model, Engine engine)
+        {
+            this.Model = model;
+            this.Engine = engine;
+        }
+
+        public Car(string model, Engine engine, int weight)
+            : this(model, engine)
+        {
+            this.Weight = weight;
+        }
+
+        public Car(string model, Engine engine, string color)
+            : this(model, engine)
+        {
+            this.Color = color;
+        }
+
+        public Car(string model, Engine engine, int weight, string color)
+            : this(model, engine)
+        {
+            this.Weight = weight;
+            this.Color = color;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            string weightText = this.Weight == 0 ? "n/a" : this.Weight.ToString();
+            string colorText = this.Color == null ? "n/a" : this.Color.ToString();
+
+            sb.AppendLine($"{this.Model}:");
+            sb.Append(this.Engine.ToString());
+            sb.AppendLine($"  Weight: {weightText}");
+            sb.Append($"  Color: {colorText}");
+            return sb.ToString();
+        }
+    }
+}
