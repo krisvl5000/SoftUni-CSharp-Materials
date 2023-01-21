4a9a596cefbb9eaf24d3ce66f42c77d4b4496a41
Kristian <krisvl5000@gmail.com>
2023-01-21 18:37:31 +0200

    Uploading Solution

--- 03. C# Advanced/Generics Exerciese/07. Tuple/Program.cs
+++ 03. C# Advanced/Generics Exerciese/07. Tuple/Program.cs
@@ -1,11 +1,42 @@
 ﻿using System;
 
-namespace _01._Hello_Softuni
+namespace Box
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string fullName = input[0] + " " + input[1];
+            string address = input[2];
+
+            Tuple<string, string> firstTuple = 
+                new Tuple<string, string>(fullName, address);
+
+            string[] secondInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string name = secondInput[0];
+            int litersOfBeer = int.Parse(secondInput[1]);
+
+            Tuple<string, int> secondTuple = new Tuple<string, int>(name, litersOfBeer);
+
+            string[] thirdInput = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int first = int.Parse(thirdInput[0]);
+            double second = double.Parse(thirdInput[1]);
+
+            Tuple<int, double> thirdTuple = new Tuple<int, double>(first, second);
+
+            Console.WriteLine(firstTuple);
+            Console.WriteLine(secondTuple);
+            Console.WriteLine(thirdTuple);
+
 
 
         }
