ce9fa94d654f743a59c1b40019fa8cd10283e011
Kristian <krisvl5000@gmail.com>
2023-01-21 15:04:19 +0200

    Update EqualityScale.cs

--- 03. C# Advanced/Generics Lab/03. Generic Scale/EqualityScale.cs
+++ 03. C# Advanced/Generics Lab/03. Generic Scale/EqualityScale.cs
@@ -8,6 +8,19 @@ namespace GenericScale
 {
     public class EqualityScale <T>
     {
+        public EqualityScale(T left, T right)
+        {
+            Left = left;
+            Right = right;
+        }
 
+        public T Left { get; set; }
+
+        public T Right { get; set; }
+
+        public bool AreEqual()
+        {
+            return Left.Equals(Right);
+        }
     }
 }
