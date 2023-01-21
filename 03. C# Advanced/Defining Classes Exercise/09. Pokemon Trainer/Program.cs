20efea719988e7fad30a031a6da8fac92aab318a
Kristian <krisvl5000@gmail.com>
2023-01-21 11:00:28 +0200

    Uploading Pokemon Trainer

--- 03. C# Advanced/Defining Classes Exercise/09. Pokemon Trainer/Program.cs
+++ 03. C# Advanced/Defining Classes Exercise/09. Pokemon Trainer/Program.cs
@@ -3,15 +3,112 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DefiningClasses;
 
-namespace _01._Hello_Softuni
+namespace DefiningClasses
 {
-    internal class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
+            List<Trainer> trainersList = new List<Trainer>();
 
+            string[] cmdArgs = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
+            while (cmdArgs[0] != "Tournament")
+            {
+                string trainerName = cmdArgs[0];
+                string pokemonName = cmdArgs[1];
+                string pokemonElement = cmdArgs[2];
+                int pokemonHealth = int.Parse(cmdArgs[3]);
+
+                if (!trainersList.Any(x => x.Name == trainerName))
+                {
+                    Trainer trainer = new Trainer(trainerName);
+                    trainersList.Add(trainer);
+
+                    Pokemon pokemon = new Pokemon
+                    (pokemonName, pokemonElement, pokemonHealth);
+
+                    trainer.List.Add(pokemon);
+                }
+                else
+                {
+                    Trainer trainer = trainersList
+                        .FirstOrDefault(x => x.Name == trainerName);
+
+                    Pokemon pokemon = new Pokemon
+                    (pokemonName, pokemonElement, pokemonHealth);
+
+                    trainer.List.Add(pokemon);
+                }
+
+                cmdArgs = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                if (command == "End")
+                {
+                    break;
+                }
+
+                if (command == "Fire")
+                {
+                    HasSuchPokemon(command, trainersList);
+                }
+                else if (command == "Water")
+                {
+                    HasSuchPokemon(command, trainersList);
+                }
+                else if (command == "Electricity")
+                {
+                    HasSuchPokemon(command, trainersList);
+                }
+            }
+
+            trainersList = trainersList
+                .OrderByDescending(x => x.NumberOfBadges)
+                .ToList();
+
+            foreach (var trainer in trainersList)
+            {
+                Console.WriteLine(trainer);
+            }
+        }
+
+        static void HasSuchPokemon(string command, List<Trainer> trainersList)
+        {
+            foreach (var trainer in trainersList)
+            {
+                if (trainer.List.Any(x => x.Element == command))
+                {
+                    trainer.NumberOfBadges++;
+                }
+                else
+                {
+                    List<Pokemon> pokemonToRemove = new List<Pokemon>();
+
+                    foreach (var pokemon in trainer.List)
+                    {
+                        pokemon.Health -= 10;
+
+                        if (pokemon.Health <= 0)
+                        {
+                            pokemonToRemove.Add(pokemon);
+                        }
+                    }
+
+                    foreach (var pokemon in pokemonToRemove)
+                    {
+                        trainer.List.Remove(pokemon);
+                    }
+                }
+            }
         }
     }
 }
---
+++ 03. C# Advanced/Defining Classes Exercise/09. Pokemon Trainer/Trainer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Trainer
+    {
+        private string name;
+        private int numberOfBadges;
+        private List<Pokemon> list;
+
+        public Trainer(string name)
+        {
+            Name = name;
+            NumberOfBadges = 0;
+        }
+        public string Name { get; set; }
+
+        public int NumberOfBadges { get; set; }
+
+        public List<Pokemon> List { get; set; } = new List<Pokemon>();
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.NumberOfBadges} {this.List.Count}";
+        }
+    }
+}
