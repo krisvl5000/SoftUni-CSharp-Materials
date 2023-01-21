8bc1c46e075b1c3a7fd8da7d344f8266b32e0563
Kristian <krisvl5000@gmail.com>
2023-01-21 14:53:40 +0200

    Update ArrayCreator.cs

--- 03. C# Advanced/Generics Lab/02. Array Creator/ArrayCreator.cs
+++ 03. C# Advanced/Generics Lab/02. Array Creator/ArrayCreator.cs
@@ -4,20 +4,19 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Test
-{ 
-    public class ArrayCreator
+namespace GenericArrayCreator;
+
+public class ArrayCreator
+{
+    public static T[] Create<T>(int length, T item)
     {
-        public static T[] Create<T>(int length, T item)
+        var result = new T[length];
+
+        for (int i = 0; i < length; i++)
         {
-            var result = new T[length];
-
-            for (int i = 0; i < length; i++)
-            {
-                result[i] = item;
-            }
-
-            return result;
+            result[i] = item;
         }
+
+        return result;
     }
 }
