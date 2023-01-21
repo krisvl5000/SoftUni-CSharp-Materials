5084aea10edd90fd6c2a03a091963128bb0d70b2
Kristian <krisvl5000@gmail.com>
2023-01-21 11:18:09 +0200

    Update Program.cs

--- 03. C# Advanced/Defining Classes Exercise/10. SofutUni Parking/Program.cs
+++ 03. C# Advanced/Defining Classes Exercise/10. SofutUni Parking/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace SoftUniParking
+{
+    internal class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
