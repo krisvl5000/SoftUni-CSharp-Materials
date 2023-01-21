c46b2812cf9d946732aefe00cbae111de6935cfa
Kristian <krisvl5000@gmail.com>
2023-01-21 15:17:39 +0200

    Update Program.cs

--- 03. C# Advanced/Generics Exerciese/01. Generic Box of String (New)/Program.cs
+++ 03. C# Advanced/Generics Exerciese/01. Generic Box of String (New)/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Box
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Box<string> box = new Box<string>();
+
+                Console.WriteLine(box);
+            }
+
+        }
+    }
+}
