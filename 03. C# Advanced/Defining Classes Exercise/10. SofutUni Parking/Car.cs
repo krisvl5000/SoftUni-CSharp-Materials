acc1bbef1e657d96eafd3323453455745e2d7f69
Kristian <krisvl5000@gmail.com>
2023-01-21 11:18:03 +0200

    Update Car.cs

--- 03. C# Advanced/Defining Classes Exercise/10. SofutUni Parking/Car.cs
+++ 03. C# Advanced/Defining Classes Exercise/10. SofutUni Parking/Car.cs
@@ -4,9 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _10._SofutUni_Parking
+namespace SoftUniParking
 {
-    internal class Car
+    public class Car
     {
+        private string make;
+        private string model;
+        private int horsePower;
+        private string registrationNumber;
+
+        public Car(string make, string model, int horsePower, string registrationNumber)
+        {
+            Make = make;
+            Model = model;
+            HorsePower = horsePower;
+            RegistrationNumber = registrationNumber;
+        }
+
+        public string Make { get; set; }
+
+        public string Model { get; set; }
+
+        public int HorsePower { get; set; }
+
+        public string RegistrationNumber { get; set; }
+
+        public override string ToString()
+        {
+            return $"Make: {this.Make}" +
+                $"\nModel: {this.Model}" +
+                $"\nHorsePower: {this.HorsePower}" +
+                $"\nRegistrationNumber: {this.RegistrationNumber}";
+        }
     }
 }
