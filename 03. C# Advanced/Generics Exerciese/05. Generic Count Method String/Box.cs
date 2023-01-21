17f29b9b012007c690f00bdc9be16ddd53d7674c
Kristian <krisvl5000@gmail.com>
2023-01-21 17:30:47 +0200

    Uploading Solution

---
+++ 03. C# Advanced/Generics Exerciese/05. Generic Count Method String/Box.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public static int HowManyAreGreater<T>(List<Box<T>> list, T element)
+        {
+            Comparer<T> comparer = Comparer<T>.Default;
+
+            int result = list
+                .Where(x => comparer.Compare(x.Value, element) > 0)
+                .ToList().Count;
+
+            return result;
+        }
+    }
+}
--- 03. C# Advanced/Generics Exerciese/05. Generic Count Method String/Program.cs
+++ 03. C# Advanced/Generics Exerciese/05. Generic Count Method String/Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 
-namespace _01._Hello_Softuni
+namespace Box
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            List<Box<string>> list = new List<Box<string>>();
 
+            int n = int.Parse(Console.ReadLine());
 
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                Box<string> box = new Box<string>(input);
+                list.Add(box);
+            }
+
+            string element = Console.ReadLine();
+
+            Console.WriteLine(Box<string>.HowManyAreGreater(list, element));
         }
     }
 }
