024ca52e8da7b5164479e3e1cd55f6a3a52f1c57
Kristian <krisvl5000@gmail.com>
2023-01-21 15:31:01 +0200

    Create Box.cs

---
+++ 03. C# Advanced/Generics Exerciese/02. Generic Box of Integer/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Value.GetType()}: {Value}";
+        }
+    }
+}
