afc7211a36a1befea16e986729d330710d4a27e6
Kristian <krisvl5000@gmail.com>
2023-01-21 17:36:10 +0200

    Uploading Solution

--- 03. C# Advanced/Generics Exerciese/06. Generic Count Method Double/Program.cs
+++ 03. C# Advanced/Generics Exerciese/06. Generic Count Method Double/Program.cs
@@ -1,13 +1,26 @@
 ﻿using System;
 
-namespace _01._Hello_Softuni
+namespace Box
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            List<Box<double>> list = new List<Box<double>>();
 
+            int n = int.Parse(Console.ReadLine());
 
+            for (int i = 0; i < n; i++)
+            {
+                double input = double.Parse(Console.ReadLine());
+
+                Box<double> box = new Box<double>(input);
+                list.Add(box);
+            }
+
+            double element = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Box<double>.HowManyAreGreater(list, element));
         }
     }
 }
