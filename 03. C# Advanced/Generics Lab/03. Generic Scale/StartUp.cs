2da379b9964d1ca305720ff1d4eedc01188a0a3c
Kristian <krisvl5000@gmail.com>
2023-01-21 15:07:51 +0200

    Create StartUp.cs

---
+++ 03. C# Advanced/Generics Lab/03. Generic Scale/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GenericScale;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<string> scale = new EqualityScale<string>("A", "A");
+
+            Console.WriteLine(scale.AreEqual());
+
+        }
+    }
+}
