12983ed58f9ee6ba8681ca0ebcb5619d25c83b90
Kristian <krisvl5000@gmail.com>
2023-01-21 16:41:41 +0200

    Uploading Swap Method

--- 03. C# Advanced/Generics Exerciese/03. Generic Swap Method String/Program.cs
+++ 03. C# Advanced/Generics Exerciese/03. Generic Swap Method String/Program.cs
@@ -1,13 +1,42 @@
 ﻿using System;
 
-namespace _01._Hello_Softuni
+namespace List
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+            List<string> list = new List<string>();
 
+            int n = int.Parse(Console.ReadLine());
 
+            for (int i = 0; i < n; i++)
+            {
+                string input = Console.ReadLine();
+
+                list.Add(input);
+            }
+
+            string[] swapArgs = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int firstIndex = int.Parse(swapArgs[0]);
+            int secondIndex = int.Parse(swapArgs[1]);
+
+            List<string> swappedList = Swap(list, firstIndex, secondIndex);
+
+            foreach (var item in swappedList)
+            {
+                Console.WriteLine($"{item.GetType()}: {item}");
+            }
+        }
+
+        public static List<T> Swap<T>(List<T> list, int firstIndex, int secondIndex)
+        {
+            T temp = list[firstIndex];
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = temp;
+            return list;
         }
     }
 }
