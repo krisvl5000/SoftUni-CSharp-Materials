16317ef9a51bfedbf27445fa5cfd29834bce29a2
Kristian <krisvl5000@gmail.com>
2023-01-21 14:56:10 +0200

    Create StartUp.cs

---
+++ 03. C# Advanced/Generics Lab/02. Array Creator/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using GenericArrayCreator;
+
+namespace GenericArrayCreator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var numbers = ArrayCreator.Create(100, 6);
+
+            var texts = ArrayCreator.Create(100, "Ivailo");
+
+        }
+    }
+}
