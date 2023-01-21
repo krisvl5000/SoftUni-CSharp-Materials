e2d8af6efb95557d6b9155dc8dcbb0c44867c73a
Kristian <krisvl5000@gmail.com>
2023-01-21 15:26:04 +0200

    Update Box.cs

--- 03. C# Advanced/Generics Exerciese/01. Generic Box of String (New)/Box.cs
+++ 03. C# Advanced/Generics Exerciese/01. Generic Box of String (New)/Box.cs
@@ -8,11 +8,16 @@ namespace Box
 {
     public class Box<T>
     {
+        public Box(T value)
+        {
+            Value = value;
+        }
+
         public T Value { get; set; }
 
         public override string ToString()
         {
-            return typeof(T).Name;
+            return $"{Value.GetType()}: {Value}";
         }
     }
 }
