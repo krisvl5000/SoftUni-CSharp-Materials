10ba69e050a58786d7762e66eda54746da811b89
Kristian <krisvl5000@gmail.com>
2023-01-21 13:23:30 +0200

    Update Box.cs

--- 03. C# Advanced/Generics Lab/01. Box/Box.cs
+++ 03. C# Advanced/Generics Lab/01. Box/Box.cs
@@ -8,29 +8,18 @@ namespace BoxOfT
 {
     public class Box<T>
     {
-        private List<T> data;
+        private Stack<T> elements = new Stack<T>();
 
-        public Box()
+        public int Count => elements.Count;
+
+        public void Add(T element)
         {
-            this.data = data;
-        }
-
-        public int Count => data.Count;
-
-        public void Add(T item)
-        {
-            this.data.Add(item);
+            elements.Push(element);
         }
 
         public T Remove()
         {
-            if (this.data.Count == 0)
-            {
-                throw new InvalidOperationException("The box is empty.");
-            }
-            var lastElement = this.data[this.data.Count - 1];
-            this.data.RemoveAt(this.data.Count - 1);
-            return lastElement;
+            return elements.Pop();
         }
     }
 }
