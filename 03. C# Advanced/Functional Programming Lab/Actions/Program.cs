ef9d406582d2a4bac558b3fb6c70bdae92616f06
Kristian <krisvl5000@gmail.com>
2023-01-22 12:56:22 +0200

    Create Program.cs

---
+++ 03. C# Advanced/Functional Programming Lab/Actions/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _01._Hello_Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // You can use Action for void methods
+
+            Action<int, string, object, decimal> strangeFunction = StrangeFunc;
+
+        }
+        static void StrangeFunc(int x, string y, object z, decimal xx)
+        {
+            Console.WriteLine("Strange Function called");
+        }
+    }
+}
