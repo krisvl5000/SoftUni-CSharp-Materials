49638f1308e307e8552f98d6f547447cdbd8b91f
Kristian <krisvl5000@gmail.com>
2023-01-22 11:45:49 +0200

    Update Program.cs

--- 03. C# Advanced/Functional Programming Lab/01. Sort Even Numbers/Program.cs
+++ 03. C# Advanced/Functional Programming Lab/01. Sort Even Numbers/Program.cs
@@ -6,7 +6,14 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int[] arr = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
 
+            Console.WriteLine(String.Join(", ", arr
+                .Where(x => x % 2 == 0)
+                .OrderBy(x => x)));
 
         }
     }
