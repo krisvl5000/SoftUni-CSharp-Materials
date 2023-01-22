57ec4faf25450c5a8c2e27d3eaf089b9b571fb90
Kristian <krisvl5000@gmail.com>
2023-01-22 12:01:18 +0200

    Update Program.cs

--- 03. C# Advanced/Functional Programming Lab/Functional Programming Lab/Program.cs
+++ 03. C# Advanced/Functional Programming Lab/Functional Programming Lab/Program.cs
@@ -6,7 +6,19 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            //We can create variables holding entire methods, but we need to
+            //define the method we are going to use
+            //Func<int, int, int> multiplier = Multiply;
+            //Console.WriteLine(multiplier(5, 6));
+        }
 
+        static int Multiply(int x, int y)
+        {
+            return x * y;
+        }
+
+        static Func<int, int, int> GetMethod(string input)
+        {
 
         }
     }
