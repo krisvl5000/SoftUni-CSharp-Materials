92dc8c838f0b4b0ac7a762f5edf577c246146257
Kristian <krisvl5000@gmail.com>
2023-01-23 10:08:27 +0200

    Update Program.cs

--- 03. C# Advanced/Functional Programming Exercises/Functional Programming Exercises/Program.cs
+++ 03. C# Advanced/Functional Programming Exercises/Functional Programming Exercises/Program.cs
@@ -6,8 +6,10 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            Action<string[]> print = x => Console.WriteLine(String.Join("\n", x));
 
-
+            string[] names = Console.ReadLine().Split();
+            print(names);
         }
     }
 }
