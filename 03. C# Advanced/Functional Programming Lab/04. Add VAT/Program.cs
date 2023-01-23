c2262f122a1d6be8de85852ee19a53d013d8674b
Kristian <krisvl5000@gmail.com>
2023-01-23 09:17:43 +0200

    Update Program.cs

--- 03. C# Advanced/Functional Programming Lab/04. Add VAT/Program.cs
+++ 03. C# Advanced/Functional Programming Lab/04. Add VAT/Program.cs
@@ -6,8 +6,20 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            double[] prices = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
 
+            Func<double, double> vatAdder = price => price + price * 0.2;
 
+            foreach (var item in prices)
+            {
+                double afterVat = 0;
+                afterVat = vatAdder(item);
+
+                Console.WriteLine($"{afterVat:F2}");
+            }
         }
     }
 }
