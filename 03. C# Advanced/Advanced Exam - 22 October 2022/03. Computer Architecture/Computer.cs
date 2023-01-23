3411e45ba4537216c695cc420df584c04d6a30b5
Kristian <krisvl5000@gmail.com>
2023-01-23 19:12:37 +0200

    Update Computer.cs

--- 03. C# Advanced/Advanced Exam - 22 October 2022/03. Computer Architecture/Computer.cs
+++ 03. C# Advanced/Advanced Exam - 22 October 2022/03. Computer Architecture/Computer.cs
@@ -8,6 +8,62 @@ namespace ComputerArchitecture
 {
     public class Computer
     {
+        public Computer(string model, int capacity)
+        {
+            Model = model;
+            Capacity = capacity;
+        }
 
+        public string Model { get; set; }
+
+        public int Capacity { get; set; }
+
+        public List<CPU> Multiprocessor { get; set; } = new List<CPU>();
+
+        public int Count { get { return Multiprocessor.Count; } }
+
+        public void Add(CPU cpu)
+        {
+            if (Multiprocessor.Count < Capacity)
+            {
+                Multiprocessor.Add(cpu);
+            }
+        }
+
+        public bool Remove(string brand)
+        {
+            if (Multiprocessor.Any(x => x.Brand == brand))
+            {
+                CPU cpuToRemove = Multiprocessor.First(x => x.Brand == brand);
+                Multiprocessor.Remove(cpuToRemove);
+                return true;
+            }
+
+            return false;
+        }
+
+        public CPU MostPowerful()
+        {
+            List<CPU> cpuList = new List<CPU>();
+
+            cpuList = Multiprocessor.OrderByDescending(x => x.Frequency).ToList();
+
+            return cpuList.First();
+        }
+
+        public CPU GetCPU(string brand)
+        {
+            if (Multiprocessor.Any(x => x.Brand == brand))
+            {
+                return Multiprocessor.First(x => x.Brand == brand);
+            }
+
+            return null;
+        }
+
+        public string Report()
+        {
+            return String.Join("\n", Multiprocessor);
+        }
     }
 }
