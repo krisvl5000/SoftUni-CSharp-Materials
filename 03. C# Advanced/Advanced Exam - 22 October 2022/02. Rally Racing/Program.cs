9c86f92dc23124a7c3101cda9b715e07eeb24567
Kristian <krisvl5000@gmail.com>
2023-01-23 11:13:36 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam - 22 October 2022/02. Rally Racing/Program.cs
+++ 03. C# Advanced/Advanced Exam - 22 October 2022/02. Rally Racing/Program.cs
@@ -6,7 +6,74 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            // Creating the matrix
 
+            int n = int.Parse(Console.ReadLine());
+            int racingNumber = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string raceRoute = Console.ReadLine();
+                
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = raceRoute[col];
+                }
+            }
+
+            // Finding the tunnel locations
+
+            int firstLocationRow;
+            int firstLocationCol;
+
+            int secondLocationRow;
+            int secondLocationCol;
+
+            bool firstLocationHasBeenFound = false;
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (matrix[row, col] == 'T')
+                    {
+                        if (!firstLocationHasBeenFound)
+                        {
+                            firstLocationRow = row;
+                            firstLocationCol = col;
+
+                            firstLocationHasBeenFound = true;
+                        }
+                        else
+                        {
+                            secondLocationRow = row;
+                            secondLocationCol = col;
+                        }
+                    }
+                }
+            }
+
+            // Moving the car
+
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int kilometersPassed = 0;
+
+            while (input[0] != "End")
+            {
+                string direction = input[0];
+
+                for (int row = 0; row < n; row++)
+                {
+                    for (int col = 0; col < n; col++)
+                    {
+
+                    }
+                }
+            }
 
         }
     }
