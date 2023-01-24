729fef9817baf16c2981dd2c5f03e5582d1e8341
Kristian <krisvl5000@gmail.com>
2023-01-24 15:11:17 +0200

    Update Racer.cs

--- 03. C# Advanced/Advanced Exam - 20 February 2021/03. The Race/Racer.cs
+++ 03. C# Advanced/Advanced Exam - 20 February 2021/03. The Race/Racer.cs
@@ -4,9 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03._The_Race
+namespace TheRace
 {
-    internal class Racer
+    public class Racer
     {
+        private string name;
+        private int age;
+        private string county;
+        private Car car;
+
+        public Racer(string name, int age, string country, Car car)
+        {
+            Name = name;
+            Age = age;
+            Country = country;
+            Car = car;
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public string Country { get; set; }
+
+        public Car Car { get; set; }
+
+        public override string ToString()
+        {
+            return $"Racer: {this.Name}, {this.Age} ({this.Country})";
+        }
     }
 }
