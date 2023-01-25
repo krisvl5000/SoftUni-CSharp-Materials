c835dc3e103d22b6754e94375bfccac5f822c10e
Kristian <krisvl5000@gmail.com>
2023-01-25 15:58:25 +0200

    Update Renovator.cs

--- 03. C# Advanced/Advanced Exam - 25 June 2022/Advanced Exam - 25 June 2022/03. Renovators_Skeleton/Renovator.cs
+++ 03. C# Advanced/Advanced Exam - 25 June 2022/Advanced Exam - 25 June 2022/03. Renovators_Skeleton/Renovator.cs
@@ -8,5 +8,39 @@ namespace Renovators
 {
     public class Renovator
     {
+        private string name;
+        private string type;
+        private double rate;
+        private int days;
+        private bool hired = false;
+
+        public Renovator(string name, string type, double rate, int days)
+        {
+            this.name = name;
+            this.type = type;
+            this.rate = rate;
+            this.days = days;
+        }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public double Rate { get; set; }
+
+        public int Days { get; set; }
+
+        public bool Hired { get; set; } = false;
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"-Renovator: {this.name}");
+            sb.AppendLine($"--Specialty: {this.type}");
+            sb.AppendLine($"--Rate per day: {this.rate} BGN");
+            
+            string stringToReturn = sb.ToString();
+            return stringToReturn.TrimEnd();
+        }
     }
 }
