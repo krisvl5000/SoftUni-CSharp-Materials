f505419aa8a4713aa56f9195a0ffc4ec5ffc02e0
Kristian <krisvl5000@gmail.com>
2023-01-25 15:58:22 +0200

    Update Catalog.cs

--- 03. C# Advanced/Advanced Exam - 25 June 2022/Advanced Exam - 25 June 2022/03. Renovators_Skeleton/Catalog.cs
+++ 03. C# Advanced/Advanced Exam - 25 June 2022/Advanced Exam - 25 June 2022/03. Renovators_Skeleton/Catalog.cs
@@ -8,5 +8,119 @@ namespace Renovators
 {
     public class Catalog
     {
+        private List<Renovator> renovators;
+
+        public Catalog(string name, int neededRenovators, string project)
+        {
+            Name = name;
+            NeededRenovators = neededRenovators;
+            Project = project;
+            this.renovators = new List<Renovator>();
+        }
+
+        public string Name { get; set; }
+
+        public int NeededRenovators { get; set; }
+
+        public string Project { get; set; }
+
+        public int Count => renovators.Count;
+
+        public string AddRenovator(Renovator renovator)
+        {
+            if (renovator.Name != null && renovator.Type != null)
+            {
+                return "Invalid renovator's information.";
+            }
+
+            if (renovators.Count >= NeededRenovators)
+            {
+                return "Renovators are no more needed.";
+            }
+
+            if (renovator.Rate > 350)
+            {
+                return "Invalid renovator's rate.";
+            }
+
+            renovators.Add(renovator);
+            return $"Successfully added {renovator.Name} to the catalog";
+        }
+
+        public bool RemoveRenovator(string name)
+        {
+            if (renovators.Any(x => x.Name == name))
+            {
+                Renovator renovatorToRemove = renovators
+                    .FirstOrDefault(x => x.Name == name);
+
+                renovators.Remove(renovatorToRemove);
+                return true;
+            }
+
+            return false;
+        }
+
+        public int RemoveRenovatorBySpecialty(string type)
+        {
+            if (renovators.Any(x => x.Type == type))
+            {
+                List<Renovator> renovatorsToRemove = renovators
+                    .Where(x => x.Type == type).ToList();
+
+                foreach (var item in renovatorsToRemove)
+                {
+                    renovators.Remove(item);
+                }
+
+                return renovatorsToRemove.Count;
+            }
+
+            return 0;
+        }
+
+        public Renovator HireRenovator(string name)
+        {
+            if (renovators.Any(x => x.Name == name))
+            {
+                Renovator renovatorToHire = renovators.First(x => x.Name == name);
+                renovatorToHire.Hired = true;
+                return renovatorToHire;
+            }
+
+            return null;
+        }
+
+        public List<Renovator> PayRenovators(int days)
+        {
+            List<Renovator> renovators = new List<Renovator>();
+
+            foreach (var item in renovators)
+            {
+                if (item.Days >= days)
+                {
+                    renovators.Add(item);
+                }
+            }
+
+            return renovators;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Renovators available for Project {this.Project}:");
+
+            foreach (var item in renovators)
+            {
+                if (!item.Hired)
+                {
+                    sb.AppendLine(item.ToString());
+                }
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
