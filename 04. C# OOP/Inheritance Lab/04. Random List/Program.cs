46510fa58037063e159c26eed2e7a9c59f26bd4a
Kristian <krisvl5000@gmail.com>
2023-01-26 16:44:14 +0200

    Update Program.cs

--- 04. C# OOP/Inheritance Lab/04. Random List/Program.cs
+++ 04. C# OOP/Inheritance Lab/04. Random List/Program.cs
@@ -6,8 +6,12 @@ namespace CustomRandomList
     {
         static void Main(string[] args)
         {
+            RandomList list = new RandomList();
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
 
-
+            Console.WriteLine(list.RandomString());
         }
     }
 }
