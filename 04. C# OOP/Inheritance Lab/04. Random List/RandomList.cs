0708cfdb45788b597245f87c39ccf69c3bc11a87
Kristian <krisvl5000@gmail.com>
2023-01-26 16:55:13 +0200

    Update RandomList.cs

--- 04. C# OOP/Inheritance Lab/04. Random List/RandomList.cs
+++ 04. C# OOP/Inheritance Lab/04. Random List/RandomList.cs
@@ -18,7 +18,7 @@ namespace CustomRandomList
         public string RandomString()
         {
             int index = random.Next(0, Count);
-            string removedElement = this[index]; // this allows us to access the indexator
+            string removedElement = this[index]; // this allows us to access the indexer
             this.RemoveAt(index);
 
             return removedElement;
