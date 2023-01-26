16f52ca36a1b5b7e78543de88ad4ef30830560c6
Kristian <krisvl5000@gmail.com>
2023-01-26 16:55:12 +0200

    Create StackOfStrings.cs

---
+++ 04. C# OOP/Inheritance Lab/05. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        private Stack<string> stack;
+
+        public StackOfStrings()
+        {
+            stack = new Stack<string>();
+        }
+
+        public bool IsEmpty()
+        {
+            return stack.Count == 0;
+        }
+
+        public Stack<string> AddRange()
+        {
+
+        }
+    }
+}
