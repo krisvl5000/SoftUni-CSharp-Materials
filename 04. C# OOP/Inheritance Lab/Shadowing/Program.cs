b84b3dd6b9b405af02ed79cc089fbb2c93d92155
Kristian <krisvl5000@gmail.com>
2023-01-26 15:12:58 +0200

    Update Program.cs

--- 04. C# OOP/Inheritance Lab/Shadowing/Program.cs
+++ 04. C# OOP/Inheritance Lab/Shadowing/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Shadowing
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            //How to avoid confusion with variables when using inheritance
+            Child c = new Child();
+            c.Add();
+        }
+    }
+
+    public class Parent
+    {
+        public int number = 5;
+    }
+
+    public class Child : Parent
+    {
+        public void Add()
+        {
+            Console.WriteLine(number);
+        }
+    }
+}
