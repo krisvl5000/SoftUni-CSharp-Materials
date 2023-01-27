4e29e22b8b616917b5f152d8979c676fd1edc6ca
Kristian <krisvl5000@gmail.com>
2023-01-27 10:14:47 +0200

    Create SoulMaster.cs

---
+++ 04. C# OOP/Inheritance Exercise/03. Players and Monsters/SoulMaster.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._Players_and_Monsters
+{
+    internal class SoulMaster
+    {
+    }
+}
