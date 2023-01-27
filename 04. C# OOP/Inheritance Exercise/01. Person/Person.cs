ae8fa518192c30d6519cde5e4d4e27c052950705
Kristian <krisvl5000@gmail.com>
2023-01-27 09:49:15 +0200

    Create Person.cs

---
+++ 04. C# OOP/Inheritance Exercise/01. Person/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+
+            if (age < 0)
+            {
+                age = 0;
+            }
+            else if (age > 15)
+            {
+                age = 15;
+            }
+        }
+
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(String.Format($"Name: {Name}, Age: {Age}"));
+
+            return sb.ToString();
+        }
+    }
+}
