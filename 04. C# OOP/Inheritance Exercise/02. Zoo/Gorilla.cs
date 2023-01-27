da6971904d5ae71867903e492c36070d79800293
Kristian <krisvl5000@gmail.com>
2023-01-27 09:58:08 +0200

    Create Gorilla.cs

---
+++ 04. C# OOP/Inheritance Exercise/02. Zoo/Gorilla.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._Zoo
+{
+    internal class Gorilla
+    {
+    }
+}
