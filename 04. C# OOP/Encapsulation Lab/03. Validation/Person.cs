bd3109485b1cc7da93e2fb8e911b33d6e767132e
Kristian <krisvl5000@gmail.com>
2023-01-28 11:38:37 +0200

    Update Person.cs

--- 04. C# OOP/Encapsulation Lab/03. Validation/Person.cs
+++ 04. C# OOP/Encapsulation Lab/03. Validation/Person.cs
@@ -8,6 +8,11 @@ namespace PersonsInfo
 {
     public class Person
     {
+        private string firstName; 
+        private string lastName;
+        private int age;
+        private decimal salary;
+
         public Person(string firstName, string lastName, int age, decimal salary)
         {
             FirstName = firstName;
@@ -15,13 +20,71 @@ namespace PersonsInfo
             Age = age;
             Salary = salary;
         }
-        public string FirstName { get; private set; }
+        public string FirstName 
+        {
+            get
+            {
+                return firstName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than " +
+                        "3 symbols!");
+                }
 
-        public string LastName { get; private set; }
+                firstName = value;
+            }
+        }
 
-        public int Age { get; private set; }
+        public string LastName
+        {
+            get { return lastName; }
 
-        public decimal Salary { get; private set; }
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than " +
+                        "3 symbols!");
+                }
+
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+
+            set
+            {
+                if (value < 650)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+
+                salary = value;
+            }
+        }
 
         public override string ToString()
         {
