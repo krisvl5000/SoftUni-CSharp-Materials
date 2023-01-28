c80fd1bbc030c026ccfdd31aadaae0b0f974419f
Kristian <krisvl5000@gmail.com>
2023-01-28 14:52:25 +0200

    Update Program.cs

--- 04. C# OOP/Encapsulation Exercise/01. Class Box Data/Program.cs
+++ 04. C# OOP/Encapsulation Exercise/01. Class Box Data/Program.cs
@@ -6,8 +6,15 @@ namespace ClassBoxData
     {
         static void Main(string[] args)
         {
+            double length = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
 
+            Box box = new Box(length, width, height);
 
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+            Console.WriteLine($"Volume - {box.Volume():F2}");
         }
     }
 }
