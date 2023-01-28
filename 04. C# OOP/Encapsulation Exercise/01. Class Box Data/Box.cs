5cb442b38313fd0ef45e70eec91e7b8a6b2941b7
Kristian <krisvl5000@gmail.com>
2023-01-28 14:52:24 +0200

    Update Box.cs

--- 04. C# OOP/Encapsulation Exercise/01. Class Box Data/Box.cs
+++ 04. C# OOP/Encapsulation Exercise/01. Class Box Data/Box.cs
@@ -8,6 +8,75 @@ namespace ClassBoxData
 {
     public class Box
     {
+        private double length;
+        private double width;
+        private double height;
 
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+
+        public double Length
+        {
+            get { return length; }
+
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                length = value;
+            }
+        }
+
+        public double Width
+        {
+            get { return width; }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                width = value;
+            }
+        }
+
+        public double Height
+        {
+            get { return height; }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                height = value;
+            }
+        }
+
+        public double SurfaceArea()
+        {
+            return 2 * length * width + 2 * length * height + 2 * width * height;
+        }
+
+        public double LateralSurfaceArea()
+        {
+            return 2 * length * height + 2 * width * height;
+        }
+
+        public double Volume()
+        {
+            return length * width * height;
+        }
     }
 }
