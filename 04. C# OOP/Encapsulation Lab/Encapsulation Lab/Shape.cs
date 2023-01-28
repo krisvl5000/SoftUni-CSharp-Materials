216b9b15a04f2c663aa42c77c23a2b18acb82741
Kristian <krisvl5000@gmail.com>
2023-01-28 10:21:58 +0200

    Update Shape.cs

--- 04. C# OOP/Encapsulation Lab/Encapsulation Lab/Shape.cs
+++ 04. C# OOP/Encapsulation Lab/Encapsulation Lab/Shape.cs
@@ -4,10 +4,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Encapsulation_Lab
+namespace Encapsulation
 {
     public class Shape
     {
-        
+        private int drawCount = 0;
+        private int maxDrawCount = 4000;
+        private int x = 25;
+        private int y = 10;
+
+        private int size = 6;
+
+        public virtual void Draw()
+        {
+            if (CanDrawShape())
+            {
+                Console.SetCursorPosition(x, y);
+                Console.WriteLine($"Drawing shape! with size {6}");
+            }
+        }
+
+        private bool CanDrawShape()
+        {
+            drawCount++;
+
+            if (drawCount > maxDrawCount)
+            {
+                Console.WriteLine("Too many shapes");
+                return false;
+            }
+
+            return true;
+        }
     }
 }
