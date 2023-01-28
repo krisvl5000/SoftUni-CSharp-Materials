3f54ba97f10d92d3ec1a28b3aff403e04b0effdb
Kristian <krisvl5000@gmail.com>
2023-01-28 11:48:59 +0200

    Create Person.cs

---
+++ 04. C# OOP/Encapsulation Lab/04. Team/Person.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonsInfo
+{
+    public class Person
+    {
+        private string firstName; 
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+        public string FirstName 
+        {
+            get
+            {
+                return firstName;
+            }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than " +
+                        "3 symbols!");
+                }
+
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+
+            set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than " +
+                        "3 symbols!");
+                }
+
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+
+            set
+            {
+                if (value < 650)
+                {
+                    throw new ArgumentException("Salary cannot be less than 650 leva!");
+                }
+
+                salary = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (Age < 30)
+            {
+                percentage /= 2;
+            }
+
+            Salary = Salary + Salary * percentage / 100;
+        }
+    }
+}
