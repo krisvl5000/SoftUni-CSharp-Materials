6fde7fed5bbd634ec8781421ab982fd9e86443d0
Kristian <krisvl5000@gmail.com>
2023-01-29 15:11:03 +0200

    Update Team.cs

--- 04. C# OOP/Encapsulation Exercise/05. Football Team Generator/Team.cs
+++ 04. C# OOP/Encapsulation Exercise/05. Football Team Generator/Team.cs
@@ -59,5 +59,10 @@ namespace FootballTeamGenerator
 
             playerList.Remove(playerToRemove);
         }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Rating}";
+        }
     }
 }
