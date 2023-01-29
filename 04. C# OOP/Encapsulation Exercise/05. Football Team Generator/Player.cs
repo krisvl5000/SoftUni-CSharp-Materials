0338c2b21673272e8083b98cd202d04498c4154e
Kristian <krisvl5000@gmail.com>
2023-01-29 11:31:06 +0200

    Update Player.cs

--- 04. C# OOP/Encapsulation Exercise/05. Football Team Generator/Player.cs
+++ 04. C# OOP/Encapsulation Exercise/05. Football Team Generator/Player.cs
@@ -4,9 +4,44 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _05._Football_Team_Generator
+namespace FootballTeamGenerator
 {
-    internal class Player
+    public class Player
     {
+        private string name;
+
+        public Player(string name, int endurance, int sprint, 
+            int dribble, int passing, int shooting)
+        {
+            Name = name;
+            Stats = new Stats(endurance, sprint, dribble, passing, shooting);
+        }
+
+        public string Name
+        {
+            get { return name; }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    // We should never directly write on the console in getters/setters/ctors/metohs
+                    // and so on...
+                    // We should not access directly the console or another writer in a model class
+                    throw new ArgumentException(ExceptionMessages.NAME_CANNOT_BE_NULL_OR_WHITESPACE);
+                }
+
+                name = value;
+            }
+        }
+
+        public Stats Stats { get; private set; }
+
+        public double OverallRating
+            => (Stats.Endurance + 
+               Stats.Sprint + 
+               Stats.Dribble + 
+               Stats.Passing + 
+               Stats.Shooting) / 5.0;
     }
 }
