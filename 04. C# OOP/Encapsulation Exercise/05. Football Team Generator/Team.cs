f0fd866ff31c200698f14cc7b4683eecdf15a536
Kristian <krisvl5000@gmail.com>
2023-01-29 11:52:10 +0200

    Update Team.cs

--- 04. C# OOP/Encapsulation Exercise/05. Football Team Generator/Team.cs
+++ 04. C# OOP/Encapsulation Exercise/05. Football Team Generator/Team.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _05._Football_Team_Generator
+namespace FootballTeamGenerator
 {
-    internal class Team
+    public class Team
     {
+        private string name;
+
+        public Team(string name)
+        {
+            Name = name;
+        }
+
+        public string Name
+        {
+            get { return name; }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(
+                        ExceptionMessages.NAME_CANNOT_BE_NULL_OR_WHITESPACE);
+                }
+
+                name = value;
+            }
+        }
     }
 }
