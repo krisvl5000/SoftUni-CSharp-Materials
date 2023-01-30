6788dcb5030b6ec1b7fde1f9ecf2f07c353527d2
Kristian <krisvl5000@gmail.com>
2023-01-30 12:27:44 +0200

    Update Program.cs

--- 04. C# OOP/Interfaces and Abstraction Lab/01. Shapes/Program.cs
+++ 04. C# OOP/Interfaces and Abstraction Lab/01. Shapes/Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 
-namespace _01._Hello_Softuni
+namespace Shapes
 {
-    internal class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
+            var radius = int.Parse(Console.ReadLine());
+            IDrawable circle = new Circle(radius);
 
+            var width = int.Parse(Console.ReadLine());
+            var height = int.Parse(Console.ReadLine());
+            IDrawable rect = new Rectangle(width, height);
 
+            circle.Draw();
+            rect.Draw();
         }
     }
 }
