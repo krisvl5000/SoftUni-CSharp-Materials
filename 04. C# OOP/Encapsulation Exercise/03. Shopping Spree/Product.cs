61fd68b166ec9744e97b3c5e55c216a006db4144
Kristian <krisvl5000@gmail.com>
2023-01-30 15:16:20 +0200

    Update Product.cs

--- 04. C# OOP/Encapsulation Exercise/03. Shopping Spree/Product.cs
+++ 04. C# OOP/Encapsulation Exercise/03. Shopping Spree/Product.cs
@@ -4,9 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03._Shopping_Spree
+namespace ShoppingSpree
 {
-    internal class Product
+    public class Product
     {
+        private string name;
+        private int cost;
+
+        public Product(string name, int cost)
+        {
+            Name = name;
+            Cost = cost;
+        }
+
+        public string Name
+        {
+            get { return name; }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+
+        public int Cost
+        {
+            get { return cost; }
+
+            private set
+            {
+                if (double.IsNegative(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                cost = value;
+            }
+        }
     }
 }
