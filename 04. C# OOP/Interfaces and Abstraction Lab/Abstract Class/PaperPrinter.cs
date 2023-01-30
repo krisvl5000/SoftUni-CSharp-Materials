dffedaf6df521474ec05c8f0748bf3992fed65ec
Kristian <krisvl5000@gmail.com>
2023-01-30 12:48:31 +0200

    Create PaperPrinter.cs

---
+++ 04. C# OOP/Interfaces and Abstraction Lab/Abstract Class/PaperPrinter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class PaperPrinter : Printer
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Printing to paper");
+        }
+    }
+}
