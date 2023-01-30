11dfdd6c9180e1a601fc591db4d032254b08df59
Kristian <krisvl5000@gmail.com>
2023-01-30 14:48:46 +0200

    Create IDrawer.cs

---
+++ 04. C# OOP/Interfaces and Abstraction Lab/Game/IDrawer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Game
+{
+    interface IDrawer
+    {
+        void Write(string message);
+
+        void WriteLine(string message);
+
+        void WriteAt(string message, int x, int y);
+    }
+}
