0482de23c754a858b71f518c1e9266e625688079
Kristian <krisvl5000@gmail.com>
2023-01-30 12:48:28 +0200

    Create ThreeDPrinter.cs

---
+++ 04. C# OOP/Interfaces and Abstraction Lab/Abstract Class/ThreeDPrinter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractClass
+{
+    public class ThreeDPrinter : Printer
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Printing in 3d");
+        }
+    }
+}
