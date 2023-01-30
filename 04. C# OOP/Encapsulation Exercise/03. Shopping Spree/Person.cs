dc77cbdfc8687c9ba85749dc3a4d25cbf92e4a67
Kristian <krisvl5000@gmail.com>
2023-01-30 15:16:14 +0200

    Update Person.cs

--- 04. C# OOP/Encapsulation Exercise/03. Shopping Spree/Person.cs
+++ 04. C# OOP/Encapsulation Exercise/03. Shopping Spree/Person.cs
@@ -4,9 +4,50 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03._Shopping_Spree
+namespace ShoppingSpree
 {
-    internal class Person
+    public class Person
     {
+        private string name;
+        private int money;
+
+        public Person(string name, int money)
+        {
+            Name = name;
+            Money = money;
+            Products = new List<Product>();
+        }
+
+        public string Name
+        {
+            get { return name; }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                name = value;
+            }
+        }
+
+        public int Money
+        {
+            get { return money; }
+
+            private set
+            {
+                if (double.IsNegative(value))
+                {
+                    throw new ArgumentException("Name cannot be empty");
+                }
+
+                money = value;
+            }
+        }
+
+        public List<Product> Products { get; set; }
     }
 }
