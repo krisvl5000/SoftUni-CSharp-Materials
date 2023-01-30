af4ce4270479b6037dd99b9ae3b2a4030bab601d
Kristian <krisvl5000@gmail.com>
2023-01-30 14:25:51 +0200

    Create Program.cs

---
+++ 04. C# OOP/Interfaces and Abstraction Lab/Game/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Game
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Flap");
+            }
+
+        }
+    }
+}
