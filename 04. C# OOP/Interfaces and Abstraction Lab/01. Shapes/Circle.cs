fd4f84b642211f8ad2f996842e520a4d760b3ef5
Kristian <krisvl5000@gmail.com>
2023-01-30 12:27:41 +0200

    Update Circle.cs

--- 04. C# OOP/Interfaces and Abstraction Lab/01. Shapes/Circle.cs
+++ 04. C# OOP/Interfaces and Abstraction Lab/01. Shapes/Circle.cs
@@ -4,9 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _01._Shapes
+namespace Shapes
 {
-    internal class Circle
+    public class Circle : IDrawable
     {
+        private int radius;
+
+        public Circle(int radius)
+        {
+            this.radius = radius;
+        }
+
+        public void Draw()
+        {
+            double rIn = radius - 0.4;
+            double rOut = radius + 0.4;
+
+            for (int y = radius; y < rOut; y--)
+            {
+                for (double x = -radius; x < rOut; x += 0.5)
+                {
+                    double value = x * x + y * y;
+
+                    if (value >= rIn * rIn && value <= rOut * rOut)
+                    {
+                        Console.WriteLine("*");
+                    }
+                    else
+                    {
+                        Console.WriteLine(" ");
+                    }
+                }
+
+                Console.WriteLine();
+            }
+        }
     }
 }
