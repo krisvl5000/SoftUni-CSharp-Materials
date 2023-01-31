7e09eaa41673dc1478cfc7d1f98b05538622f762
Kristian <krisvl5000@gmail.com>
2023-01-31 14:40:21 +0200

    Update Citizen.cs

--- 04. C# OOP/Interfaces and Abstraction Exercise/02. Multiple Implementation/Citizen.cs
+++ 04. C# OOP/Interfaces and Abstraction Exercise/02. Multiple Implementation/Citizen.cs
@@ -1,14 +1,18 @@
 ﻿namespace PersonInfo
 {
-    public class Citizen : IPerson
+    public class Citizen : IPerson, IIdentifiable, IBirthable
     {
         private string name;
         private int age;
+        private string id;
+        private string birthdate;
 
-        public Citizen(string name, int age)
+        public Citizen(string name, int age, string id, string birthdate)
         {
             Name = name;
             Age = age;
+            Id = id;
+            Birthdate = birthdate;
         }
 
         public string Name
@@ -38,6 +42,36 @@
                 }
 
                 age = value;
+            }
+        }
+
+        public string Id
+        {
+            get { return id; }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Id cannot be null or empty!");
+                }
+
+                id = value;
+            }
+        }
+
+        public string Birthdate
+        {
+            get { return birthdate; }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Birthdate cannot be null or empty!");
+                }
+
+                birthdate = value;
             }
         }
     }
