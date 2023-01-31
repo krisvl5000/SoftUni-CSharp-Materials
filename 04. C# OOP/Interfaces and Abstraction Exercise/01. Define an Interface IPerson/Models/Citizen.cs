17d825af1f34ada9f25bd5ecd089cc5162476e35
Kristian <krisvl5000@gmail.com>
2023-01-31 10:45:11 +0200

    Update Citizen.cs

--- 04. C# OOP/Interfaces and Abstraction Exercise/01. Define an Interface IPerson/Models/Citizen.cs
+++ 04. C# OOP/Interfaces and Abstraction Exercise/01. Define an Interface IPerson/Models/Citizen.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PersonInfo.Models
+﻿namespace PersonInfo.Models
 {
-    internal class Citizen
+    using Interfaces;
+
+    public class Citizen : IPerson
     {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get { return name; }
+
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Name cannot be null or empty!");
+                }
+
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age must be a positive number!");
+                }
+
+                age = value;
+            }
+        }
     }
 }
