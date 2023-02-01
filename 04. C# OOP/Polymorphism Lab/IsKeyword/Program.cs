2fc392c085ec73834bddd49c7ca5ee2d13c9b781
Kristian <krisvl5000@gmail.com>
2023-02-01 10:50:36 +0200

    Update Program.cs

--- 04. C# OOP/Polymorphism Lab/IsKeyword/Program.cs
+++ 04. C# OOP/Polymorphism Lab/IsKeyword/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace IsKeyword
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape shape;
+
+            if (new Random().Next() % 2 == 0)
+            {
+                shape = new Circle();
+            }
+            else
+            {
+                shape = new Rectangle();
+            }
+
+            shape.Draw();
+
+        }
+    }
+}
