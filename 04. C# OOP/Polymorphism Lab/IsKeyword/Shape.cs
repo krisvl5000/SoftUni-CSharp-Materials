42ea43e1f5b6a23069168fc6554172acdc442fcd
Kristian <krisvl5000@gmail.com>
2023-02-01 10:50:31 +0200

    Create Shape.cs

---
+++ 04. C# OOP/Polymorphism Lab/IsKeyword/Shape.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsKeyword
+{
+    public class Shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Shape draw");
+        }
+    }
+}
