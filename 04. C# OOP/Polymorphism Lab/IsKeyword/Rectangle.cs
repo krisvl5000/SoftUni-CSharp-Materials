bb7cad97d25ec2dbe5298a48cb68704e8cb6a49e
Kristian <krisvl5000@gmail.com>
2023-02-01 10:50:34 +0200

    Create Rectangle.cs

---
+++ 04. C# OOP/Polymorphism Lab/IsKeyword/Rectangle.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsKeyword
+{
+    public class Rectangle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Rectangle draw");
+        }
+
+        public int Width { get; set; }
+
+        public int Height { get; set; }
+    }
+}
