f60f5f18a78401366cbdefb2ab79b0f315d82d09
Kristian <krisvl5000@gmail.com>
2023-02-01 12:27:30 +0200

    Update Circle.cs

--- 04. C# OOP/Polymorphism Lab/03. Shapes/Circle.cs
+++ 04. C# OOP/Polymorphism Lab/03. Shapes/Circle.cs
@@ -4,9 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03._Shapes
+namespace Shapes
 {
-    internal class Circle
+    public class Circle : Shape
     {
+        private double radius;
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double Radius { get; private set; }
+
+        public override double CalculateArea()
+        {
+            return 2 * Math.PI * Radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return Math.PI * Radius * Radius;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw();
+        }
     }
 }
