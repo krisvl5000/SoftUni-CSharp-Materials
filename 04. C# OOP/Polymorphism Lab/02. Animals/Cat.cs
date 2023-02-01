5352e30c43a5b2b4194f13c548a965eedcda697a
Kristian <krisvl5000@gmail.com>
2023-02-01 12:10:12 +0200

    Create Cat.cs

---
+++ 04. C# OOP/Polymorphism Lab/02. Animals/Cat.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Animals
+{
+    public class Cat : Animal
+    {
+        public Cat(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+
+        }
+
+        public override string ExplainSelf()
+        {
+            return base.ExplainSelf() +
+                $"\nMEEOW";
+        }
+
+    }
+}
