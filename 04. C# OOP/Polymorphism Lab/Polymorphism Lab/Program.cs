0dd8fe51a4b58317ca4dbf10b4c9bfeaeb3338ce
Kristian <krisvl5000@gmail.com>
2023-02-01 09:59:34 +0200

    Update Program.cs

--- 04. C# OOP/Polymorphism Lab/Polymorphism Lab/Program.cs
+++ 04. C# OOP/Polymorphism Lab/Polymorphism Lab/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Polymorphism
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+
+
+        }
+    }
+}
