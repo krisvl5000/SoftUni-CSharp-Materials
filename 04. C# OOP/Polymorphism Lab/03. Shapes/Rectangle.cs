5b29056e35d5079b98ed169a4b3d04d9727c0076
Kristian <krisvl5000@gmail.com>
2023-02-01 12:27:35 +0200

    Update Rectangle.cs

--- 04. C# OOP/Polymorphism Lab/03. Shapes/Rectangle.cs
+++ 04. C# OOP/Polymorphism Lab/03. Shapes/Rectangle.cs
@@ -4,9 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03._Shapes
+namespace System
 {
-    internal class Rectangle
+    public class Rectangle : Shape
     {
+        private double height;
+        private double width;
+
+        public Rectangle(double height, double width)
+        {
+            Heigth = height;
+            Width = width;
+        }
+
+        public double Heigth { get; private set; }
+
+        public double Width { get; private set; }
+
+        public override double CalculateArea()
+        {
+            return Heigth * Width;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Heigth + 2 * Width;
+        }
+
+        public override string Draw()
+        {
+            return base.Draw();
+        }
     }
 }
