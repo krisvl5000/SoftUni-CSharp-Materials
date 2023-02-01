0c548ddc12574757721c3b5c02b08ca248efbd2d
Kristian <krisvl5000@gmail.com>
2023-02-01 09:59:31 +0200

    Update OldKitchen.cs

--- 04. C# OOP/Polymorphism Lab/Polymorphism Lab/OldKitchen.cs
+++ 04. C# OOP/Polymorphism Lab/Polymorphism Lab/OldKitchen.cs
@@ -4,9 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Polymorphism_Lab
+namespace Polymorphism
 {
-    internal class OldKitchen
+    public class OldKitchen : Kitchen
     {
+        public override void CleanKitchen()
+        {
+            Console.WriteLine("Old Kitchen: izhvurli vsichko i izmii");
+        }
+
+        public override void CookMeat()
+        {
+            Console.WriteLine("Old Kitchen: slozhi go na ogunq i gledai da ne se oparish");
+        }
+
+        public override void CookVegetarian()
+        {
+            Console.WriteLine("Old Kitchen: nqma vegetarianci");
+        }
     }
 }
