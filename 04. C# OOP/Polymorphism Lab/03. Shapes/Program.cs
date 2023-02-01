1ab5b9796ffcdabc030139e6c4196612a883cca5
Kristian <krisvl5000@gmail.com>
2023-02-01 16:26:59 +0200

    Update Program.cs

--- 04. C# OOP/Polymorphism Lab/03. Shapes/Program.cs
+++ 04. C# OOP/Polymorphism Lab/03. Shapes/Program.cs
@@ -6,7 +6,10 @@ namespace Shapes
     {
         static void Main(string[] args)
         {
-
+            Shape shape = new Circle(5.0);
+            Shape rectangle = new Rectangle(34.0, 434.0);
+            Console.WriteLine(shape.Draw());
+            Console.WriteLine(rectangle.Draw());
 
         }
     }
