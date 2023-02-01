fc23587fd52513af98ad0850dd5ad5439a995138
Kristian <krisvl5000@gmail.com>
2023-02-01 10:50:30 +0200

    Create Circle.cs

---
+++ 04. C# OOP/Polymorphism Lab/IsKeyword/Circle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IsKeyword
+{
+    public class Circle : Shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Circle draw");
+        }
+
+        public int Raduis { get; set; }
+    }
+}
