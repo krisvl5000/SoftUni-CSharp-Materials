c810c9e3052c4b370519a7eba72909269ed3203b
Kristian <krisvl5000@gmail.com>
2023-02-01 11:53:25 +0200

    Update Program.cs

--- 04. C# OOP/Polymorphism Lab/01. Math Operations/Program.cs
+++ 04. C# OOP/Polymorphism Lab/01. Math Operations/Program.cs
@@ -1,13 +1,16 @@
 ﻿using System;
 
-namespace _01._Hello_Softuni
+namespace Operations
 {
-    internal class Program
+    public class StartUp
     {
         static void Main(string[] args)
         {
+            MathOperations mo = new MathOperations();
 
-
+            Console.WriteLine(mo.Add(2, 3));
+            Console.WriteLine(mo.Add(2.2, 3.3, 5.5));
+            Console.WriteLine(mo.Add(2.2m, 3.3m, 4.4m));
         }
     }
 }
