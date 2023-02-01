73c3fbf3052841087968f51306d627764f5cdc26
Kristian <krisvl5000@gmail.com>
2023-02-01 10:38:02 +0200

    Create RobotKitchen.cs

---
+++ 04. C# OOP/Polymorphism Lab/Polymorphism Lab/RobotKitchen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class RobotKitchen : ModernKitchen
+    {
+        public override void CookMeat()
+        {
+            Console.WriteLine("Robot Kitchen: nai-pravilno sgotvenoto meso");
+        }
+
+        public override void CookSalad()
+        {
+            Console.WriteLine("Robot Kitchen: gurme salata (nqma q)");
+        }
+
+        public void RobotClean()
+        {
+            Console.WriteLine("Robot Kitchen : Robotite shte ni prevzemat");
+        }
+    }
+}
