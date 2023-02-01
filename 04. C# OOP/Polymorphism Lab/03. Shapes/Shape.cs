19106e23ecd1d7c4635832f614f5e3f711f149a2
Kristian <krisvl5000@gmail.com>
2023-02-01 12:27:34 +0200

    Create Shape.cs

---
+++ 04. C# OOP/Polymorphism Lab/03. Shapes/Shape.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace System
+{
+    public abstract class Shape
+    {
+        public abstract double CalculatePerimeter();
+
+        public abstract double CalculateArea();
+
+        public virtual string Draw()
+        {
+            return $"Drawing {typeof(object)}";
+        }
+    }
+}
