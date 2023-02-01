652e6b04406ba57e0642fc1b9a305cd13812c93f
Kristian <krisvl5000@gmail.com>
2023-02-01 09:59:27 +0200

    Create NormalKitchen.cs

---
+++ 04. C# OOP/Polymorphism Lab/Polymorphism Lab/NormalKitchen.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    public class NormalKitchen : Kitchen
+    {
+        public override void CleanKitchen()
+        {
+            Console.WriteLine("Normal Kitchen: izmii spokoino");
+        }
+
+        public override void CookMeat()
+        {
+            Console.WriteLine("Normal Kitchen: sgotvi mesoto");
+        }
+
+        public override void CookVegetarian()
+        {
+            Console.WriteLine("Normal Kitchen: eto salata");
+        }
+    }
+}
