cb6234090b6fed47d30b5e3f234d6a6cefd93ba8
Kristian <krisvl5000@gmail.com>
2023-02-02 15:07:00 +0200

    Update Vehicle.cs

--- 04. C# OOP/Polymorphism Exercise/01. Vehicles/Models/Vehicle.cs
+++ 04. C# OOP/Polymorphism Exercise/01. Vehicles/Models/Vehicle.cs
@@ -4,9 +4,40 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _01._Vehicles.Models
+namespace Vehicles
 {
-    internal class Vehicle
+    public abstract class Vehicle : IVehicle
     {
+        protected Vehicle(double fuelQuantity, 
+            double fuelConsumption, 
+            double fuelConsumptionIncrement)
+        {
+            FuelQuantity = fuelQuantity;
+            FuelConsumption = fuelConsumption + fuelConsumptionIncrement;
+        }
+
+        public double FuelQuantity { get; private set; }
+
+        public double FuelConsumption { get; private set; }
+
+        public string Drive(double distance)
+        {
+            double neededFuel = distance * FuelConsumption;
+
+            if (neededFuel > FuelQuantity)
+            {
+                throw new InsufficientFuelException(String
+                    .Format(ExceptionMessages
+                    .INSUFFICIENT_FUEL_EXCEPTION_MESSAGE, GetType().Name));
+            }
+
+            FuelQuantity -= neededFuel;
+            return $"{GetType().Name} travelled {distance} km";
+        }
+
+        public virtual void Refuel(double liters)
+        {
+            FuelQuantity += liters;
+        }
     }
 }
