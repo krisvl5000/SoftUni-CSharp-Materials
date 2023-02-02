b89ffbc9d710e87f8cc11ebadb61bac0f4dc741a
Kristian <krisvl5000@gmail.com>
2023-02-02 15:39:19 +0200

    Update Car.cs

--- 04. C# OOP/Polymorphism Exercise/01. Vehicles/Models/Car.cs
+++ 04. C# OOP/Polymorphism Exercise/01. Vehicles/Models/Car.cs
@@ -4,9 +4,16 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _01._Vehicles.Models
+namespace Vehicles
 {
-    internal class Car
+    public class Car : Vehicle
     {
+        private const double FUEL_CONSUMPTION_INCEREMENT = 0.9;
+        public Car(double fuelQuantity, 
+            double fuelConsumption) : 
+            base(fuelQuantity, fuelConsumption, FUEL_CONSUMPTION_INCEREMENT)
+        {
+
+        }
     }
 }
