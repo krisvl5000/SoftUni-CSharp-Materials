f62a0fcbee9ddd85b4a6c806f36321459b495088
Kristian <krisvl5000@gmail.com>
2023-02-02 15:39:17 +0200

    Update Truck.cs

--- 04. C# OOP/Polymorphism Exercise/01. Vehicles/Models/Truck.cs
+++ 04. C# OOP/Polymorphism Exercise/01. Vehicles/Models/Truck.cs
@@ -4,9 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _01._Vehicles.Models
+namespace Vehicles
 {
-    internal class Truck
+    public class Truck : Vehicle
     {
+        private const double FUEL_CONSUMPTION_INCREMENT = 1.6;
+        public Truck(double fuelQuantity, 
+            double fuelConsumption) : 
+            base(fuelQuantity, fuelConsumption, FUEL_CONSUMPTION_INCREMENT)
+        {
+
+        }
+
+        public override void Refuel(double liters)
+        {
+            base.Refuel(liters * 0.95);
+        }
     }
 }
