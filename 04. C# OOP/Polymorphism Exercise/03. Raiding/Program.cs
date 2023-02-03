550ba4dc9ab0162ff3577838d9a81fbc3f3a815d
Kristian <krisvl5000@gmail.com>
2023-02-03 16:00:31 +0200

    Update Program.cs

--- 04. C# OOP/Polymorphism Exercise/03. Raiding/Program.cs
+++ 04. C# OOP/Polymorphism Exercise/03. Raiding/Program.cs
@@ -6,7 +6,12 @@ namespace Raiding
     {
         static void Main(string[] args)
         {
-            
+            IReader reader = new ConsoleReader();
+            IWriter writer = new ConsoleWriter();
+
+            IEngine engine = new Engine(reader, writer);
+
+            engine.Run();
 
         }
     }
