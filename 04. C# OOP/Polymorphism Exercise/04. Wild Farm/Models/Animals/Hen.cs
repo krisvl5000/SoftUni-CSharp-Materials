7af11456b9c21769d1c31f9ebeab36f0dcdf8326
Kristian <krisvl5000@gmail.com>
2023-02-05 11:03:27 +0200

    Create Hen.cs

---
+++ 04. C# OOP/Polymorphism Exercise/04. Wild Farm/Models/Animals/Hen.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04._Wild_Farm.Models.Animals
+{
+    internal class Hen
+    {
+    }
+}
