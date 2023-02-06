9c109e5c9b266a272c35b0258c82d37c74f09348
Kristian <krisvl5000@gmail.com>
2023-02-06 10:21:07 +0200

    Update Program.cs

--- 04. C# OOP/Exceptions and Error Handling Lab/01. Square Root/Program.cs
+++ 04. C# OOP/Exceptions and Error Handling Lab/01. Square Root/Program.cs
@@ -6,7 +6,25 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
 
+                if (n < 0)
+                {
+                    throw new ArgumentException();
+                }
+
+                Console.WriteLine(Math.Sqrt(n));
+            }
+            catch (ArgumentException ae)
+            {
+                Console.WriteLine("Invalid number.");
+            }
+            finally
+            {
+                Console.WriteLine("Goodbye.");
+            }
 
         }
     }
