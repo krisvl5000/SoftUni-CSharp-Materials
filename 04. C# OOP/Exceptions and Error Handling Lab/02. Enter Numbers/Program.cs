310bb144c5e22704ee76babba563de51feb4bb22
Kristian <krisvl5000@gmail.com>
2023-02-06 10:29:07 +0200

    Update Program.cs

--- 04. C# OOP/Exceptions and Error Handling Lab/02. Enter Numbers/Program.cs
+++ 04. C# OOP/Exceptions and Error Handling Lab/02. Enter Numbers/Program.cs
@@ -6,8 +6,26 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            List<int> list = new List<int>();
 
+        }
 
+        static void ReadNumber(int start, int end)
+        {
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+
+                if (n < start || n > end)
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+
+                
+            }
         }
     }
 }
