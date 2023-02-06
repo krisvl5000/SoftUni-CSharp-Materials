fb8b628b4e36a4664b8ea17fea679b4f436e775a
Kristian <krisvl5000@gmail.com>
2023-02-06 12:18:34 +0200

    Update Program.cs

--- 04. C# OOP/Exceptions and Error Handling Lab/04. Sum of Integers/Program.cs
+++ 04. C# OOP/Exceptions and Error Handling Lab/04. Sum of Integers/Program.cs
@@ -6,7 +6,32 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
 
+            int totalSum = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                try
+                {
+                    int num = int.Parse(input[i]);
+                    totalSum += num;
+                }
+                catch (FormatException fe)
+                {
+                    Console.WriteLine($"The element '{input[i]}' is in wrong format!");
+                }
+                catch (OverflowException ofe)
+                {
+                    Console.WriteLine($"The '{input[i]}' is out of range!");
+                }
+
+                Console.WriteLine($"Element '{input[i]}' processed - " +
+                        $"current sum: {totalSum}");
+            }
+
+            Console.WriteLine($"The total sum of all integers is: {totalSum}");
 
         }
     }
