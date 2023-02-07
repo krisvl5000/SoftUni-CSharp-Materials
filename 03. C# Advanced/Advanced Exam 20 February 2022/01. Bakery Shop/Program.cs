cc26f98eb9db11b1fc2af8c3dbe551bb17ddf89f
Kristian <krisvl5000@gmail.com>
2023-02-07 14:47:38 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam 20 February 2022/01. Bakery Shop/Program.cs
+++ 03. C# Advanced/Advanced Exam 20 February 2022/01. Bakery Shop/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,6 +8,91 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            double[] waterArr = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Queue<double> waterQueue = new Queue<double>(waterArr);
+
+            double[] flourArr = Console.ReadLine().Split().Select(double.Parse).ToArray();
+            Stack<double> flourStack = new Stack<double>(flourArr);
+
+            var dict = new Dictionary<string, int>();
+
+            while (waterQueue.Count > 0 || flourStack.Count > 0)
+            {
+                double water = waterQueue.Peek();
+                double flour = flourStack.Peek();
+
+                double waterRatio = water * 100 / (water + flour);
+
+                if (waterRatio == 50)
+                {
+                    if (!dict.ContainsKey("croissant"))
+                    {
+                        dict.Add("croissant", 0);
+                    }
+
+                    dict["croissant"]++;
+
+                    waterQueue.Dequeue();
+                    flourStack.Pop();
+                }
+                else if (waterRatio == 40)
+                {
+                    if (!dict.ContainsKey("muffin"))
+                    {
+                        dict.Add("muffin", 0);
+                    }
+
+                    dict["muffin"]++;
+
+                    waterQueue.Dequeue();
+                    flourStack.Pop();
+                }
+                else if (waterRatio == 30)
+                {
+                    if (!dict.ContainsKey("baguette"))
+                    {
+                        dict.Add("baguette", 0);
+                    }
+
+                    dict["baguette"]++;
+
+                    waterQueue.Dequeue();
+                    flourStack.Pop();
+                }
+                else if (waterRatio == 20)
+                {
+                    if (!dict.ContainsKey("bagel"))
+                    {
+                        dict.Add("bagel", 0);
+                    }
+
+                    dict["bagel"]++;
+
+                    waterQueue.Dequeue();
+                    flourStack.Pop();
+                }
+                else
+                {
+                    if (!dict.ContainsKey("croissant"))
+                    {
+                        dict.Add("croissant", 0);
+                    }
+
+                    dict["croissant"]++;
+
+                    double flourUsed = flourStack.Pop();
+                    flourUsed -= water;
+                    flourStack.Push(flourUsed);
+
+                    while (flour < water) // might need some refactoring
+                    {
+                        flourUsed = flourStack.Pop();
+                        flourUsed -= water;
+                        flourStack.Push(flourUsed);
+                    }
+                }
+            }
+
 
 
         }
