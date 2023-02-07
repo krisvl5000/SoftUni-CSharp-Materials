b4832bc6327c26e90b330a75f067c0f9f99470a7
Kristian <krisvl5000@gmail.com>
2023-02-07 12:11:27 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam 20 February 2022/01. Bakery Shop/Program.cs
+++ 03. C# Advanced/Advanced Exam 20 February 2022/01. Bakery Shop/Program.cs
@@ -6,8 +6,84 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+            char[,] matrix = new char[n, n];
 
+            int branchesCounter = 0;
 
+            for (int row1 = 0; row1 < n; row1++)
+            {
+                string line = Console.ReadLine();
+                for (int col1 = 0; col1 < n; col1++)
+                {
+                    matrix[row1, col1] = line[col1];
+
+                    if (matrix[row1, col1] != '-' && 
+                        matrix[row1, col1] != 'B' &&
+                        matrix[row1, col1] != 'F')
+                    {
+                        branchesCounter++;
+                    }
+                }
+            }
+
+            (int row, int col) = FindBeaverLocation(matrix);
+
+            string command = Console.ReadLine();
+            while (command != "end")
+            {
+                if (command == "up")
+                {
+                    Move(matrix, row, col, row - 1, col);
+                }
+                else if (command == "down")
+                {
+                    Move(matrix, row, col, row + 1, col);
+                }
+                else if (command == "left")
+                {
+                    Move(matrix, row, col, row, col - 1);
+                }
+                else if (command == "right")
+                {
+                    Move(matrix, row, col, row, col + 1);
+                }
+
+                command = Console.ReadLine();
+            }
+
+            //for (int row = 0; row < n; row++)
+            //{
+            //    for (int col = 0; col < n; col++)
+            //    {
+            //        Console.Write(matrix[row, col]);
+            //    }
+
+            //    Console.WriteLine();
+            //}
+
+        }
+
+        static void Move(char[,] matrix, int row, int col, int newRow, int newCol)
+        {
+            matrix[row, col] = '-';
+            matrix[newRow, newCol] = 'B';
+        }
+
+        static (int row, int col) FindBeaverLocation(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 'B')
+                    {
+                        return (row, col);
+                    }
+                }
+            }
+
+            throw new Exception();
         }
     }
 }
