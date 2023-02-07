3380518fe3f4ea99fdd3c2132759c72ed527b45d
Kristian <krisvl5000@gmail.com>
2023-02-07 17:28:44 +0200

    Update Fish.cs

--- 03. C# Advanced/Advanced Exam 20 February 2022/03. Fishing Net/Fish.cs
+++ 03. C# Advanced/Advanced Exam 20 February 2022/03. Fishing Net/Fish.cs
@@ -4,9 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03._Fishing_Net
+namespace FishingNet
 {
-    internal class Fish
+    public class Fish
     {
+        private string fishType;
+        private double fishLength;
+        private double fishWeight;
+
+        public Fish(string fishType, double fishLength, double fishWeight)
+        {
+            Type = fishType;
+            Length = fishLength;
+            Weight = fishWeight;
+        }
+
+        public string Type { get; set; }
+
+        public double Length { get; set; }
+
+        public double Weight { get; set; }
+
+        public override string ToString()
+        {
+            return $"There is a {Type}, {Length} cm. long, " +
+                $"and {Weight} gr. in weight.";
+        }
     }
 }
