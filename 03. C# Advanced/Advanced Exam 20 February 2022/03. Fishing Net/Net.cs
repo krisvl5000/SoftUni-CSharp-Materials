2f2ddac9d023f39c8581e210c2b217bb7ac42a0c
Kristian <krisvl5000@gmail.com>
2023-02-07 17:28:48 +0200

    Update Net.cs

--- 03. C# Advanced/Advanced Exam 20 February 2022/03. Fishing Net/Net.cs
+++ 03. C# Advanced/Advanced Exam 20 February 2022/03. Fishing Net/Net.cs
@@ -4,9 +4,76 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03._Fishing_Net
+namespace FishingNet
 {
-    internal class Net
+    public class Net
     {
+        private readonly List<Fish> fish;
+
+        public Net(string material, int capacity)
+        {
+            Material = material;
+            Capacity = capacity;
+            fish = new List<Fish>();
+        }
+
+        public string Material { get; set; }
+
+        public int Capacity { get; set; }
+
+        public List<Fish> Fish { get { return this.fish; }  }
+
+        public int Count => fish.Count;
+
+        public string AddFish(Fish fish)
+        {
+            if (string.IsNullOrWhiteSpace(fish.Type))
+            {
+                return "Invalid fish.";
+            }
+            if (this.fish.Count == Capacity)
+            {
+                return "Fishing net is full";
+            }
+
+            this.fish.Add(fish);
+            return $"Successfully added {fish.Type} to the fishing net.";
+        }
+
+        public bool ReleaseFish(double weight)
+        {
+            Fish fishToRealease = fish.FirstOrDefault(x => x.Weight == weight);
+
+            if (fishToRealease == null)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public Fish GetFish(string fishType)
+        {
+            return fish.FirstOrDefault(x => x.Type == fishType);
+        }
+
+        public Fish GetBiggestFish()
+        {
+            return fish.OrderByDescending(x => x.Length).First();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Into the {Material}:");
+
+            foreach (var item in fish.OrderByDescending(x => x.Length))
+            {
+                sb.AppendLine(item.ToString().TrimEnd());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
