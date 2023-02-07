c35375557f1dc5cde578858a25227585bc6332a9
Kristian <krisvl5000@gmail.com>
2023-02-07 12:57:19 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam 20 February 2022/01. Bakery Shop/Program.cs
+++ 03. C# Advanced/Advanced Exam 20 February 2022/01. Bakery Shop/Program.cs
@@ -2,88 +2,203 @@
 
 namespace _01._Hello_Softuni
 {
+    class Beaver
+    {
+        public Beaver(int rowIndex, int colIndex, int branchesLeft)
+        {
+            RowIndex = rowIndex;
+            ColIndex = colIndex;
+            BranchesCollected = new Stack<char>();
+            BranchesLeft = branchesLeft;
+        }
+
+        public int RowIndex { get; set; }
+
+        public int ColIndex { get; set; }
+
+        public Stack<char> BranchesCollected { get; set; }
+
+        public int BranchesLeft { get; set; }
+
+        public void Move(string command, char[,] pond)
+        {
+            int size = pond.GetLength(0);
+            pond[RowIndex, ColIndex] = '-';
+
+            int newRowIndex = RowIndex;
+            int newColIndex = ColIndex;
+
+            switch (command)
+            {
+                case "up":
+                    newRowIndex--;
+                    break;
+                case "down":
+                    newRowIndex++;
+                    break;
+                case "left":
+                    newColIndex--;
+                    break;
+                case "right":
+                    newColIndex++;
+                    break;
+            }
+
+            if (newRowIndex < 0 || newRowIndex >= size ||
+                newColIndex < 0 || newColIndex >= size)
+            {
+                if (BranchesCollected.Any())
+                {
+                    BranchesCollected.Pop();
+                }
+
+                return;
+            }
+
+            RowIndex = newRowIndex;
+            ColIndex = newColIndex;
+
+            char symbol = pond[RowIndex, ColIndex];
+
+            pond[RowIndex, ColIndex] = '-';
+
+            if (char.IsLower(symbol))
+            {
+                BranchesCollected.Push(symbol);
+                BranchesLeft--;
+            }
+            else if (symbol == 'F')
+            {
+                Swim(pond, command);
+            }
+        }
+
+        public void Swim(char[,] pond, string command)
+        {
+            int maxIndex = pond.GetLength(0) - 1;
+
+            if (command == "up" && RowIndex == 0) command = "down";
+            else if (command == "down" && RowIndex == maxIndex) command = "up";
+            else if (command == "left" && ColIndex == 0) command = "right";
+            else if (command == "right" && ColIndex == maxIndex) command = "left";
+
+            switch (command)
+            {
+                case "up": RowIndex = 0; break;
+                case "down": RowIndex = maxIndex; break;
+                case "left": ColIndex = 0; break;
+                case "right": ColIndex = maxIndex; break;
+            }
+
+            pond[RowIndex, ColIndex] = '-';
+
+            char symbol = pond[RowIndex, ColIndex];
+
+            if (char.IsLower(symbol))
+            {
+                BranchesCollected.Push(symbol);
+                BranchesLeft--;
+            }
+            else if (symbol == 'F')
+            {
+                Swim(pond, command);
+            }
+        }
+    }
+
     internal class Program
     {
+
         static void Main(string[] args)
         {
-            int n = int.Parse(Console.ReadLine());
-            char[,] matrix = new char[n, n];
+            char[,] pond = ReadPondData(int.Parse(Console.ReadLine()));
+            int totalBranches = GetBranchesCount(pond);
+            (int beaverRow, int beaverCol) = GetBeaverLocation(pond);
 
-            int branchesCounter = 0;
+            Beaver beaver = new Beaver(beaverRow, beaverCol, totalBranches);
 
-            for (int row1 = 0; row1 < n; row1++)
+            string command;
+            while ((command = Console.ReadLine()) != "end" && beaver.BranchesLeft > 0)
+            {
+                beaver.Move(command, pond);
+            }
+
+            PrintResult(pond, beaver);
+        }
+
+        private static void PrintResult(char[,] pond, Beaver beaver)
+        {
+            pond[beaver.RowIndex, beaver.ColIndex] = 'B';
+
+            if (beaver.BranchesLeft == 0)
+            {
+                Console.WriteLine($"The beaver successfully collect " +
+                    $"{beaver.BranchesCollected.Count} wood branches: "
+                    + String.Join(", ", beaver.BranchesCollected
+                    .ToArray().Reverse()) + ".");
+            }
+            else
+            {
+                Console.WriteLine($"The beaver failed to collect every wood branch. " +
+                    $"There are {beaver.BranchesLeft} branches left.");
+            }
+
+            for (int row = 0; row < pond.GetLength(0); row++)
+            {
+                for (int col = 0; col < pond.GetLength(1); col++)
+                {
+                    Console.Write(pond[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static char[,] ReadPondData(int size)
+        {
+            char[,] pond = new char[size, size];
+
+            for (int row = 0; row < size; row++)
             {
                 string line = Console.ReadLine();
-                for (int col1 = 0; col1 < n; col1++)
+                for (int col = 0; col < size; col++)
                 {
-                    matrix[row1, col1] = line[col1];
-
-                    if (matrix[row1, col1] != '-' && 
-                        matrix[row1, col1] != 'B' &&
-                        matrix[row1, col1] != 'F')
-                    {
-                        branchesCounter++;
-                    }
+                    pond[row, col] = line[col];
                 }
             }
 
-            (int row, int col) = FindBeaverLocation(matrix);
+            return pond;
+        }
 
-            string command = Console.ReadLine();
-            while (command != "end")
+        static int GetBranchesCount(char[,] pond)
+        {
+            int count = 0;
+
+            foreach (var item in pond)
             {
-                if (command == "up")
+                if (char.IsLower(item))
                 {
-                    Move(matrix, row, col, row - 1, col);
+                    count++;
                 }
-                else if (command == "down")
-                {
-                    Move(matrix, row, col, row + 1, col);
-                }
-                else if (command == "left")
-                {
-                    Move(matrix, row, col, row, col - 1);
-                }
-                else if (command == "right")
-                {
-                    Move(matrix, row, col, row, col + 1);
-                }
-
-                command = Console.ReadLine();
             }
 
-            //for (int row = 0; row < n; row++)
-            //{
-            //    for (int col = 0; col < n; col++)
-            //    {
-            //        Console.Write(matrix[row, col]);
-            //    }
-
-            //    Console.WriteLine();
-            //}
-
+            return count;
         }
 
-        static void Move(char[,] matrix, int row, int col, int newRow, int newCol)
+        static (int row, int col) GetBeaverLocation(char[,] pond)
         {
-            matrix[row, col] = '-';
-            matrix[newRow, newCol] = 'B';
-        }
-
-        static (int row, int col) FindBeaverLocation(char[,] matrix)
-        {
-            for (int row = 0; row < matrix.GetLength(0); row++)
+            for (int row = 0; row < pond.GetLength(0); row++)
             {
-                for (int col = 0; col < matrix.GetLength(1); col++)
+                for (int col = 0; col < pond.GetLength(1); col++)
                 {
-                    if (matrix[row, col] == 'B')
+                    if (pond[row, col] == 'B')
                     {
                         return (row, col);
                     }
                 }
             }
 
-            throw new Exception();
+            throw new ArgumentException();
         }
     }
 }
