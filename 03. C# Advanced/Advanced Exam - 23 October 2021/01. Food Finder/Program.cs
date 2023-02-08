7fb0a28a88c818deedb2e1bd55d7cec1b8615541
Kristian <krisvl5000@gmail.com>
2023-02-08 09:55:10 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam - 23 October 2021/01. Food Finder/Program.cs
+++ 03. C# Advanced/Advanced Exam - 23 October 2021/01. Food Finder/Program.cs
@@ -6,7 +6,49 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            char[] vowelsArr = Console.ReadLine().Split().Select(char.Parse).ToArray();
+            char[] consonantsArr = Console.ReadLine().Split().Select(char.Parse).ToArray();
 
+            Queue<char> vowelsQueue = new Queue<char>(vowelsArr);
+            Stack<char> consonantsStack = new Stack<char>(consonantsArr);
+
+            List<string> completeWords = new List<string>();
+
+            var dict = new Dictionary<string, int>();
+            dict.Add("pear", 4);
+            dict.Add("flour", 5);
+            dict.Add("pork", 4);
+            dict.Add("olive", 5);
+
+            while (consonantsStack.Count > 0)
+            {
+                char vowel = vowelsQueue.Dequeue();
+                char consonant = consonantsStack.Pop();
+
+                foreach (var item in dict)
+                {
+                    string key = item.Key;
+                    if (key.Contains(vowel) || key.Contains(consonant))
+                    {
+                        dict[key]--;
+
+                        if (dict[key] == 0)
+                        {
+                            completeWords.Add(key);
+                            dict.Remove(key);
+                        }
+                    }
+                }
+
+                vowelsQueue.Enqueue(vowel);
+            }
+
+            Console.WriteLine($"Words found: {completeWords.Count}");
+
+            foreach (var item in completeWords)
+            {
+                Console.WriteLine(item);
+            }
 
         }
     }
