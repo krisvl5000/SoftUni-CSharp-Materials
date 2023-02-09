f1b89366c580b44ed9bd77a04231fc275ab76707
Kristian <krisvl5000@gmail.com>
2023-02-09 11:56:23 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam - 14 December 2022/02. Navy Battle/Program.cs
+++ 03. C# Advanced/Advanced Exam - 14 December 2022/02. Navy Battle/Program.cs
@@ -6,8 +6,112 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            char[,] matrix = GetMatrixData();
 
+            (int row, int col) = GetPosition(matrix);
 
+            int mines = 0;
+            int shipsDestroyed = 0;
+
+            matrix[row, col] = '-'; // might need to change later
+
+            while (true)
+            {
+                string command = Console.ReadLine();
+
+                switch (command)
+                {
+                    case "up":
+                        row--;
+                        break;
+                    case "down":
+                        row++;
+                        break;
+                    case "left":
+                        col--;
+                        break;
+                    case "right":
+                        col++;
+                        break;
+                }
+
+                if (matrix[row, col] == '*')
+                {
+                    matrix[row, col] = '-';
+                    mines++;
+
+                    if (mines == 3)
+                    {
+                        Console.WriteLine($"Mission failed, U-9 disappeared! " +
+                            $"Last known cordinates [{row}, {col}]!");
+                        break;
+                    }
+                }
+                else if (matrix[row, col] == 'C')
+                {
+                    matrix[row, col] = '-';
+                    shipsDestroyed++;
+
+                    if (shipsDestroyed == 3)
+                    {
+                        Console.WriteLine("Mission accomplished, U-9 has destroyed " +
+                            $"all battle cruisers of the enemy!");
+                        break;
+                    }
+                }
+            }
+
+            PrintLastPosition(matrix, row, col);
+
+        }
+
+        static void PrintLastPosition(char[,] matrix, int lastRow, int lastCol)
+        {
+            matrix[lastRow, lastCol] = 'S';
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col]);
+                }
+
+                Console.WriteLine();
+            }
+        }
+
+        static char[,] GetMatrixData()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                string line = Console.ReadLine();
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+
+            return matrix;
+        }
+
+        static (int, int) GetPosition(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 'S')
+                    {
+                        return (row, col);
+                    }
+                }
+            }
+
+            throw new ArgumentException();
         }
     }
 }
