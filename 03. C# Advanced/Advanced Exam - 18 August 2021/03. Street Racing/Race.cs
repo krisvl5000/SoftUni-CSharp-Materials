d9bf45b68ccea55e5147f8168239bf41fe7a444e
Kristian <krisvl5000@gmail.com>
2023-02-12 13:31:00 +0200

    Create Race.cs

---
+++ 03. C# Advanced/Advanced Exam - 18 August 2021/03. Street Racing/Race.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StreetRacing
+{
+    public class Race
+    {
+        public Race(string name, string type, int laps, int capacity, int maxHorsePower)
+        {
+            Name = name;
+            Type = type;
+            Laps = laps;
+            Capacity = capacity;
+            MaxHorsePower = maxHorsePower;
+            Participants = new List<Car>();
+        }
+
+        public List<Car> Participants { get; set; }
+
+        public string Name { get; set; }
+
+        public string Type { get; set; }
+
+        public int Laps { get; set; }
+
+        public int Capacity { get; set; }
+
+        public int MaxHorsePower { get; set; }
+
+        public int Count => Participants.Count;
+
+        public void Add(Car car)
+        {
+            if (!Participants.Any(x => x.LicensePlate == car.LicensePlate) &&
+                Participants.Count < Capacity && car.HorsePower <= MaxHorsePower)
+            {
+                Participants.Add(car);
+            }
+        }
+
+        public bool Remove(string licensePlate)
+        {
+            Car carToRemove = Participants
+                .FirstOrDefault(x => x.LicensePlate == licensePlate);
+
+            if (carToRemove != null)
+            {
+                Participants.Remove(carToRemove);
+                return true;
+            }
+
+            return false;
+        }
+
+        public Car FindParticipant(string licensePlate)
+        {
+            return Participants.FirstOrDefault(x => x.LicensePlate == licensePlate);
+        }
+
+        public Car GetMostPowerfulCar()
+        {
+            return Participants.OrderByDescending(x => x.HorsePower).FirstOrDefault();
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Race: {Name} - Type: {Type} (Laps: {Laps})");
+
+            foreach (var item in Participants)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
