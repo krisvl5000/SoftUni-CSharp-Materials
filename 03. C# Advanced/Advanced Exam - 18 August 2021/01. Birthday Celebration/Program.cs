2ea135b4e5c49d740d5f74aa6d4778926f09b745
Kristian <krisvl5000@gmail.com>
2023-02-12 13:10:52 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam - 18 August 2021/01. Birthday Celebration/Program.cs
+++ 03. C# Advanced/Advanced Exam - 18 August 2021/01. Birthday Celebration/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,8 +8,60 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            Queue<int> guestQueue = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
 
+            Stack<int> platesStack = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
 
+            int wastedFood = 0;
+
+            while (guestQueue.Count > 0 && platesStack.Count > 0)
+            {
+                int guest = guestQueue.Peek();
+                int plate = platesStack.Peek();
+
+                if (plate >= guest)
+                {
+                    wastedFood += plate - guest;
+                    guestQueue.Dequeue();
+                    platesStack.Pop();
+                }
+                else
+                {
+                    int lastPositiveValue = guest;
+                    int lastPlateValue = 0;
+
+                    while (guest > 0)
+                    {
+                        lastPlateValue = platesStack.Pop();
+                        guest -= lastPlateValue;
+
+                        if (guest > 0)
+                        {
+                            lastPositiveValue = guest;
+                        }
+
+                    }
+                    wastedFood += lastPlateValue - lastPositiveValue;
+                    guestQueue.Dequeue();
+                }
+            }
+
+            if (guestQueue.Count > 0)
+            {
+                Console.WriteLine($"Guests: {String.Join(" ", guestQueue)}");
+            }
+            else
+            {
+                Console.WriteLine($"Plates: {String.Join(" ", platesStack)}");
+            }
+
+            Console.WriteLine($"Wasted grams of food: {wastedFood}");
         }
     }
 }
