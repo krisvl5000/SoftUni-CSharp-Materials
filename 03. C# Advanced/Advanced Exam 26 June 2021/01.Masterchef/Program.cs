c63028988c874c09cbde112c6c336e094b9d1a8d
Kristian <krisvl5000@gmail.com>
2023-02-13 20:17:30 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam 26 June 2021/01.Masterchef/Program.cs
+++ 03. C# Advanced/Advanced Exam 26 June 2021/01.Masterchef/Program.cs
@@ -6,8 +6,84 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            Queue<int> ingredients = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
 
+            Stack<int> freshness = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
 
+            var dict = new Dictionary<string, int>();
+            dict.Add("Dipping Sauce", 150);
+            dict.Add("Green salad", 250);
+            dict.Add("Chocolate cake", 300);
+            dict.Add("Lobster", 400);
+
+            var mealsMade = new Dictionary<string, int>();
+
+            while (ingredients.Count > 0 && freshness.Count > 0)
+            {
+                int totalFreshness = ingredients.Peek() * freshness.Peek();
+                bool dishWasMade = false;
+
+                foreach (var item in dict)
+                {
+                    if (totalFreshness == item.Value)
+                    {
+                        if (!mealsMade.ContainsKey(item.Key))
+                        {
+                            mealsMade.Add(item.Key, 0);
+                        }
+
+                        mealsMade[item.Key]++;
+                        dishWasMade = true;
+                    }
+                }
+
+                if (!dishWasMade)
+                {
+                    freshness.Pop();
+                    int newIngredient = ingredients.Dequeue() + 5;
+                    ingredients.Enqueue(newIngredient);
+                }
+            }
+
+            int totalMealsMade = 0;
+
+            foreach (var item in mealsMade)
+            {
+                totalMealsMade += item.Value;
+            }
+
+            if (totalMealsMade >= 4 && mealsMade.Count == 4)
+            {
+                Console.WriteLine($"Applause! The judges are fascinated by " +
+                    $"your dishes!");
+            }
+            else
+            {
+                Console.WriteLine("You were voted off. Better luck next year.");
+            }
+
+            int ingredientsLeft = 0;
+
+            if (ingredients.Count > 0)
+            {
+                while (ingredients.Count != 0)
+                {
+                    ingredientsLeft += ingredients.Dequeue();
+                }
+            }
+
+            mealsMade = (Dictionary<string, int>)mealsMade.OrderByDescending(x => x.Key);
+
+            foreach (var item in mealsMade)
+            {
+                Console.WriteLine($"# {item.Key}--> {item.Value}");
+            }
         }
     }
 }
