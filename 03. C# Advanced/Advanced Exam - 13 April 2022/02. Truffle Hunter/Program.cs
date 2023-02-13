3272339e0585c23ea576eeb033de7143ea8c4a22
Kristian <krisvl5000@gmail.com>
2023-02-13 11:13:16 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam - 13 April 2022/02. Truffle Hunter/Program.cs
+++ 03. C# Advanced/Advanced Exam - 13 April 2022/02. Truffle Hunter/Program.cs
@@ -6,8 +6,77 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            int n = int.Parse(Console.ReadLine());
+
+            char[,] matrix = GetMatrix(n);
+
+            int row = 0;
+            int col = 0;
+
+            while (true)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string command = input[0];
+
+                if (command == "Collect")
+                {
+                    row = int.Parse(input[1]);
+                    col = int.Parse(input[2]);
 
 
+                }
+                else if (command == "Wild_Boar")
+                {
+                    row = int.Parse(input[1]);
+                    col = int.Parse(input[2]);
+                    string direction = input[3];
+
+                    switch (direction)
+                    {
+                        case "up":
+
+                            break;
+                        case "down":
+
+                            break;
+                        case "left":
+
+                            break;
+                        case "right":
+
+                            break;
+                    }
+                }
+                else if (command == "Stop")
+                {
+                    break;
+                }
+
+            }
+
+        }
+
+        static char[,] GetMatrix(int n)
+        {
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] line = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+
+            return matrix;
         }
     }
 }
