620a20c328d03354c8b6f829653826014145e66f
Kristian <krisvl5000@gmail.com>
2023-02-14 14:07:40 +0200

    Update Ingredient.cs

--- 03. C# Advanced/Advanced Exam 14 April 2021/03. Cocktail Party/Ingredient.cs
+++ 03. C# Advanced/Advanced Exam 14 April 2021/03. Cocktail Party/Ingredient.cs
@@ -4,9 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03._Cocktail_Party
+namespace CocktailParty
 {
-    internal class Ingredient
+    public class Ingredient
     {
+        public Ingredient(string name, int alcohol, int quantity)
+        {
+            Name = name;
+            Alcohol = alcohol;
+            Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+
+        public int Alcohol { get; set; }
+
+        public int Quantity { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Ingredient: {Name}");
+            sb.AppendLine($"Quantity: {Quantity}");
+            sb.AppendLine($"Alcohol: {Alcohol}");
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
