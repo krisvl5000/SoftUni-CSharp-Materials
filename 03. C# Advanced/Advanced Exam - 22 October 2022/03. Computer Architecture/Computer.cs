1dd581bda70da35a25a81210a69bf2b8fae0d60a
Kristian <krisvl5000@gmail.com>
2023-02-14 21:42:29 +0200

    Update Computer.cs

--- 03. C# Advanced/Advanced Exam - 22 October 2022/03. Computer Architecture/Computer.cs
+++ 03. C# Advanced/Advanced Exam - 22 October 2022/03. Computer Architecture/Computer.cs
@@ -4,9 +4,68 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03._Computer_Architecture
+namespace ComputerArchitecture
 {
-    internal class Computer
+    public class Computer
     {
+        public Computer(string model, int capacity)
+        {
+            Model = model;
+            Capacity = capacity;
+            Multiprocessor = new List<CPU>();
+        }
+
+        public string Model { get; set; }
+
+        public int Capacity { get; set; }
+
+        public List<CPU> Multiprocessor { get; set; }
+
+        public int Count => Multiprocessor.Count;
+
+        public void Add(CPU cpu)
+        {
+            if (Multiprocessor.Count < Capacity)
+            {
+                Multiprocessor.Add(cpu);
+            }
+        }
+
+        public bool Remove(string brand)
+        {
+            CPU cpuToRemove = Multiprocessor.FirstOrDefault(x => x.Brand == brand);
+
+            if (cpuToRemove != null)
+            {
+                Multiprocessor.Remove(cpuToRemove);
+                return true;
+            }
+
+            return false;
+        }
+
+        public CPU MostPowerful()
+        {
+            return Multiprocessor.OrderByDescending(x => x.Frequency).FirstOrDefault();
+        }
+
+        public CPU GetCPU(string brand)
+        {
+            return Multiprocessor.FirstOrDefault(x => x.Brand == brand);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"CPUs int the Computer {Model}:");
+
+            foreach (var item in Multiprocessor)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
