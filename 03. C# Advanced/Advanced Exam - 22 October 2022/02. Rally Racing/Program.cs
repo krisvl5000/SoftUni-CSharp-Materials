91e0b1f827800bc969eb2b005727d2f5b1f74ccc
Kristian <krisvl5000@gmail.com>
2023-02-14 19:04:34 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam - 22 October 2022/02. Rally Racing/Program.cs
+++ 03. C# Advanced/Advanced Exam - 22 October 2022/02. Rally Racing/Program.cs
@@ -6,195 +6,121 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
-            // Creating the matrix
-
             int n = int.Parse(Console.ReadLine());
             string racingNumber = Console.ReadLine();
 
+            char[,] matrix = GetMatrix(n);
+
+            int row = 0;
+            int col = 0;
+
+            int kmPassed = 0;
+
+            bool carFinished = false;
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "End")
+                {
+                    break;
+                }
+
+                kmPassed += 10;
+
+                switch (input)
+                {
+                    case "up":
+                        row--;
+                        break;
+                    case "down":
+                        row++;
+                        break;
+                    case "left":
+                        col--;
+                        break;
+                    case "right":
+                        col++;
+                        break;
+                }
+
+                if (matrix[row, col] == 'T')
+                {
+                    matrix[row, col] = '.';
+                    kmPassed += 20;
+
+                    (row, col) = GetTLocation(matrix);
+                    matrix[row, col] = '.';
+                }
+                else if (matrix[row, col] == 'F')
+                {
+                    carFinished = true;
+                    break;
+                }
+            }
+
+            if (carFinished)
+            {
+                Console.WriteLine($"Racing car {racingNumber} finished the stage!");
+            }
+            else
+            {
+                Console.WriteLine($"Racing car {racingNumber} DNF.");
+            }
+
+            Console.WriteLine($"Distance covered {kmPassed} km.");
+            matrix[row, col] = 'C';
+            PrintMatrix(matrix);
+        }
+
+        static char[,] GetMatrix(int n)
+        {
             char[,] matrix = new char[n, n];
 
             for (int row = 0; row < n; row++)
             {
-                string[] raceRouteRaw = Console.ReadLine()
-                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
-
-                string raceRoute = String.Join("", raceRouteRaw);
+                char[] line = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
 
                 for (int col = 0; col < n; col++)
                 {
-                    matrix[row, col] = raceRoute[col];
+                    matrix[row, col] = line[col];
                 }
             }
 
-            // Finding the tunnel and finish line locations
+            return matrix;
+        }
 
-            int firstLocationRow = 0;
-            int firstLocationCol = 0;
-
-            int secondLocationRow = 0;
-            int secondLocationCol = 0;
-
-            bool firstLocationHasBeenFound = false;
-
-            int fRow = 0;
-            int fCol = 0;
-
-            for (int row = 0; row < n; row++)
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                for (int col = 0; col < n; col++)
+                for (int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    if (matrix[row, col] == 'T')
-                    {
-                        if (!firstLocationHasBeenFound)
-                        {
-                            firstLocationRow = row;
-                            firstLocationCol = col;
-
-                            firstLocationHasBeenFound = true;
-                        }
-                        else
-                        {
-                            secondLocationRow = row;
-                            secondLocationCol = col;
-                        }
-                    }
-                    else if (matrix[row, col] == 'F')
-                    {
-                        fRow = row;
-                        fCol = col;
-                    }
-                }
-            }
-
-            // Moving the car
-
-            string[] input = Console.ReadLine()
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
-
-            int kilometersTravelled = 0;
-
-            bool carFinished = false;
-            bool disqualified = false;
-
-            int lastPositionRow = 0;
-            int lastPositionCol = 0;
-
-            while (input[0] != "End")
-            {
-                string direction = input[0];
-
-                for (int row = lastPositionRow; row < n; row++)
-                {
-                    for (int col = lastPositionCol; col < n; col++)
-                    {
-                        if (direction == "left")
-                        {
-                            col--;
-                        }
-                        else if (direction == "right")
-                        {
-                            col++;
-                        }
-                        else if (direction == "up")
-                        {
-                            row--;
-                        }
-                        else if (direction == "down")
-                        {
-                            row++;
-                        }
-
-                        if (matrix[row, col] == 'T')
-                        {
-                            matrix[row, col] = '.'; // removed the "T"s from the matrix
-                            kilometersTravelled += 30;
-
-                            if (row == firstLocationRow && col == firstLocationCol)
-                            {
-                                row = secondLocationRow;
-                                col = secondLocationCol;
-
-                                lastPositionRow = row;
-                                lastPositionCol = col;
-                                break;
-                            }
-                            else if (row == secondLocationRow && col == secondLocationCol)
-                            {
-                                row = firstLocationRow;
-                                col = firstLocationCol;
-
-                                lastPositionRow = row;
-                                lastPositionCol = col;
-                                break;
-                            }
-
-                        }
-                        else if (matrix[row, col] == 'F')
-                        {
-                            kilometersTravelled += 10;
-
-                            Console.WriteLine($"Racing car {racingNumber} finished " +
-                                $"the stage!");
-
-                            carFinished = true;
-
-                            lastPositionRow = row;
-                            lastPositionCol = col;
-                            break;
-                        }
-                        else
-                        {
-                            kilometersTravelled += 10;
-                            lastPositionRow = row;
-                            lastPositionCol = col;
-                            break;
-                        }
-
-                        break; // added break to make col unreachable
-                    }
-                    break;
-                }
-
-                if (carFinished)
-                {
-                    break;
-                }
-
-                input = Console.ReadLine()
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
-            }
-
-            if (!carFinished)
-            {
-                disqualified = true;
-                Console.WriteLine($"Racing car {racingNumber} DNF.");
-            }
-
-            Console.WriteLine($"Distance covered {kilometersTravelled} km.");
-
-            for (int row = 0; row < n; row++)
-            {
-                for (int col = 0; col < n; col++)
-                {
-                    if (row == lastPositionRow && col == lastPositionCol)
-                    {
-                        Console.Write("C");
-                    }
-                    else
-                    {
-                        if (disqualified && row == fRow && col == fCol)
-                        {
-                            Console.Write("F");
-                        }
-                        else
-                        {
-                            Console.Write(".");
-                        }
-                    }
+                    Console.Write(matrix[row, col]);
                 }
 
                 Console.WriteLine();
             }
+        }
+
+        static (int row, int col) GetTLocation(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (matrix[row, col] == 'T')
+                    {
+                        return (row, col);
+                    }
+                }
+            }
+
+            throw new ArgumentException();
         }
     }
 }
