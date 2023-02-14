df54bad37bd64702e914edba3c900794a8020ffe
Kristian <krisvl5000@gmail.com>
2023-02-14 09:46:17 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam 26 June 2021/01.Masterchef/Program.cs
+++ 03. C# Advanced/Advanced Exam 26 June 2021/01.Masterchef/Program.cs
@@ -1,114 +1,86 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 
-namespace _01._Hello_Softuni
+namespace _01.Masterchef
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Queue<int> ingredientsQueue = new Queue<int>(Console.ReadLine()
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            Queue<int> ingredients = new Queue<int>(Console.ReadLine()
+                .Split()
                 .Select(int.Parse)
                 .ToArray());
 
-            Stack<int> freshnessStack = new Stack<int>(Console.ReadLine()
-                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            Stack<int> freshness = new Stack<int>(Console.ReadLine()
+                .Split()
                 .Select(int.Parse)
                 .ToArray());
 
-            var dict = new Dictionary<string, int>();
-            dict.Add("Dipping sauce", 150);
-            dict.Add("Green salad", 250);
-            dict.Add("Chocolate cake", 300);
-            dict.Add("Lobster", 400);
+            Dictionary<int, string> listOfMeals = GetListOfMeals();
 
-            var mealsMade = new Dictionary<string, int>();
+            var cookedDishes = new SortedDictionary<string, int>();
 
-            while (ingredientsQueue.Count > 0 && freshnessStack.Count > 0)
+            while (ingredients.Any() && freshness.Any())
             {
-                if (ingredientsQueue.Peek() == 0)
+                int currentIngredient = ingredients.Dequeue();
+
+                if (currentIngredient == 0)
                 {
-                    ingredientsQueue.Dequeue();
+                    continue;
                 }
 
-                if (ingredientsQueue.Count == 0)
-                {
-                    break;
-                }
+                int currentFreshness = freshness.Pop();
+                int product = currentIngredient * currentFreshness;
 
-                if (freshnessStack.Count == 0)
+                if (listOfMeals.ContainsKey(product))
                 {
-                    break;
-                }
+                    string meal = listOfMeals[product];
 
-                int totalFreshness = ingredientsQueue.Peek() * freshnessStack.Peek();
-                bool dishWasMade = false;
-
-                foreach (var item in dict)
-                {
-                    if (totalFreshness == item.Value)
+                    if (!cookedDishes.ContainsKey(meal))
                     {
-                        if (!mealsMade.ContainsKey(item.Key))
-                        {
-                            mealsMade.Add(item.Key, 0);
-                        }
-
-                        mealsMade[item.Key]++;
-                        dishWasMade = true;
-
-                        ingredientsQueue.Dequeue();
-                        freshnessStack.Pop();
-                        break;
+                        cookedDishes.Add(meal, 0);
                     }
-                }
 
-                if (!dishWasMade)
+                    cookedDishes[meal]++;
+                }
+                else
                 {
-                    freshnessStack.Pop();
-                    int newIngredient = ingredientsQueue.Dequeue() + 5;
-                    ingredientsQueue.Enqueue(newIngredient);
+                    ingredients.Enqueue(currentIngredient + 5);
                 }
             }
 
-            int totalMealsMade = 0;
-
-            foreach (var item in mealsMade)
+            if (cookedDishes.Count >= 4)
             {
-                totalMealsMade += item.Value;
-            }
-
-            if (totalMealsMade >= 4 && mealsMade.Count == 4)
-            {
-                Console.WriteLine($"Applause! The judges are fascinated by " +
-                    $"your dishes!");
+                Console.WriteLine($"Applause! The judges are fascinated " +
+                    $"by your dishes!");
             }
             else
             {
-                Console.WriteLine("You were voted off. Better luck next year.");
+                Console.WriteLine($"You were voted off. Better luck next year.");
             }
 
-            int ingredientsLeft = 0;
-
-            if (ingredientsQueue.Count > 0)
+            if (ingredients.Any())
             {
-                while (ingredientsQueue.Count != 0)
+                Console.WriteLine($"Ingredients left: {ingredients.Sum()}");
+            }
+            if (cookedDishes.Count > 0)
+            {
+                foreach (var dish in cookedDishes)
                 {
-                    ingredientsLeft += ingredientsQueue.Dequeue();
+                    Console.WriteLine($" # {dish.Key} --> {dish.Value}");
                 }
-
-                Console.WriteLine($"Ingredients left: {ingredientsLeft}");
-            }
-
-            var orderedMealsMade = mealsMade
-                .OrderByDescending(x => x.Value)
-                .ThenBy(x => x.Key);
-
-            foreach (var item in orderedMealsMade)
-            {
-                Console.WriteLine($" # {item.Key} --> {item.Value}");
             }
         }
+
+        static Dictionary<int, string> GetListOfMeals()
+            => new Dictionary<int, string>()
+            {
+                { 150, "Dipping sauce" },
+                { 250, "Green salad" },
+                { 300, "Chocolate cake" },
+                { 400,  "Lobster" }
+            };
     }
 }
