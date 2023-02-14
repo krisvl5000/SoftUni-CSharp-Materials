0122d184388ddb66c7d3c204f809a6f487dcba09
Kristian <krisvl5000@gmail.com>
2023-02-14 11:50:52 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam 14 April 2021/01.Warm Winter/Program.cs
+++ 03. C# Advanced/Advanced Exam 14 April 2021/01.Warm Winter/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -6,7 +8,53 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
+            Stack<int> hatsStack = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
 
+            Queue<int> scarfsQueue = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            List<int> sets = new List<int>();
+
+            while (true)
+            {
+                if (hatsStack.Count == 0)
+                {
+                    break;
+                }
+                if (scarfsQueue.Count == 0)
+                {
+                    break;
+                }
+
+                if (hatsStack.Peek() > scarfsQueue.Peek())
+                {
+                    int newSet = hatsStack.Peek() + scarfsQueue.Peek();
+                    sets.Add(newSet);
+
+                    hatsStack.Pop();
+                    scarfsQueue.Dequeue();
+                    continue;
+                }
+                else if (scarfsQueue.Peek() > hatsStack.Peek())
+                {
+                    hatsStack.Pop();
+                    continue;
+                }
+                else if (hatsStack.Peek() == scarfsQueue.Peek())
+                {
+                    scarfsQueue.Dequeue();
+                    hatsStack.Push(hatsStack.Pop() + 1);
+                    continue;
+                }
+            }
+
+            Console.WriteLine($"The most expensive set is: {sets.Max()}");
+            Console.WriteLine(String.Join(" ", sets));
 
         }
     }
