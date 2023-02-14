0c78a3cf05e30ab904ae14ffcddc05f234c2aeac
Kristian <krisvl5000@gmail.com>
2023-02-14 21:42:34 +0200

    Update CPU.cs

--- 03. C# Advanced/Advanced Exam - 22 October 2022/03. Computer Architecture/CPU.cs
+++ 03. C# Advanced/Advanced Exam - 22 October 2022/03. Computer Architecture/CPU.cs
@@ -4,9 +4,32 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03._Computer_Architecture
+namespace ComputerArchitecture
 {
-    internal class CPU
+    public class CPU
     {
+        public CPU(string brand, int cores, double frequency)
+        {
+            Brand = brand;
+            Cores = cores;
+            Frequency = frequency;
+        }
+
+        public string Brand { get; set; }
+
+        public int Cores { get; set; }
+
+        public double Frequency { get; set; }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{Brand} CPU:");
+            sb.AppendLine($"Cores: {Cores}");
+            sb.AppendLine($"Frequency: {Frequency:F1} GHz");
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
