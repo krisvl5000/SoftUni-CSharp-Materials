0edb4f0479f6f4ba566c3f5aa3fcc11763f321ab
Kristian <krisvl5000@gmail.com>
2023-02-14 11:14:15 +0200

    Update SkiRental.cs

--- 03. C# Advanced/Advanced Exam 26 June 2021/03. Ski Rental/SkiRental.cs
+++ 03. C# Advanced/Advanced Exam 26 June 2021/03. Ski Rental/SkiRental.cs
@@ -4,9 +4,75 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace _03._Ski_Rental
+namespace SkiRental
 {
-    internal class SkiRental
+    public class SkiRental
     {
+        private List<Ski> data;
+
+        public SkiRental(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            data = new List<Ski>();
+        }
+       
+        public string Name { get; set; }
+
+        public int Capacity { get; set; }
+
+        public List<Ski> Data => data;
+
+        public int Count => Data.Count;
+
+        public void Add(Ski ski)
+        {
+            if (Data.Count < Capacity)
+            {
+                Data.Add(ski);
+            }
+        }
+
+        public bool Remove(string manufacturer, string model)
+        {
+            Ski skiToRemove = Data.FirstOrDefault(x => x.Manufacturer == manufacturer
+            && x.Model == model);
+
+            if (skiToRemove != null)
+            {
+                Data.Remove(skiToRemove);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public Ski GetNewestSki()
+        {
+            Ski newestSki = Data.OrderByDescending(x => x.Year).FirstOrDefault();
+            return newestSki;
+        }
+
+        public Ski GetSki(string manufacturer, string model)
+        {
+            return Data.FirstOrDefault(x => x.Manufacturer == manufacturer
+            && x.Model == model);
+        }
+
+        public string GetStatistics()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"The skis stored in {Name}:");
+
+            foreach (var ski in Data)
+            {
+                sb.AppendLine(ski.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
     }
 }
