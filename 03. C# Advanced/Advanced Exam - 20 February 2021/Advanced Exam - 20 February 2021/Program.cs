f3709de52d445ed1aa1e2b52b604e67ba45d784d
Kristian <krisvl5000@gmail.com>
2023-02-15 11:14:15 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam - 20 February 2021/Advanced Exam - 20 February 2021/Program.cs
+++ 03. C# Advanced/Advanced Exam - 20 February 2021/Advanced Exam - 20 February 2021/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
@@ -7,56 +9,33 @@ namespace _01._Hello_Softuni
         static void Main(string[] args)
         {
             int numberOfWaves = int.Parse(Console.ReadLine());
-
-            Queue<int> plates = new Queue<int>(Console.ReadLine()
-                .Split()
-                .Select(int.Parse)
-                .ToArray());
-
+            Queue<int> plates = new Queue<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
             Stack<int> orcs = new Stack<int>();
             int plate = plates.Peek();
 
             for (int wave = 1; wave <= numberOfWaves; wave++)
             {
-                orcs = new Stack<int>(Console.ReadLine()
-                    .Split()
-                    .Select(int.Parse)
-                    .ToArray());
-
+                orcs = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToArray());
                 if (wave % 3 == 0)
-                {
                     plates.Enqueue(int.Parse(Console.ReadLine()));
-                }
 
                 int orc = orcs.Peek();
-
                 while (orcs.Any() && plates.Any())
                 {
                     int damage = Math.Min(plate, orc);
                     plate -= damage;
                     orc -= damage;
-
                     if (orc == 0)
                     {
                         orcs.Pop();
-
-                        if (orcs.Any())
-                        {
-                            orc = orcs.Peek();
-                        }
+                        if (orcs.Any()) orc = orcs.Peek();
                     }
-
                     if (plate == 0)
                     {
                         plates.Dequeue();
-
-                        if (plates.Any())
-                        {
-                            plate = plates.Peek();
-                        }
+                        if (plates.Any()) plate = plates.Peek();
                     }
                 }
-
                 if (!plates.Any())
                 {
                     orcs.Pop();
@@ -67,15 +46,16 @@ namespace _01._Hello_Softuni
 
             if (plates.Any())
             {
+                List<int> platesLeft = new List<int>(plates);
+                platesLeft[0] = plate;
                 Console.WriteLine("The people successfully repulsed the orc's attack.");
-                Console.WriteLine($"Plates left: {string.Join(", ", plates)}");
+                Console.WriteLine($"Plates left: {string.Join(", ", platesLeft)}");
             }
             else
             {
                 Console.WriteLine("The orcs successfully destroyed the Gondor's defense.");
                 Console.WriteLine($"Orcs left: {string.Join(", ", orcs)}");
             }
-
         }
     }
 }
