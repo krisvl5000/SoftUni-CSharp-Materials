3ccf7009fac7bb474bab305c5a96fcab83a88e50
Kristian <krisvl5000@gmail.com>
2023-02-15 11:11:27 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam - 20 February 2021/Advanced Exam - 20 February 2021/Program.cs
+++ 03. C# Advanced/Advanced Exam - 20 February 2021/Advanced Exam - 20 February 2021/Program.cs
@@ -1,43 +1,62 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 
-namespace _01._The_Fight_for_Gondor
+namespace _01._Hello_Softuni
 {
     internal class Program
     {
-        static void Main()
+        static void Main(string[] args)
         {
             int numberOfWaves = int.Parse(Console.ReadLine());
+
             Queue<int> plates = new Queue<int>(Console.ReadLine()
-                .Split().Select(int.Parse).ToArray());
+                .Split()
+                .Select(int.Parse)
+                .ToArray());
+
             Stack<int> orcs = new Stack<int>();
             int plate = plates.Peek();
 
             for (int wave = 1; wave <= numberOfWaves; wave++)
             {
                 orcs = new Stack<int>(Console.ReadLine()
-                    .Split().Select(int.Parse).ToArray());
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray());
+
                 if (wave % 3 == 0)
+                {
                     plates.Enqueue(int.Parse(Console.ReadLine()));
+                }
 
                 int orc = orcs.Peek();
+
                 while (orcs.Any() && plates.Any())
                 {
                     int damage = Math.Min(plate, orc);
                     plate -= damage;
                     orc -= damage;
+
                     if (orc == 0)
                     {
                         orcs.Pop();
-                        if (orcs.Any()) orc = orcs.Peek();
+
+                        if (orcs.Any())
+                        {
+                            orc = orcs.Peek();
+                        }
                     }
+
                     if (plate == 0)
                     {
                         plates.Dequeue();
-                        if (plates.Any()) plate = plates.Peek();
+
+                        if (plates.Any())
+                        {
+                            plate = plates.Peek();
+                        }
                     }
                 }
+
                 if (!plates.Any())
                 {
                     orcs.Pop();
@@ -48,16 +67,15 @@ namespace _01._The_Fight_for_Gondor
 
             if (plates.Any())
             {
-                List<int> platesLeft = new List<int>(plates);
-                platesLeft[0] = plate;
                 Console.WriteLine("The people successfully repulsed the orc's attack.");
-                Console.WriteLine($"Plates left: {string.Join(", ", platesLeft)}");
+                Console.WriteLine($"Plates left: {string.Join(", ", plates)}");
             }
             else
             {
                 Console.WriteLine("The orcs successfully destroyed the Gondor's defense.");
                 Console.WriteLine($"Orcs left: {string.Join(", ", orcs)}");
             }
+
         }
     }
 }
