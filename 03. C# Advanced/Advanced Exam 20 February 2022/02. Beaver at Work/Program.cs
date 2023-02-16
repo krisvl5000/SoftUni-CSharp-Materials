e1e0ec09178cd798dec42c18252233c144fda26b
Kristian <krisvl5000@gmail.com>
2023-02-16 11:27:05 +0200

    Update Program.cs

--- 03. C# Advanced/Advanced Exam 20 February 2022/02. Beaver at Work/Program.cs
+++ 03. C# Advanced/Advanced Exam 20 February 2022/02. Beaver at Work/Program.cs
@@ -1,200 +1,43 @@
 ﻿using System;
-using System.Linq;
-using System.Collections.Generic;
 
 namespace _01._Hello_Softuni
 {
-    class Beaver
-    {
-        public Beaver(int rowIndex, int colIndex, int branchesLeft)
-        {
-            RowIndex = rowIndex;
-            ColIndex = colIndex;
-            BranchesCollected = new Stack<char>();
-            BranchesLeft = branchesLeft;
-        }
-
-        public int RowIndex { get; set; }
-
-        public int ColIndex { get; set; }
-
-        public Stack<char> BranchesCollected { get; set; }
-
-        public int BranchesLeft { get; set; }
-
-        public void Move(string command, char[,] pond)
-        {
-            int size = pond.GetLength(0);
-            pond[RowIndex, ColIndex] = '-';
-
-            int newRowIndex = RowIndex;
-            int newColIndex = ColIndex;
-
-            switch (command)
-            {
-                case "up":
-                    newRowIndex--;
-                    break;
-                case "down":
-                    newRowIndex++;
-                    break;
-                case "left":
-                    newColIndex--;
-                    break;
-                case "right":
-                    newColIndex++;
-                    break;
-            }
-
-            if (newRowIndex < 0 || newRowIndex >= size ||
-                newColIndex < 0 || newColIndex >= size)
-            {
-                if (BranchesCollected.Any())
-                {
-                    BranchesCollected.Pop();
-                }
-
-                return;
-            }
-
-            RowIndex = newRowIndex;
-            ColIndex = newColIndex;
-
-            char symbol = pond[RowIndex, ColIndex];
-
-            pond[RowIndex, ColIndex] = '-';
-
-            if (char.IsLower(symbol))
-            {
-                BranchesCollected.Push(symbol);
-                BranchesLeft--;
-            }
-            else if (symbol == 'F')
-            {
-                Swim(pond, command);
-            }
-        }
-
-        public void Swim(char[,] pond, string command)
-        {
-            int maxIndex = pond.GetLength(0) - 1;
-
-            if (command == "up" && RowIndex == 0) command = "down";
-            else if (command == "down" && RowIndex == maxIndex) command = "up";
-            else if (command == "left" && ColIndex == 0) command = "right";
-            else if (command == "right" && ColIndex == maxIndex) command = "left";
-
-            switch (command)
-            {
-                case "up": RowIndex = 0; break;
-                case "down": RowIndex = maxIndex; break;
-                case "left": ColIndex = 0; break;
-                case "right": ColIndex = maxIndex; break;
-            }
-
-            pond[RowIndex, ColIndex] = '-';
-
-            char symbol = pond[RowIndex, ColIndex];
-
-            if (char.IsLower(symbol))
-            {
-                BranchesCollected.Push(symbol);
-                BranchesLeft--;
-            }
-            else if (symbol == 'F')
-            {
-                Swim(pond, command);
-            }
-        }
-    }
-
     internal class Program
     {
-
         static void Main(string[] args)
         {
-            char[,] pond = ReadPondData(int.Parse(Console.ReadLine()));
-            int totalBranches = GetBranchesCount(pond);
-            (int beaverRow, int beaverCol) = GetBeaverLocation(pond);
+            int n = int.Parse(Console.ReadLine());
 
-            Beaver beaver = new Beaver(beaverRow, beaverCol, totalBranches);
+            char[,] matrix = GetMatrix(n);
 
-            string command;
-            while ((command = Console.ReadLine()) != "end" && beaver.BranchesLeft > 0)
-            {
-                beaver.Move(command, pond);
-            }
+            (int row, int col) = GetBeaverLocation(matrix);
 
-            PrintResult(pond, beaver);
+            int totalbranches = GetBranchesNumber(matrix);
+
         }
 
-        private static void PrintResult(char[,] pond, Beaver beaver)
+        private static int GetBranchesNumber(char[,] matrix)
         {
-            pond[beaver.RowIndex, beaver.ColIndex] = 'B';
+            int counter = 0;
 
-            if (beaver.BranchesLeft == 0)
+            for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                Console.WriteLine($"The Beaver successfully collect " +
-                    $"{beaver.BranchesCollected.Count} wood branches: "
-                    + String.Join(", ", beaver.BranchesCollected
-                    .ToArray().Reverse()) + ".");
-            }
-            else
-            {
-                Console.WriteLine($"The Beaver failed to collect every wood branch. " +
-                    $"There are {beaver.BranchesLeft} branches left.");
-            }
-
-            for (int row = 0; row < pond.GetLength(0); row++)
-            {
-                for (int col = 0; col < pond.GetLength(1); col++)
+                for (int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    Console.Write($"{pond[row, col]} ");
-                }
-
-                Console.WriteLine();
-            }
-        }
-
-        static char[,] ReadPondData(int size)
-        {
-            char[,] pond = new char[size, size];
-
-            for (int row = 0; row < size; row++)
-            {
-                char[] line = Console.ReadLine().ToCharArray()
-                    .Where(x => x != ' ').ToArray();
-                for (int col = 0; col < size; col++)
-                {
-                    pond[row, col] = line[col];
+                    if (matrix[row, col] != 'F' &&
+                        matrix[row, col] != 'B' &&
+                        )
                 }
             }
-
-            return pond;
         }
 
-        static int GetBranchesCount(char[,] pond)
+        private static (int row, int col) GetBeaverLocation(char[,] matrix)
         {
-            int count = 0;
-
-            foreach (var item in pond)
+            for (int row = 0; row < matrix.GetLength(0); row++)
             {
-                if (char.IsLower(item))
+                for (int col = 0; col < matrix.GetLength(1); col++)
                 {
-                    count++;
-                }
-            }
-
-            return count;
-        }
-
-        static (int row, int col) GetBeaverLocation(char[,] pond)
-        {
-            for (int row = 0; row < pond.GetLength(0); row++)
-            {
-                for (int col = 0; col < pond.GetLength(1); col++)
-                {
-                    if (pond[row, col] == 'B')
+                    if (matrix[row, col] == 'B')
                     {
                         return (row, col);
                     }
@@ -202,6 +45,39 @@ namespace _01._Hello_Softuni
             }
 
             throw new ArgumentException();
+        }
+
+        static char[,] GetMatrix(int n)
+        {
+            char[,] matrix = new char[n, n];
+
+            for (int row = 0; row < n; row++)
+            {
+                char[] line = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = line[col];
+                }
+            }
+
+            return matrix;
+        }
+
+        static void PrintMatrix(char[,] matrix)
+        {
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    Console.Write(matrix[row, col] + " ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }
