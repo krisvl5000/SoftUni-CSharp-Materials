00a52cc16816969c20a3dfb0e671ff50e276f725
Kristian <krisvl5000@gmail.com>
2023-02-17 19:16:19 +0200

    Update UsersService.cs

--- 04. C# OOP/SOLID Principles/SOLID Principles/UsersService.cs
+++ 04. C# OOP/SOLID Principles/SOLID Principles/UsersService.cs
@@ -4,9 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SOLID_Principles
+namespace Solid
 {
-    internal class UsersService
+    public class UsersService
     {
+        public void Register(string email, string password)
+        {
+            {
+                if (!IsEmailValid(email))
+                {
+                    throw new ArgumentException("Email is not valid.");
+                }
+                if (!IsPasswordValid(password))
+                {
+                    throw new ArgumentException("Password is not valid.");
+                }
+            }
+        }
+
+        private bool IsPasswordValid(string password)
+        {
+            // 500 lines of code
+            // check if password is valid as per our reqs
+            // check in online databases if password has been compromised
+            return true;
+        }
+
+        private bool IsEmailValid(string email)
+        {
+            // 50 lines of email validation
+            return true;
+        }
     }
 }
