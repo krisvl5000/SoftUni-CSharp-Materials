8dc1d9024b6a1f5d9f6865f0f905e7c5f8111580
Kristian <krisvl5000@gmail.com>
2023-02-20 23:05:52 +0200

    Update Private.cs

--- 04. C# OOP/Interfaces and Abstraction Exercise/07. Military Elite/Models/Private.cs
+++ 04. C# OOP/Interfaces and Abstraction Exercise/07. Military Elite/Models/Private.cs
@@ -8,6 +8,15 @@ namespace MilitaryElite
 {
     public class Private : Soldier, IPrivate
     {
+        public Private( string id, string firstName, string lastName, decimal salary)
+            : base(id, firstName, lastName)
+        {
+            Salary = salary;
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
         public decimal Salary { get; set; }
     }
 }
