09538b9276ac0874f562db694edee86269a99f8c
Kristian <krisvl5000@gmail.com>
2023-02-20 10:45:05 +0200

    Update Program.cs

--- 04. C# OOP/Interfaces and Abstraction Exercise/01. Define an Interface IPerson/Program.cs
+++ 04. C# OOP/Interfaces and Abstraction Exercise/01. Define an Interface IPerson/Program.cs
@@ -4,11 +4,8 @@
     {
         static void Main(string[] args)
         {
-            string name = Console.ReadLine();
-            int age = int.Parse(Console.ReadLine());
-            IPerson person = new Citizen(name, age);
-            Console.WriteLine(person.Name);
-            Console.WriteLine(person.Age);
+
+
         }
     }
 }
