35ee10ce1fef2c29555031cf4d6844fbb1bddd20
Kristian <krisvl5000@gmail.com>
2023-02-20 23:05:51 +0200

    Update Soldier.cs

--- 04. C# OOP/Interfaces and Abstraction Exercise/07. Military Elite/Models/Soldier.cs
+++ 04. C# OOP/Interfaces and Abstraction Exercise/07. Military Elite/Models/Soldier.cs
@@ -8,6 +8,18 @@ namespace MilitaryElite
 {
     public class Soldier : ISoldier
     {
+        public Soldier()
+        {
+
+        }
+
+        public Soldier(string id, string firstName, string lastName) : this()
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+        }
+
         public string Id { get; set; }
 
         public string FirstName {get; set; }
