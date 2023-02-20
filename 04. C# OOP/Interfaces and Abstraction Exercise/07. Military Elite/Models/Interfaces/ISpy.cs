f598faf242bc84cbcbaba023fc8ab74a62781303
Kristian <krisvl5000@gmail.com>
2023-02-20 20:54:36 +0200

    Create ISpy.cs

---
+++ 04. C# OOP/Interfaces and Abstraction Exercise/07. Military Elite/Models/Interfaces/ISpy.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryElite
+{
+    public interface ISpy : ISoldier
+    {
+        int CodeNumber { get; }
+    }
+}
