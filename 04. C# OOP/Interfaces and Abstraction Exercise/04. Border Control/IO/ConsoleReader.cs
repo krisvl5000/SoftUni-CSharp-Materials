69378b2d7b7da0a3ed6e290efb5f9763500f56b7
Kristian <krisvl5000@gmail.com>
2023-02-20 15:33:31 +0200

    Create ConsoleReader.cs

---
+++ 04. C# OOP/Interfaces and Abstraction Exercise/04. Border Control/IO/ConsoleReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04._Border_Control.IO
+{
+    internal class ConsoleReader
+    {
+    }
+}
