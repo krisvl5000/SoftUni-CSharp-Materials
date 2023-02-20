58a42d277c1ac60a8c019a995a315bf990ca026d
Kristian <krisvl5000@gmail.com>
2023-02-20 10:49:37 +0200

    Update Citizen.cs

--- 04. C# OOP/Interfaces and Abstraction Exercise/01. Define an Interface IPerson/Citizen.cs
+++ 04. C# OOP/Interfaces and Abstraction Exercise/01. Define an Interface IPerson/Citizen.cs
@@ -6,7 +6,15 @@ using System.Threading.Tasks;
 
 namespace PersonInfo
 {
-    internal class Citizen
+    public class Citizen : IPerson
     {
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name { get; set; }
+
+        public int Age {get; set;}
     }
 }
