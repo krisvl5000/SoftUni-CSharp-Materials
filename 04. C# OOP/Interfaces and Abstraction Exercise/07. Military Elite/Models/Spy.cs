be1d5627662a340eb6f1b72e0731fb0611c371d9
Kristian <krisvl5000@gmail.com>
2023-02-20 20:54:35 +0200

    Create Spy.cs

---
+++ 04. C# OOP/Interfaces and Abstraction Exercise/07. Military Elite/Models/Spy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MilitaryElite
+{
+    public class Spy : Soldier, ISpy
+    {
+        public Spy(int codeNumber)
+        {
+            CodeNumber = codeNumber;
+        }
+
+        public int CodeNumber {get; set;}
+    }
+}
