31a3b7502ad5278e6777cbf03cfad93986551ea1
Kristian <krisvl5000@gmail.com>
2023-02-21 10:22:04 +0200

    Update SpecialisedSoldier.cs

--- 04. C# OOP/Interfaces and Abstraction Exercise/07. Military Elite/Models/SpecialisedSoldier.cs
+++ 04. C# OOP/Interfaces and Abstraction Exercise/07. Military Elite/Models/SpecialisedSoldier.cs
@@ -6,30 +6,8 @@ using System.Threading.Tasks;
 
 namespace MilitaryElite
 {
-    public class SpecialisedSoldier : Soldier, ISpecialisedSoldier
+    public class SpecialisedSoldier : Private, ISpecialisedSoldier
     {
-        private string corps;
 
-        public SpecialisedSoldier(string corps)
-        {
-            Corps = corps;
-        }
-
-        public string Corps
-        {
-            get
-            {
-                return corps;
-            }
-            set
-            {
-                if (value != "AirForces" || value != "Marines")
-                {
-                    return;
-                }
-
-                corps = value;
-            }
-        }
     }
 }
