8344d48cba283f7d6d2089253810b083b2ece3b6
Kristian <krisvl5000@gmail.com>
2023-02-22 17:38:30 +0200

    Create Controller.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 10 December 2022/Core/Controller.cs
@@ -0,0 +1,100 @@
+﻿using ChristmasPastryShop.Core.Contracts;
+using ChristmasPastryShop.Models.Booths;
+using ChristmasPastryShop.Models.Booths.Contracts;
+using ChristmasPastryShop.Models.Cocktails;
+using ChristmasPastryShop.Models.Delicacies;
+using ChristmasPastryShop.Models.Delicacies.Contracts;
+using ChristmasPastryShop.Repositories;
+using ChristmasPastryShop.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ChristmasPastryShop.Core
+{
+    public class Controller : IController
+    {
+        private BoothRepository booths;
+
+        public Controller()
+        {
+            booths = new BoothRepository();
+        }
+
+        public string AddBooth(int capacity)
+        {
+            IBooth booth = new Booth(booths.Models.Count + 1, capacity);
+            booths.AddModel(booth);
+
+            return String.Format(OutputMessages.NewBoothAdded, booth.BoothId, capacity);
+        }
+
+        public string AddDelicacy(int boothId, string delicacyTypeName,
+            string delicacyName)
+        {
+            var booth = booths.Models.FirstOrDefault(x => x.BoothId == boothId);
+
+            if (delicacyTypeName != nameof(Stolen) &&
+                delicacyTypeName != nameof(Gingerbread))
+            {
+                return String
+                    .Format(OutputMessages.InvalidDelicacyType, delicacyTypeName);
+            }
+            
+            if (booth.DelicacyMenu.Models.Any(x => x.Name == delicacyName))
+            {
+                return String
+                    .Format(OutputMessages.DelicacyAlreadyAdded, delicacyName);
+            }
+
+            IDelicacy delicacy;
+
+            if (delicacyTypeName == nameof(Gingerbread))
+            {
+                delicacy = new Gingerbread(delicacyName);
+            }
+            else
+            {
+                delicacy = new Stolen(delicacyName);
+            }
+
+            booth.DelicacyMenu.AddModel(delicacy);
+
+            return String
+                .Format(OutputMessages.NewDelicacyAdded, delicacyTypeName, delicacyName);
+        }
+
+        public string AddCocktail(int boothId, string cocktailTypeName, 
+            string cocktailName, string size)
+        {
+            var booth = booths.Models.FirstOrDefault(x => x.BoothId == boothId);
+
+            if (cocktailTypeName != nameof(MulledWine) &&
+                cocktailTypeName != nameof(Hibernation))
+            {
+
+            }    
+        }
+
+        public string BoothReport(int boothId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string LeaveBooth(int boothId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReserveBooth(int countOfPeople)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TryOrder(int boothId, string order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
