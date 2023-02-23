0088f1cf4ca708289e850df4bbed00e829a516da
Kristian <krisvl5000@gmail.com>
2023-02-23 19:56:58 +0200

    Added Templates

--- 04. C# OOP/Reflection and Attributes Lab/01. Stealer/Program.cs
+++ 04. C# OOP/Reflection and Attributes Lab/01. Stealer/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Net;
+using System.Reflection;
+
+namespace _01._Hello_Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student studemt = new Student();
+            PrintProperties(DateTime.Now);
+
+        }
+
+        public static void PrintProperties(object obj)
+        {
+            PropertyInfo[] properties = obj.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"{property.Name} : {property.PropertyType}");
+            }
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public double Score { get; set; }
+
+        public Student Friend { get; set; }
+    }
+}
