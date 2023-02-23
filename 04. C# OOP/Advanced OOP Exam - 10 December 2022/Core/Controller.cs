bb70d6849e078701e5ae672416089bdcafecb7cb
Kristian <krisvl5000@gmail.com>
2023-02-23 12:17:56 +0200

    Update Controller.cs

--- 04. C# OOP/Advanced OOP Exam - 10 December 2022/Core/Controller.cs
+++ 04. C# OOP/Advanced OOP Exam - 10 December 2022/Core/Controller.cs
@@ -8,8 +8,10 @@ using ChristmasPastryShop.Utilities.Messages;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using ChristmasPastryShop.Models.Cocktails;
+using ChristmasPastryShop.Models.Cocktails.Contracts;
 
 namespace ChristmasPastryShop.Core
 {
@@ -41,7 +43,7 @@ namespace ChristmasPastryShop.Core
                 return String
                     .Format(OutputMessages.InvalidDelicacyType, delicacyTypeName);
             }
-            
+
             if (booth.DelicacyMenu.Models.Any(x => x.Name == delicacyName))
             {
                 return String
@@ -65,16 +67,121 @@ namespace ChristmasPastryShop.Core
                 .Format(OutputMessages.NewDelicacyAdded, delicacyTypeName, delicacyName);
         }
 
-        public string AddCocktail(int boothId, string cocktailTypeName, 
+        public string AddCocktail(int boothId, string cocktailTypeName,
             string cocktailName, string size)
         {
-            var booth = booths.Models.FirstOrDefault(x => x.BoothId == boothId);
+            var booth = booths.Models.First(x => x.BoothId == boothId);
 
             if (cocktailTypeName != nameof(Hibernation) &&
                 cocktailTypeName != nameof(MulledWine))
             {
-
+                return String.Format(OutputMessages.InvalidCocktailType, cocktailTypeName);
             }
+
+            if (size != "Small" && size != "Middle" && size != "Large")
+            {
+                return String.Format(OutputMessages.InvalidCocktailSize, size);
+            }
+
+            if (booth.CocktailMenu.Models
+                .Any(x => x.Name == cocktailName && x.Size == size))
+            {
+                return String
+                    .Format(OutputMessages.CocktailAlreadyAdded, size, cocktailName);
+            }
+
+            ICocktail cocktailToAdd;
+
+            if (cocktailTypeName == nameof(Hibernation))
+            {
+                cocktailToAdd = new Hibernation(cocktailName, size);
+            }
+            else
+            {
+                cocktailToAdd = new MulledWine(cocktailName, size);
+            }
+
+            booth.CocktailMenu.AddModel(cocktailToAdd);
+
+            return String.Format(OutputMessages
+                .NewCocktailAdded, size, cocktailName, cocktailTypeName);
+        }
+        public string ReserveBooth(int countOfPeople)
+        {
+            var boothToWorkWith = booths.Models
+                .Where(x => x.IsReserved == false && x.Capacity >= countOfPeople)
+                .OrderBy(x => x.Capacity)
+                .ThenByDescending(x => x.BoothId)
+                .FirstOrDefault();
+
+            if (boothToWorkWith == null)
+            {
+                return String.Format(OutputMessages.NoAvailableBooth, countOfPeople);
+            }
+
+            boothToWorkWith.ChangeStatus();
+
+            return String.Format(OutputMessages
+                .BoothReservedSuccessfully, boothToWorkWith.BoothId, countOfPeople);
+        }
+        public string TryOrder(int boothId, string order)
+        {
+            var booth = booths.Models
+                .First(x => x.BoothId == boothId);
+
+            string[] args = order.Split('/');
+
+            string itemTypeName = args[0];
+            string itemName = args[1];
+            int countOfPieces = int.Parse(args[2]);
+
+            string size = " ";
+
+            if (itemTypeName == nameof(Hibernation) || itemTypeName == nameof(MulledWine))
+            {
+                size = args[3];
+            }
+
+            if (itemTypeName != nameof(Hibernation) &&
+                itemTypeName != nameof(MulledWine) &&
+                itemTypeName != nameof(Gingerbread) &&
+                itemTypeName != nameof(Stolen))
+            {
+                return string.Format(OutputMessages.NotRecognizedType, itemTypeName);
+            }
+
+            if (booth.CocktailMenu.Models
+                    .FirstOrDefault(x => x.Name == itemName) == null &&
+                booth.DelicacyMenu.Models
+                    .FirstOrDefault(x => x.Name == itemName) == null)
+            {
+                return String
+                    .Format(OutputMessages
+                        .NotRecognizedItemName, itemTypeName, itemName);
+            }
+
+            if (size != " ") // Cocktail
+            {
+                var item = booth.CocktailMenu.Models
+                    .FirstOrDefault(x => x.Name == itemName &&
+                                         x.Size == size &&
+                                         x.GetType().Name == itemTypeName);
+                if (item != null)
+                {
+                    booth.UpdateCurrentBill(item.Price * countOfPieces);
+
+                    return string
+                        .Format(OutputMessages
+                            .SuccessfullyOrdered, boothId, countOfPieces, itemName);
+                }
+
+                return String.Format(OutputMessages.CocktailStillNotAdded, size, itemName);
+            }
+            else
+            {
+                
+            }
+
         }
 
         public string BoothReport(int boothId)
@@ -83,16 +190,6 @@ namespace ChristmasPastryShop.Core
         }
 
         public string LeaveBooth(int boothId)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string ReserveBooth(int countOfPeople)
-        {
-            throw new NotImplementedException();
-        }
-
-        public string TryOrder(int boothId, string order)
         {
             throw new NotImplementedException();
         }
