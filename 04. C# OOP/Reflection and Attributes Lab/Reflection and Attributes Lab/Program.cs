0f22aadc0572df9e0440bc1b301218cd4216bbc5
Kristian <krisvl5000@gmail.com>
2023-02-25 10:18:26 +0200

    Update Program.cs

--- 04. C# OOP/Reflection and Attributes Lab/Reflection and Attributes Lab/Program.cs
+++ 04. C# OOP/Reflection and Attributes Lab/Reflection and Attributes Lab/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Net;
+using System.Reflection;
 
 namespace _01._Hello_Softuni
 {
@@ -6,8 +8,29 @@ namespace _01._Hello_Softuni
     {
         static void Main(string[] args)
         {
-
-
+            Student student = new Student();
+            PrintProperties(DateTime.Now);
         }
+
+        public static void PrintProperties(object obj)
+        {
+            PropertyInfo[] properties = obj.GetType().GetProperties();
+
+            foreach (var property in properties)
+            {
+                Console.WriteLine($"{property.Name} : {property.PropertyType}");
+            }
+        }
+    }
+
+    public class Student
+    {
+        public string Name { get; set; }
+
+        public int Age { get; set; }
+
+        public double Score { get; set; }
+
+        public Student Friend { get; set; }
     }
 }
