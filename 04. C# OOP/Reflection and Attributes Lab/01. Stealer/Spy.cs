a1237dd59a78d90b74e716920ae2129bf1a39c0e
Kristian <krisvl5000@gmail.com>
2023-02-26 10:47:11 +0200

    Update Spy.cs

--- 04. C# OOP/Reflection and Attributes Lab/01. Stealer/Spy.cs
+++ 04. C# OOP/Reflection and Attributes Lab/01. Stealer/Spy.cs
@@ -1,22 +1,35 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Stealer
 {
     public class Spy
     {
-        public Spy()
+        public string StealFieldInfo(string investigatedClass, params string[] requestedFields)
         {
-                
-        }
+            Type classType = Type.GetType(investigatedClass);
 
-        public string StealFieldInfo(string className, string[] fieldNames)
-        {
-           throw new NotImplementedException();
+            FieldInfo[] fields = classType
+                .GetFields(BindingFlags.Public | 
+                           BindingFlags.Instance | 
+                           BindingFlags.Static | 
+                           BindingFlags.NonPublic);
+
+            StringBuilder sb = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(classType);
+
+            foreach (FieldInfo field in fields)
+            {
+                if (requestedFields.Contains(field.Name))
+                {
+                    sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+                }
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
