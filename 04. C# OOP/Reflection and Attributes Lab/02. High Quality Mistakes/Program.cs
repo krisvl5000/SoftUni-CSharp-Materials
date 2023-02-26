c2fba75db3fec912bd2e8d6c2df135651dd45066
Kristian <krisvl5000@gmail.com>
2023-02-26 11:17:17 +0200

    Update Program.cs

--- 04. C# OOP/Reflection and Attributes Lab/02. High Quality Mistakes/Program.cs
+++ 04. C# OOP/Reflection and Attributes Lab/02. High Quality Mistakes/Program.cs
@@ -6,7 +6,9 @@ namespace Stealer
     {
         static void Main(string[] args)
         {
-
+            Spy spy = new Spy();
+            string result = spy.AnalyzeAccessModifiers("Hacker");
+            Console.WriteLine(result);
 
         }
     }
