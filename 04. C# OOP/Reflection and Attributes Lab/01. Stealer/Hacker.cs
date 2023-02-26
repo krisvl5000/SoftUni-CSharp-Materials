4e16447f9a835c52b830f1855c2c6230e5959f61
Kristian <krisvl5000@gmail.com>
2023-02-26 10:47:10 +0200

    Update Hacker.cs

--- 04. C# OOP/Reflection and Attributes Lab/01. Stealer/Hacker.cs
+++ 04. C# OOP/Reflection and Attributes Lab/01. Stealer/Hacker.cs
@@ -1,19 +1,23 @@
-﻿public class Hacker
+﻿namespace Stealer
 {
-    public string username = "securityGod82";
-    private string password = "mySuperSecretPassw0rd";
-
-    public string Password
+    public class Hacker
     {
-        get => this.password;
-        set => this.password = value;
+        public string username = "securityGod82";
+        private string password = "mySuperSecretPassw0rd";
+
+        public string Password
+        {
+            get => this.password;
+            set => this.password = value;
+        }
+
+        private int Id { get; set; }
+
+        public double BankAccountBalance { get; private set; }
+
+        public void DownloadAllBankAccountsInTheWorld()
+        {
+        }
     }
 
-    private int Id { get; set; }
-
-    public double BankAccountBalance { get; private set; }
-
-    public void DownloadAllBankAccountsInTheWorld()
-    {
-    }
 }
