05ab2110ea0ec0c8566baa144997dce70edbb1df
Kristian <krisvl5000@gmail.com>
2023-02-27 13:32:56 +0200

    Update Validator.cs

--- 04. C# OOP/Reflection and Attributes Exercise/02. Validation Attributes/Utilities/Validator.cs
+++ 04. C# OOP/Reflection and Attributes Exercise/02. Validation Attributes/Utilities/Validator.cs
@@ -15,9 +15,33 @@ public abstract class Validator
 
         foreach (var validationProp in properties)
         {
+            var customAttributes = validationProp.CustomAttributes.ToArray();
 
+            object propValue = validationProp.GetValue(obj);
+
+            foreach (var customAttribute in customAttributes)
+            {
+                Type attributeType = customAttribute.AttributeType;
+                MethodInfo isValidMethod = attributeType
+                    .GetMethods(BindingFlags.Instance | BindingFlags.Public)
+                    .FirstOrDefault(mi => mi.Name == "IsValid");
+
+                if (isValidMethod == null)
+                {
+                    throw new InvalidOperationException("Invalid method!");
+                }
+
+                object attributeInstance = Activator.CreateInstance(attributeType);
+                bool result = (bool)isValidMethod
+                    .Invoke(attributeInstance, new object[] { propValue});
+
+                if (!result)
+                {
+                    return false;
+                }
+            }
         }
 
-        return false;
+        return true;
     }
 }
