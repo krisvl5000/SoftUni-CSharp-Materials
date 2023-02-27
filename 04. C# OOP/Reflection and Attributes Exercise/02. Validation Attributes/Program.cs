82c3b2ba42c16b49705bf89cf1f18f2070a2ffe8
Kristian <krisvl5000@gmail.com>
2023-02-27 12:38:50 +0200

    Update Program.cs

--- 04. C# OOP/Reflection and Attributes Exercise/02. Validation Attributes/Program.cs
+++ 04. C# OOP/Reflection and Attributes Exercise/02. Validation Attributes/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using ValidationAttributes.Models;
+using ValidationAttributes.Utilities;
+
 
 namespace ValidationAttributes
 {
@@ -6,7 +8,9 @@ namespace ValidationAttributes
     {
         static void Main(string[] args)
         {
+            var person = new Person(null, -1);
 
+            bool isValidEntity = Validator.IsValid(person);
 
         }
     }
