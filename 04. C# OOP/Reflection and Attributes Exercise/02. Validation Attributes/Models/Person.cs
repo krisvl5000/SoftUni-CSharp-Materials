d9158c3a0417cb5f8d3c026c7452f0ea874f0512
Kristian <krisvl5000@gmail.com>
2023-02-27 12:38:54 +0200

    Create Person.cs

---
+++ 04. C# OOP/Reflection and Attributes Exercise/02. Validation Attributes/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿using ValidationAttributes.Utilities.Attributes;
+
+namespace ValidationAttributes.Models;
+
+public class Person
+{
+    private const int MINAGE = 12;
+    private const int MAXAGE = 90;
+
+    public Person(string fullName, int age)
+    {
+        FullName = fullName;
+        Age = age;
+    }
+
+    [MyRequired]
+    public string FullName { get; private set; }
+
+    [MyRange(MINAGE, MAXAGE)]
+    public int Age { get; private set; }
+}
