bc37d57f35fde24511547f6e907915db00fd3386
Kristian <krisvl5000@gmail.com>
2023-02-27 12:21:18 +0200

    Update MyRangeAttribute.cs

--- 04. C# OOP/Reflection and Attributes Exercise/02. Validation Attributes/Utilities/Attributes/MyRangeAttribute.cs
+++ 04. C# OOP/Reflection and Attributes Exercise/02. Validation Attributes/Utilities/Attributes/MyRangeAttribute.cs
@@ -1,6 +1,20 @@
 ﻿namespace ValidationAttributes.Utilities.Attributes;
 
-public class MyRangeAttribute
+[AttributeUsage(AttributeTargets.Property | AttributeTargets.Field)]
+public class MyRangeAttribute : MyValidationAttribute
 {
-    
+    private int minValue;
+    private int maxValue;
+
+    public MyRangeAttribute(int minValue, int maxValue)
+    {
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+    }
+
+    public override bool IsValid(object value)
+    {
+        int numValue = (int)value;
+        return numValue >= minValue && numValue <= maxValue;
+    }
 }
