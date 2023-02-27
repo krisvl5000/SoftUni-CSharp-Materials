dc511c637687c26d8aae3f0c420e9121ab28a0cf
Kristian <krisvl5000@gmail.com>
2023-02-27 12:38:52 +0200

    Create Validator.cs

---
+++ 04. C# OOP/Reflection and Attributes Exercise/02. Validation Attributes/Utilities/Validator.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using ValidationAttributes.Utilities.Attributes;
+
+namespace ValidationAttributes.Utilities;
+
+public abstract class Validator
+{
+    public static bool IsValid(object obj)
+    {
+        Type objType = obj.GetType();
+        PropertyInfo[] properties = objType.GetProperties()
+            .Where(p => p.CustomAttributes
+                .Any(ca => ca.AttributeType
+                    .IsAssignableFrom(typeof(MyValidationAttribute))))
+            .ToArray();
+
+        foreach (var validationProp in properties)
+        {
+
+        }
+
+        return false;
+    }
+}
