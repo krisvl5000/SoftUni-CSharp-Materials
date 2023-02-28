5e0df69d0e4461f4cae089b197373c09f449adb1
Kristian <krisvl5000@gmail.com>
2023-02-28 12:46:13 +0200

    Update AxeTests.cs

--- 04. C# OOP/Unit Testing Lab/01. Test Axe/Skeleton.Tests/AxeTests.cs
+++ 04. C# OOP/Unit Testing Lab/01. Test Axe/Skeleton.Tests/AxeTests.cs
@@ -5,10 +5,23 @@ namespace Skeleton.Tests
     [TestFixture]
     public class AxeTests
     {
-        [Test]
-        public void Test1()
-        {
+        private Axe axe;
+        private int attackPoints;
+        private int durabilityPoints;
 
+        [SetUp]
+        public void Setup()
+        {
+            attackPoints = 10;
+            durabilityPoints = 15;
+            Axe axe = new Axe(attackPoints, durabilityPoints);
+        }
+
+        [Test]
+        public void Test_AxeConstructorShouldSetDataProperly()
+        {
+            Assert.AreEqual(10, axe.AttackPoints);
+            Assert.AreEqual(15, axe.DurabilityPoints);
         }
     }
 }
