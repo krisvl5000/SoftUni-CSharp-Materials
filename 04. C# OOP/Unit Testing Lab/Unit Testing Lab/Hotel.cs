259ef25af08bc01e7256ca844d5e4088726cd667
Kristian <krisvl5000@gmail.com>
2023-02-28 10:06:45 +0200

    Update Hotel.cs

--- 04. C# OOP/Unit Testing Lab/Unit Testing Lab/Hotel.cs
+++ 04. C# OOP/Unit Testing Lab/Unit Testing Lab/Hotel.cs
@@ -4,9 +4,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Unit_Testing_Lab
+namespace Booking
 {
-    internal class Booking
+    internal class Hotel
     {
+        public Hotel()
+        {
+            Reservations = new List<Reservation>();
+        }
+
+        public List<Reservation> Reservations { get; set; }
+
+        public void AddReservation(DateTime startDate, DateTime endDate)
+        {
+            foreach (var item in Reservations)
+            {
+                bool overlap = item.Start < endDate && startDate < item.End;
+
+                if (overlap)
+                {
+                    throw new ArgumentException("Hotel is already " +
+                    $"booked for {startDate} and {endDate}!");
+                }
+            }
+        }
     }
 }
