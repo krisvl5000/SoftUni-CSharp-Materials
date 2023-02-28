4accef732b017b13618af5d1a919b84469711ed7
Kristian <krisvl5000@gmail.com>
2023-02-28 10:06:48 +0200

    Update Reservation.cs

--- 04. C# OOP/Unit Testing Lab/Unit Testing Lab/Reservation.cs
+++ 04. C# OOP/Unit Testing Lab/Unit Testing Lab/Reservation.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Unit_Testing_Lab
+namespace Booking
 {
-    internal class Availability
+    public class Reservation
     {
+        public DateTime Start { get; set; }
+
+        public DateTime End { get; set; }
     }
 }
