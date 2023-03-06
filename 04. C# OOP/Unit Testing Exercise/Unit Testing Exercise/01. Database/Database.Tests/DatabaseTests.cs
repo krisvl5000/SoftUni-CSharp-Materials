7f552874e2908ab0ee7395a361f64a82e26ef412
Kristian <krisvl5000@gmail.com>
2023-03-06 10:38:32 +0200

    Update DatabaseTests.cs

--- 04. C# OOP/Unit Testing Exercise/Unit Testing Exercise/01. Database/Database.Tests/DatabaseTests.cs
+++ 04. C# OOP/Unit Testing Exercise/Unit Testing Exercise/01. Database/Database.Tests/DatabaseTests.cs
@@ -5,7 +5,28 @@ namespace Database.Tests
     [TestFixture]
     public class DatabaseTests
     {
+        [SetUp]
+        public void Setup()
+        {
 
+        }
+
+        [TestCase(new int[]{ })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5 })]
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16})]
+        public void ConstructorShouldInitializeDataWithCorrectCount(int[] data)
+        {
+            // Arrange
+
+            // Act
+            Database  db = new Database(data);
+
+            // Assert
+            int expectedCount = data.Length;
+            int actualCount = db.Count;
+
+            Assert.AreEqual(expectedCount, actualCount);
+        }
 
     }
 }
