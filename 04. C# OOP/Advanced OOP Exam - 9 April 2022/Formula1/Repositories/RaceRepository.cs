ff7ce05f2d720f15d3d0879d95217f4a9ed71412
Kristian <krisvl5000@gmail.com>
2023-03-06 14:15:29 +0200

    Update RaceRepository.cs

--- 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Repositories/RaceRepository.cs
+++ 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Repositories/RaceRepository.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
 
 namespace Formula1.Repositories
 {
-    internal class Racerepository
+    public class RaceRepository : IRepository<IRace>
     {
+        public RaceRepository()
+        {
+            models = new List<IRace>();
+        }
+
+        private List<IRace> models;
+
+        public IReadOnlyCollection<IRace> Models => models.AsReadOnly();
+
+        public void Add(IRace model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IRace model)
+        {
+            return models.Remove(model);
+        }
+
+        public IRace FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.RaceName == name);
+        }
     }
 }
