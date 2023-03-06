b5a72c533009b07aa799d2448baa4f50b9ae349b
Kristian <krisvl5000@gmail.com>
2023-03-06 11:43:54 +0200

    Update DatabaseTests.cs

--- 04. C# OOP/Unit Testing Exercise/Unit Testing Exercise/01. Database/Database.Tests/DatabaseTests.cs
+++ 04. C# OOP/Unit Testing Exercise/Unit Testing Exercise/01. Database/Database.Tests/DatabaseTests.cs
@@ -28,5 +28,14 @@ namespace Database.Tests
             Assert.AreEqual(expectedCount, actualCount);
         }
 
+        [TestCase(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17})]
+        public void ConstructorShouldThrowExceptionWhenInputDataIsAbove16Count(int[] data)
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                Database db = new Database(data);
+            }, "Array's capacity must be exactly 16 integers!");
+        }
+
     }
 }
