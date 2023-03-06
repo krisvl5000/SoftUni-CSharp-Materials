c2f02566f9d748e14ac12b3bfbcb6a30b32958a9
Kristian <krisvl5000@gmail.com>
2023-03-06 14:03:02 +0200

    Update Williams.cs

--- 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Models/Williams.cs
+++ 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Models/Williams.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace Formula1.Models
 {
-    internal class Williams
+    public class Williams : FormulaOneCar
     {
+        public Williams(string model, int horsepower, double engineDisplacement) 
+            : base(model, horsepower, engineDisplacement)
+        {
+
+        }
     }
 }
