66d3c1a051911d1cf689b5b3fbabfe6646007355
Kristian <krisvl5000@gmail.com>
2023-03-06 14:03:05 +0200

    Update FormulaOneCar.cs

--- 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Models/FormulaOneCar.cs
+++ 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Models/FormulaOneCar.cs
@@ -1,12 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.Design;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Formula1.Models.Contracts;
+using Formula1.Utilities;
 
 namespace Formula1.Models
 {
-    internal class FormulaOneCar
+    public abstract class FormulaOneCar : IFormulaOneCar
     {
+        private string model;
+        private int horsepower;
+        private double engineDisplacement;
+
+        public FormulaOneCar(string model, int horsepower, double engineDisplacement)
+        {
+            Model = model;
+            Horsepower = horsepower;
+            EngineDisplacement = engineDisplacement;
+        }
+
+        public string Model
+        {
+            get { return model; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < 3)
+                {
+                    throw new ArgumentException(String
+                        .Format(ExceptionMessages.InvalidF1CarModel, value));
+                }
+                model = value;
+            }
+        }
+
+        public int Horsepower
+        {
+            get { return horsepower; }
+            private set
+            {
+                if (value < 950 || value > 1050)
+                {
+                    throw new ArgumentException(String
+                        .Format(ExceptionMessages.InvalidF1HorsePower, value));
+                }
+                horsepower = value;
+            }
+        }
+
+        public double EngineDisplacement
+        {
+            get { return engineDisplacement; }
+            private set
+            {
+                if (value < 1.60 || value > 2.00)
+                {
+                    throw new ArgumentException(String
+                        .Format(ExceptionMessages
+                            .InvalidF1EngineDisplacement, value));
+                }
+                engineDisplacement = value;
+            }
+        }
+
+        public double RaceScoreCalculator(int laps)
+        {
+            return engineDisplacement / horsepower * laps;
+        }
     }
 }
