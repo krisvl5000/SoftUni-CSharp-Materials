c624299f6931b1f0c2cc772f13ee4f9d1a539fd2
Kristian <krisvl5000@gmail.com>
2023-03-06 14:15:26 +0200

    Update FormulaOneCarRepository.cs

--- 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Repositories/FormulaOneCarRepository.cs
+++ 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Repositories/FormulaOneCarRepository.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
 
 namespace Formula1.Repositories
 {
-    internal class FormulaOneCarRepository
+    public class FormulaOneCarRepository : IRepository<IFormulaOneCar>
     {
+        public FormulaOneCarRepository()
+        {
+            models = new List<IFormulaOneCar>();
+        }
+
+        private List<IFormulaOneCar> models;
+
+        public IReadOnlyCollection<IFormulaOneCar> Models => models.AsReadOnly();
+
+        public void Add(IFormulaOneCar model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IFormulaOneCar model)
+        {
+            return models.Remove(model);
+        }
+
+        public IFormulaOneCar FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Model == name);
+        }
     }
 }
