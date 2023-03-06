ac2e905ed3e98b9121cb4dea52c4c1b2563cc864
Kristian <krisvl5000@gmail.com>
2023-03-06 17:48:51 +0200

    Update SmartphoneShopTests.cs

--- 04. C# OOP/Advanced OOP Exam - 9 April 2022/Unit Testing/SmartphoneShop.Tests/SmartphoneShopTests.cs
+++ 04. C# OOP/Advanced OOP Exam - 9 April 2022/Unit Testing/SmartphoneShop.Tests/SmartphoneShopTests.cs
@@ -21,30 +21,27 @@ namespace SmartphoneShop.Tests
         }
 
         [Test]
-        public void TestConstructorIsSettingCorrectly()
+        public void Test_ConstructorIsSettingCorrectly()
         {
             Assert.AreEqual(capacity, shop.Capacity);
             Assert.AreEqual(0, shop.Count);
         }
 
         [Test]
-        public void TestNegativeOrZeroCapacityShouldThrow()
+        public void Test_NegativeOrZeroCapacityShouldThrow()
         {
-            Assert.Throws<ArgumentException>(() =>
-            {
-                shop = new Shop(-5);
-            });
+            Assert.Throws<ArgumentException>(() => { shop = new Shop(-5); });
         }
 
         [Test]
-        public void TestAddSmartphoneShouldWork()
+        public void Test_AddSmartphoneShouldWork()
         {
             shop.Add(smartphone);
             Assert.AreEqual(1, shop.Count);
         }
 
         [Test]
-        public void TestAddSmartphoneThatExistsShouldThrow()
+        public void Test_AddSmartphoneThatExistsShouldThrow()
         {
             Assert.Throws<InvalidOperationException>(() =>
             {
@@ -54,20 +51,17 @@ namespace SmartphoneShop.Tests
         }
 
         [Test]
-        public void TestThrowsIfCapacityIsFull()
+        public void Test_ThrowsIfCapacityIsFull()
         {
             shop.Add(new Smartphone("1", 1));
             shop.Add(new Smartphone("2", 1));
             shop.Add(new Smartphone("3", 1));
 
-            Assert.Throws<InvalidOperationException>(() =>
-            {
-                shop.Add(new Smartphone("4", 1));
-            });
+            Assert.Throws<InvalidOperationException>(() => { shop.Add(new Smartphone("4", 1)); });
         }
 
         [Test]
-        public void TestRemoveSmartphoneShouldWork()
+        public void Test_RemoveSmartphoneShouldWork()
         {
             shop.Add(smartphone);
             shop.Remove(smartphone.ModelName);
@@ -76,23 +70,52 @@ namespace SmartphoneShop.Tests
         }
 
         [Test]
-        public void TestShouldThrowWhenPhoneIsNotFound()
+        public void Test_ShouldThrowWhenPhoneIsNotFound()
         {
             shop.Add(smartphone);
 
-            Assert.Throws<InvalidOperationException>(() =>
-            {
-                shop.Remove("Test");
-            });
+            Assert.Throws<InvalidOperationException>(() => { shop.Remove("Test"); });
         }
 
         [Test]
-        public void TestPhoneShouldReduceBatteryCharge()
+        public void Test_TestPhoneShouldReduceBatteryCharge()
         {
             shop.Add(smartphone);
             shop.TestPhone(modelName, 50);
 
-            Assert.AreEqual(50, smartphone.CurrentBateryCharge);
+            Assert.AreEqual(maximumBatteryCharge - 50, smartphone.CurrentBateryCharge);
+        }
+
+        [Test]
+        public void Test_TestPhoneShouldThrowWhenPhoneIsNotFound()
+        {
+            Assert.Throws<InvalidOperationException>(() => { shop.TestPhone("Test", 10); });
+        }
+
+        [Test]
+        public void Test_TestPhoneShouldNotWorkOnUnchargedPhones()
+        {
+            shop.Add(smartphone);
+
+            Assert.Throws<InvalidOperationException>(() => { shop.TestPhone(modelName, maximumBatteryCharge + 100); });
+        }
+
+        [Test]
+        public void Test_ChargePhoneShouldThrowWhenPhoneIsNotFound()
+        {
+            Assert.Throws<InvalidOperationException>(() => { shop.ChargePhone("Test"); });
+        }
+
+        [Test]
+        public void Test_ChargePhoneShouldSetBatteryToMax()
+        {
+            shop.Add(smartphone);
+
+            shop.TestPhone(modelName, 50);
+
+            shop.ChargePhone(modelName);
+
+            Assert.AreEqual(maximumBatteryCharge, smartphone.CurrentBateryCharge);
         }
     }
 }
