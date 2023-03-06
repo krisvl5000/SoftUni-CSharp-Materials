015fa27607842e923a5b0f615c7e13340a2f0ae3
Kristian <krisvl5000@gmail.com>
2023-03-06 14:15:30 +0200

    Update PilotRepository.cs

--- 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Repositories/PilotRepository.cs
+++ 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Repositories/PilotRepository.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
 
 namespace Formula1.Repositories
 {
-    internal class PilotRepository
+    public class PilotRepository : IRepository<IPilot>
     {
+        public PilotRepository()
+        {
+            models = new List<IPilot>();
+        }
+
+        private List<IPilot> models;
+
+        public IReadOnlyCollection<IPilot> Models => models.AsReadOnly();
+
+        public void Add(IPilot model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IPilot model)
+        {
+            return models.Remove(model);
+        }
+
+        public IPilot FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.FullName == name);
+        }
     }
 }
