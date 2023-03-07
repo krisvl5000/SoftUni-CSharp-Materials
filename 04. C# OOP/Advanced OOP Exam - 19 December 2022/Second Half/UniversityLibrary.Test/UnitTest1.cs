2e74dad2f2402aa6b4e416094eafcbae25eaf82a
Kristian <krisvl5000@gmail.com>
2023-03-07 14:17:18 +0200

    Update UnitTest1.cs

--- 04. C# OOP/Advanced OOP Exam - 19 December 2022/Second Half/UniversityLibrary.Test/UnitTest1.cs
+++ 04. C# OOP/Advanced OOP Exam - 19 December 2022/Second Half/UniversityLibrary.Test/UnitTest1.cs
@@ -1,17 +1,31 @@
+using System.Runtime.CompilerServices;
+
 namespace UniversityLibrary.Test
 {
     using NUnit.Framework;
     public class Tests
     {
+        private UniversityLibrary library;
+        private TextBook book;
+
         [SetUp]
         public void Setup()
         {
+            library = new UniversityLibrary();
+            book = new TextBook("1", "2", "3");
         }
 
         [Test]
-        public void Test1()
+        public void Test_TestingTheConstructor()
         {
-            Assert.Pass();
+            Assert.AreEqual(0, library.Catalogue.Count);
+        }
+
+        [Test]
+        public void Test_AreTextBooksAddedInLibrary()
+        {
+            library.AddTextBookToLibrary(book);
+            Assert.AreEqual(1, library.Catalogue.Count);
         }
     }
 }
