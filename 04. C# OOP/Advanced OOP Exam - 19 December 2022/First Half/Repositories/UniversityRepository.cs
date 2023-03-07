a3676214807163d54c8f709a5f1f69fc322742b2
Kristian <krisvl5000@gmail.com>
2023-03-07 10:46:38 +0200

    Create UniversityRepository.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 19 December 2022/First Half/Repositories/UniversityRepository.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Repositories.Contracts;
+
+namespace UniversityCompetition.Repositories
+{
+    public class UniversityRepository : IRepository<IUniversity>
+    {
+        public UniversityRepository()
+        {
+            models = new List<IUniversity>();
+        }
+
+        private List<IUniversity> models;
+
+        public IReadOnlyCollection<IUniversity> Models => models.AsReadOnly();
+
+        public void AddModel(IUniversity model)
+        {
+            models.Add(model);
+        }
+
+        public IUniversity FindById(int id)
+        {
+            return models.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IUniversity FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
