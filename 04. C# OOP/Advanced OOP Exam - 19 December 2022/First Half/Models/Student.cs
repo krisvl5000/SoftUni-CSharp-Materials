377a211d34593b91b6fe5a36a0d6c7f6169d6e45
Kristian <krisvl5000@gmail.com>
2023-03-07 10:14:56 +0200

    Update Student.cs

--- 04. C# OOP/Advanced OOP Exam - 19 December 2022/First Half/Models/Student.cs
+++ 04. C# OOP/Advanced OOP Exam - 19 December 2022/First Half/Models/Student.cs
@@ -3,10 +3,90 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Utilities.Messages;
 
 namespace UniversityCompetition.Models
 {
-    internal class Student
+    public class Student : IStudent
     {
+        public Student(int studentId, string firstName, string lastName)
+        {
+            Id = studentId;
+            FirstName = firstName;
+            LastName = lastName;
+            coveredExams = new List<int>();
+        }
+
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            private set // might need to be public
+            {
+                id = value;
+            }
+        }
+
+        private string firstName;
+
+        public string FirstName
+        {
+            get { return firstName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(String
+                        .Format(ExceptionMessages.NameNullOrWhitespace));
+                }
+                firstName = value;
+            }
+        }
+
+        private string lastName;
+
+        public string LastName
+        {
+            get { return lastName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(String
+                        .Format(ExceptionMessages.NameNullOrWhitespace));
+                }
+                lastName = value;
+            }
+        }
+
+        private List<int> coveredExams;
+
+        public IReadOnlyCollection<int> CoveredExams
+        {
+            get { return coveredExams.AsReadOnly(); }
+        }
+
+        private IUniversity university;
+
+        public IUniversity University
+        {
+            get { return university; }
+            private set
+            {
+                university = value;
+            }
+        }
+
+        public void CoverExam(ISubject subject)
+        {
+            coveredExams.Add(subject.Id);
+        }
+
+        public void JoinUniversity(IUniversity university)
+        {
+            University = university;
+        }
     }
 }
