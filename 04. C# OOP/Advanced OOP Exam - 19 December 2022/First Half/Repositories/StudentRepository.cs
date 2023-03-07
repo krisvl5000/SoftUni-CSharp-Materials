47d2249228d694edafd642e96361aeca4346795f
Kristian <krisvl5000@gmail.com>
2023-03-07 10:46:37 +0200

    Update StudentRepository.cs

--- 04. C# OOP/Advanced OOP Exam - 19 December 2022/First Half/Repositories/StudentRepository.cs
+++ 04. C# OOP/Advanced OOP Exam - 19 December 2022/First Half/Repositories/StudentRepository.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Repositories.Contracts;
 
 namespace UniversityCompetition.Repositories
 {
-    internal class StudentRepository
+    public class StudentRepository : IRepository<IStudent>
     {
+        public StudentRepository()
+        {
+            models = new List<IStudent>();
+        }
+
+        private List<IStudent> models;
+
+        public IReadOnlyCollection<IStudent> Models => models.AsReadOnly();
+
+        public void AddModel(IStudent model)
+        {
+            models.Add(model);
+        }
+
+        public IStudent FindById(int id)
+        {
+            return models.FirstOrDefault(x => x.Id == id);
+        }
+
+        public IStudent FindByName(string name)
+        {
+            string[] nameArgs = name.Split(' ');
+
+            return models.FirstOrDefault(x => x.FirstName == nameArgs[0] &&
+                                              x.LastName == nameArgs[1]);
+        }
     }
 }
