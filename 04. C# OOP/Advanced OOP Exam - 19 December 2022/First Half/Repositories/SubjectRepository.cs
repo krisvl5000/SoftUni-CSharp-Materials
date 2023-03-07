a4725276dbeb64b0e1c0371b6a0339e36697a08f
Kristian <krisvl5000@gmail.com>
2023-03-07 10:46:40 +0200

    Update SubjectRepository.cs

--- 04. C# OOP/Advanced OOP Exam - 19 December 2022/First Half/Repositories/SubjectRepository.cs
+++ 04. C# OOP/Advanced OOP Exam - 19 December 2022/First Half/Repositories/SubjectRepository.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Repositories.Contracts;
 
 namespace UniversityCompetition.Repositories
 {
-    internal class SubjectRepository
+    public class SubjectRepository : IRepository<ISubject>
     {
+        public SubjectRepository()
+        {
+            models = new List<ISubject>();
+        }
+
+        private List<ISubject> models;
+
+        public IReadOnlyCollection<ISubject> Models => models.AsReadOnly();
+
+        public void AddModel(ISubject model)
+        {
+            models.Add(model);
+        }
+
+        public ISubject FindById(int id)
+        {
+            return models.FirstOrDefault(x => x.Id == id);
+        }
+
+        public ISubject FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
     }
 }
