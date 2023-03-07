67fd764a339095edefb80cab076302100771dbb2
Kristian <krisvl5000@gmail.com>
2023-03-07 10:14:53 +0200

    Update Subject.cs

--- 04. C# OOP/Advanced OOP Exam - 19 December 2022/First Half/Models/Subject.cs
+++ 04. C# OOP/Advanced OOP Exam - 19 December 2022/First Half/Models/Subject.cs
@@ -3,10 +3,57 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UniversityCompetition.Models.Contracts;
+using UniversityCompetition.Utilities.Messages;
 
 namespace UniversityCompetition.Models
 {
-    internal class Subject
+    public abstract class Subject : ISubject
     {
+        public Subject(int subjectId, string subjectName, double subjectRate)
+        {
+            Id = subjectId;
+            Name = subjectName;
+            Rate = subjectRate;
+        }
+
+        private int id;
+
+        public int Id
+        {
+            get { return id; }
+            private set // might have to be public so it can be set from outside
+            {
+                id = value;
+            }
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(String
+                        .Format(ExceptionMessages.NameNullOrWhitespace));
+                }
+                name = value;
+            }
+        }
+
+        private double rate;
+
+        public double Rate
+        {
+            get { return rate; }
+            private set
+            {
+                rate = value;
+            }
+        }
+
     }
 }
