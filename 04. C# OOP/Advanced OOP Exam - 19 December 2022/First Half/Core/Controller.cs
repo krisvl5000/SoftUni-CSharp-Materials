203c0b599c131a682c5679651377c1e1856bf2fe
Kristian <krisvl5000@gmail.com>
2023-03-07 10:49:56 +0200

    Update Controller.cs

--- 04. C# OOP/Advanced OOP Exam - 19 December 2022/First Half/Core/Controller.cs
+++ 04. C# OOP/Advanced OOP Exam - 19 December 2022/First Half/Core/Controller.cs
@@ -3,10 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UniversityCompetition.Core.Contracts;
 
 namespace UniversityCompetition.Core
 {
-    internal class Controller
+    public class Controller : IController
     {
+        public Controller()
+        {
+            
+        }
+
+        public string AddSubject(string subjectName, string subjectType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddUniversity(string universityName, string category, int capacity, List<string> requiredSubjects)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddStudent(string firstName, string lastName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TakeExam(int studentId, int subjectId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ApplyToUniversity(string studentName, string universityName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UniversityReport(int universityId)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
