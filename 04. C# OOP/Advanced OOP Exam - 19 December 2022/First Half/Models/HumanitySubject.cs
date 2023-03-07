ec45a2efbb887bda6b54fe2ae8e8e7f914f64363
Kristian <krisvl5000@gmail.com>
2023-03-07 10:14:50 +0200

    Update HumanitySubject.cs

--- 04. C# OOP/Advanced OOP Exam - 19 December 2022/First Half/Models/HumanitySubject.cs
+++ 04. C# OOP/Advanced OOP Exam - 19 December 2022/First Half/Models/HumanitySubject.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace UniversityCompetition.Models
 {
-    internal class HumanitySubject
+    public class HumanitySubject : Subject
     {
+        private const double SUBJECT_RATE = 1.15;
+        public HumanitySubject(int subjectId, string subjectName) : base(subjectId, subjectName, SUBJECT_RATE)
+        {
+
+        }
     }
 }
