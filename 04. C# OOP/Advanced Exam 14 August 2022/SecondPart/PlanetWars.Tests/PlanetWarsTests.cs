36f527a578a797d9ea240fc4d7a572658d870814
Kristian <krisvl5000@gmail.com>
2023-03-08 22:44:38 +0200

    Update PlanetWarsTests.cs

--- 04. C# OOP/Advanced Exam 14 August 2022/SecondPart/PlanetWars.Tests/PlanetWarsTests.cs
+++ 04. C# OOP/Advanced Exam 14 August 2022/SecondPart/PlanetWars.Tests/PlanetWarsTests.cs
@@ -10,11 +10,23 @@ namespace PlanetWars.Tests
         public class PlanetWarsTests
         {
             private Planet planet;
+            private Planet weakerPlanet;
+            private Planet strongerPlanet;
+
+            private Weapon weapon;
+            private Weapon weakerWeapon;
+            private Weapon strongerWeapon;
 
             [SetUp]
             public void SetUp()
             {
                 planet = new Planet("Name", 10.0);
+                weakerPlanet = new Planet("WeakerPlanet", 5.0);
+                strongerPlanet = new Planet("StrongerPlanet", 15.0);
+
+                weapon = new Weapon("Name", 10.0, 10);
+                weakerWeapon = new Weapon("WeakerWeapon", 10, 5);
+                strongerWeapon = new Weapon("StrongerWeapon", 10, 15);
             }
 
             [Test]
@@ -74,6 +86,68 @@ namespace PlanetWars.Tests
                 Assert.Throws<InvalidOperationException>(() =>
                 {
                     planet.SpendFunds(11.0);
+                });
+            }
+
+            [Test]
+            public void Test_IsAddWeaponWorkingProperly()
+            {
+                planet.AddWeapon(weapon);
+                Assert.AreEqual(1, planet.Weapons.Count);
+            }
+
+            [Test]
+            public void Test_IsAddWeaponThrowingWhenTheSameNameIsAdded()
+            {
+                Assert.Throws<InvalidOperationException>(() =>
+                {
+                    planet.AddWeapon(weapon);
+                    var newWeapon = new Weapon("Name", 10.0, 10);
+                    planet.AddWeapon(newWeapon);
+                });
+            }
+
+            [Test]
+            public void Test_IsRemoveWeaponWorkingProperly()
+            {
+                planet.AddWeapon(weapon);
+                planet.RemoveWeapon("Name");
+                Assert.AreEqual(0, planet.Weapons.Count);
+            }
+
+            [Test]
+            public void Test_IsUpgradeWeaponWorkingProperly()
+            {
+                planet.AddWeapon(weapon);
+                planet.UpgradeWeapon("Name");
+                Assert.AreEqual(11, weapon.DestructionLevel);
+            }
+
+            [Test]
+            public void Test_IsRemoveWeaponThrowingWhenNameIsNotFound()
+            {
+                Assert.Throws<InvalidOperationException>(() =>
+                {
+                    planet.UpgradeWeapon("NonExistentName");
+                });
+            }
+
+            [Test]
+            public void Test_IsDestroyOpponentWorkingProperly()
+            {
+                planet.AddWeapon(weapon);
+                weakerPlanet.AddWeapon(weakerWeapon);
+                Assert.AreEqual($"WeakerPlanet is destructed!", planet.DestructOpponent(weakerPlanet));
+            }
+
+            [Test]
+            public void Test_IsDestroyThrowingWhenFightingStrongerPlanet()
+            {
+                Assert.Throws<InvalidOperationException>(() =>
+                {
+                    planet.AddWeapon(weapon);
+                    strongerPlanet.AddWeapon(strongerWeapon);
+                    planet.DestructOpponent(strongerPlanet);
                 });
             }
         }
