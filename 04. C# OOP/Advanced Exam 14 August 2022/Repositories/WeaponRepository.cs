f3f92586a3a9c15da7115e7fb5d35756007bdd10
Kristian <krisvl5000@gmail.com>
2023-03-08 11:51:46 +0200

    Update WeaponRepository.cs

--- 04. C# OOP/Advanced Exam 14 August 2022/Repositories/WeaponRepository.cs
+++ 04. C# OOP/Advanced Exam 14 August 2022/Repositories/WeaponRepository.cs
@@ -1,10 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
+using PlanetWars.Models.Weapons.Contracts;
+using PlanetWars.Repositories.Contracts;
 
 namespace PlanetWars.Repositories
 {
-    internal class WeaponRepository
+    public class WeaponRepository : IRepository<IWeapon>
     {
+        private List<IWeapon> models;
+
+        public WeaponRepository()
+        {
+            models = new List<IWeapon>();
+        }
+
+        public IReadOnlyCollection<IWeapon> Models => models.AsReadOnly();
+
+        public void AddItem(IWeapon model)
+        {
+            models.Add(model);
+        }
+
+        public IWeapon FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.GetType().Name == name); // might not return the name that is needed
+        }
+
+        public bool RemoveItem(string name)
+        {
+            var itemToRemove = models.FirstOrDefault(x => x.GetType().Name == name);
+
+            if (itemToRemove != null)
+            {
+                models.Remove(itemToRemove);
+                return true;
+            }
+
+            return false;
+            
+        }
     }
 }
