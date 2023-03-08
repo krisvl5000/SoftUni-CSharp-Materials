e6869191efe52e107edd2695f594126b5205a7e8
Kristian <krisvl5000@gmail.com>
2023-03-08 18:01:18 +0200

    Update PlanetWarsTests.cs

--- 04. C# OOP/Advanced Exam 14 August 2022/SecondPart/PlanetWars.Tests/PlanetWarsTests.cs
+++ 04. C# OOP/Advanced Exam 14 August 2022/SecondPart/PlanetWars.Tests/PlanetWarsTests.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using System;
+using System.Collections.Generic;
 
 namespace PlanetWars.Tests
 {
@@ -8,7 +9,73 @@ namespace PlanetWars.Tests
         [TestFixture]
         public class PlanetWarsTests
         {
-            
+            private Planet planet;
+
+            [SetUp]
+            public void SetUp()
+            {
+                planet = new Planet("Name", 10.0);
+            }
+
+            [Test]
+            public void TestingConstructorNameForPlanet()
+            {
+                Assert.AreEqual("Name", planet.Name);
+            }
+
+            [Test]
+            public void TestingConstructorPriceForPlanet()
+            {
+                Assert.AreEqual(10.0, planet.Budget);
+            }
+
+            [Test]
+            public void TestingConstructorWeaponsForPlanet()
+            {
+                Assert.AreEqual(planet.Weapons, new List<Weapon>());
+            }
+
+            [Test]
+            public void Test_DoesPlanetThrowExceptionForInvalidName()
+            {
+                Assert.Throws<ArgumentException>(() =>
+                {
+                    planet = new Planet("", 10.0);
+                });
+            }
+
+            [Test]
+            public void Test_DoesPlanetThrowExceptionForInvalidBudget()
+            {
+                Assert.Throws<ArgumentException>(() =>
+                {
+                    planet = new Planet("Name", -5);
+                });
+            }
+
+            [Test]
+            public void Test_IsProfitWorking()
+            {
+                planet.Profit(50);
+
+                Assert.AreEqual(60.0, planet.Budget);
+            }
+
+            [Test]
+            public void Test_IsSpendFundsWorkingWithValidValues()
+            {
+                planet.SpendFunds(5.0);
+                Assert.AreEqual(5.0, planet.Budget);
+            }
+
+            [Test]
+            public void Test_IsSpendFundsThrowingWithInvalidValues()
+            {
+                Assert.Throws<InvalidOperationException>(() =>
+                {
+                    planet.SpendFunds(11.0);
+                });
+            }
         }
     }
 }
