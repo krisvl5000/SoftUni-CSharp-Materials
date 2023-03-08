f5769b796b44316b7849f37faf263380602b49ed
Kristian <krisvl5000@gmail.com>
2023-03-08 12:32:42 +0200

    Create Controller.cs

---
+++ 04. C# OOP/Advanced Exam 14 August 2022/Core/Controller.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using PlanetWars.Core.Contracts;
+using PlanetWars.Models.MilitaryUnits;
+using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Models.Planets;
+using PlanetWars.Models.Planets.Contracts;
+using PlanetWars.Repositories;
+using PlanetWars.Repositories.Contracts;
+using PlanetWars.Utilities.Messages;
+
+namespace PlanetWars.Core
+{
+    public class Controller : IController
+    {
+        private IRepository<IPlanet> planetRepository;
+
+        public Controller()
+        {
+            planetRepository = new PlanetRepository();
+        }
+
+        public string CreatePlanet(string name, double budget)
+        {
+            if (planetRepository.Models.Any(x => x.Name == name))
+            {
+                return  String.Format(OutputMessages.ExistingPlanet, name);
+            }
+
+            IPlanet planet = new Planet(name, budget);
+            return String.Format(OutputMessages.NewPlanet, name);
+        }
+
+        public string AddUnit(string unitTypeName, string planetName)
+        {
+            var planet = planetRepository.Models.FirstOrDefault(x => x.Name == unitTypeName);
+
+            if (planet == null)
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.UnexistingPlanet, planetName));
+            }
+
+            if (unitTypeName != "AnonymousImpactUnit" &&
+                unitTypeName != "SpaceForces" &&
+                unitTypeName != "StormTroopers")
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.ItemNotAvailable, unitTypeName));
+            }
+
+            if (planetRepository.Models.Any(x => x.GetType().Name == unitTypeName))
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.UnitAlreadyAdded, unitTypeName,
+                    planetName));
+            }
+
+            IMilitaryUnit unit = null;
+
+            if (unitTypeName == "AnonymousImpactUnit")
+            {
+                unit = new AnonymousImpactUnit();
+            }
+            else if (unitTypeName == "SpaceForces")
+            {
+                unit = new SpaceForces();
+            }
+            else if (unitTypeName == "StormTroopers")
+            {
+                unit = new StormTroopers();
+            }
+
+            planet.AddUnit(unit);
+
+            return String.Format(OutputMessages.UnitAdded, unitTypeName, planetName);
+
+        }
+
+        public string AddWeapon(string planetName, string weaponTypeName, int destructionLevel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SpecializeForces(string planetName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SpaceCombat(string planetOne, string planetTwo)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ForcesReport()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
