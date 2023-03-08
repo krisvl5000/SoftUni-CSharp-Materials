e07e11a589dc87c598d921ba5f0f928b4cc22878
Kristian <krisvl5000@gmail.com>
2023-03-08 10:35:10 +0200

    Update MilitaryUnit.cs

--- 04. C# OOP/Advanced Exam 14 August 2022/Models/MilitaryUnits/MilitaryUnit.cs
+++ 04. C# OOP/Advanced Exam 14 August 2022/Models/MilitaryUnits/MilitaryUnit.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using PlanetWars.Models.MilitaryUnits.Contracts;
+using PlanetWars.Utilities.Messages;
 
 namespace PlanetWars.Models.MilitaryUnits
 {
-    internal class MilitaryUnit
+    public abstract class MilitaryUnit : IMilitaryUnit
     {
+        public MilitaryUnit(double cost)
+        {
+            Cost = cost;
+            EnduranceLevel = 1;
+        }
+
+        public double Cost { get; private set; }
+        public int EnduranceLevel { get; private set; }
+        public void IncreaseEndurance()
+        {
+            EnduranceLevel++;
+
+            if (EnduranceLevel > 20)
+            {
+                EnduranceLevel = 20;
+
+                throw new ArgumentException(String.Format(ExceptionMessages.EnduranceLevelExceeded));
+            }
+        }
     }
 }
