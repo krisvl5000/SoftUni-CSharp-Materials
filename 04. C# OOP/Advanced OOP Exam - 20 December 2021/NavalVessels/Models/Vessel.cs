aedff6804d2c930acc0469f128c8026501389c74
Kristian <krisvl5000@gmail.com>
2023-03-09 10:17:51 +0200

    Update Vessel.cs

--- 04. C# OOP/Advanced OOP Exam - 20 December 2021/NavalVessels/Models/Vessel.cs
+++ 04. C# OOP/Advanced OOP Exam - 20 December 2021/NavalVessels/Models/Vessel.cs
@@ -3,10 +3,109 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NavalVessels.Models.Contracts;
+using NavalVessels.Utilities.Messages;
 
 namespace NavalVessels.Models
 {
-    internal class Vessels
+    public abstract class Vessel : IVessel
     {
+        public Vessel(string name, double mainMainWeaponCaliber, double speed, double armorThickness)
+        {
+            Name = name;
+            MainWeaponCaliber = mainMainWeaponCaliber;
+            Speed = speed;
+            ArmorThickness = armorThickness;
+            Targets = new List<string>();
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(String.Format(ExceptionMessages.InvalidVesselName));
+                }
+                name = value;
+            }
+        }
+
+        private ICaptain captain;
+
+        public ICaptain Captain
+        {
+            get { return captain; }
+            set
+            {
+                if (value == null)
+                {
+                    throw new NullReferenceException(String.Format(ExceptionMessages.InvalidCaptainToVessel));
+                }
+                captain = value;
+            }
+        }
+
+        public double ArmorThickness { get; set; }
+
+        public double MainWeaponCaliber { get; protected set; }
+
+        public double Speed { get; protected set; }
+
+        public ICollection<string> Targets { get; }
+
+        public void Attack(IVessel target)
+        {
+            if (target == null)
+            {
+                throw new NullReferenceException(String.Format(ExceptionMessages.InvalidTarget));
+            }
+
+            target.ArmorThickness -= MainWeaponCaliber;
+
+            if (target.ArmorThickness < 0)
+            {
+                target.ArmorThickness = 0;
+            }
+
+            Targets.Add(target.Name);
+        }
+
+        public abstract void RepairVessel();
+        
+            //if (this.GetType().Name == "Battleship")
+            //{
+            //    ArmorThickness = 300;
+            //}
+            //else if (this.GetType().Name == "Submarine")
+            //{
+            //    ArmorThickness = 200;
+            //}
+        
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"- {Name}");
+            sb.AppendLine($"*Type: {this.GetType().Name}");
+            sb.AppendLine($"*Armor thickness: {ArmorThickness}");
+            sb.AppendLine($"*Main weapon caliber: {MainWeaponCaliber}");
+            sb.AppendLine($"*Speed: {Speed} knots");
+
+            if (Targets.Count > 0)
+            {
+                sb.AppendLine($"Targets: {String.Join(", ", Targets)}");
+            }
+            else
+            {
+                sb.AppendLine("None");
+            }
+
+            return sb.ToString().Trim();
+        }
     }
 }
