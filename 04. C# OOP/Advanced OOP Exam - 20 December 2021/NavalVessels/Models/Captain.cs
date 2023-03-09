99fe8f6ab79a809a5344d2bd386a0bbaba881063
Kristian <krisvl5000@gmail.com>
2023-03-09 11:12:19 +0200

    Update Captain.cs

--- 04. C# OOP/Advanced OOP Exam - 20 December 2021/NavalVessels/Models/Captain.cs
+++ 04. C# OOP/Advanced OOP Exam - 20 December 2021/NavalVessels/Models/Captain.cs
@@ -3,10 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NavalVessels.Models.Contracts;
+using NavalVessels.Utilities.Messages;
 
 namespace NavalVessels.Models
 {
-    internal class Captain
+    public class Captain : ICaptain
     {
+        public Captain(string fullName)
+        {
+            FullName = fullName;
+
+            Vessels = new List<IVessel>();
+            CombatExperience = 0;
+        }
+
+        private string fullName;
+
+        public string FullName
+        {
+            get { return fullName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(String.Format(ExceptionMessages.InvalidCaptainName));
+                }
+                fullName = value;
+            }
+        }
+
+        public int CombatExperience { get; private set; }
+
+        public ICollection<IVessel> Vessels { get; }
+
+        public void AddVessel(IVessel vessel)
+        {
+            if (vessel == null)
+            {
+                throw new NullReferenceException(String.Format(ExceptionMessages.InvalidVesselForCaptain));
+            }
+            Vessels.Add(vessel);
+        }
+
+        public void IncreaseCombatExperience()
+        {
+            CombatExperience += 10;
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{FullName} has {CombatExperience} combat experience and commands {Vessels.Count} vessels");
+
+            if (Vessels.Any())
+            {
+                foreach (var vessel in Vessels)
+                {
+                    sb.AppendLine(vessel.ToString());
+                }
+            }
+
+            return sb.ToString().Trim();
+        }
     }
 }
