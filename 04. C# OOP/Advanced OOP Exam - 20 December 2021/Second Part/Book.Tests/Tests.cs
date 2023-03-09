8f0dbc628cf83bd2f14d1f13f354142955301cdd
Kristian <krisvl5000@gmail.com>
2023-03-09 21:55:58 +0200

    Update Tests.cs

--- 04. C# OOP/Advanced OOP Exam - 20 December 2021/Second Part/Book.Tests/Tests.cs
+++ 04. C# OOP/Advanced OOP Exam - 20 December 2021/Second Part/Book.Tests/Tests.cs
@@ -1,4 +1,6 @@
-﻿namespace Book.Tests
+﻿using NUnit.Framework.Constraints;
+
+namespace Book.Tests
 {
     using System;
 
@@ -18,6 +20,81 @@
         public void Test_IsConstructorWorkingProperly()
         {
             Assert.That(book.BookName == "Name" && book.Author == "Author");
+        }
+
+        [Test]
+        public void Test_IsAddFootNoteWorkingProperly()
+        {
+            book.AddFootnote(5, "Test");
+
+            Assert.That(book.FootnoteCount == 1);
+        }
+
+        [Test]
+        public void Test_IsAddFootNoteThrowingWhenAddingTheSameNumber()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                book.AddFootnote(5, "Test");
+                book.AddFootnote(5, "Test2");
+            });
+        }
+
+        [Test]
+        public void Test_IsBookNameValidationThrowingWithEmptyName()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var newBook = new Book("", "SomeAuthor");
+            });
+        }
+
+        [Test]
+        public void Test_IsAuthorNameValidationThrowingWithEmptyName()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var newBook = new Book("SomeName", "");
+            });
+        }
+
+        [Test]
+        public void Test_IsFindFootNoteWorkingProperly()
+        {
+            book.AddFootnote(1, "SomeFootNote");
+
+            Assert.That(book.FindFootnote(1) == $"Footnote #1: SomeFootNote");
+        }
+
+        [Test]
+        public void Test_IsFindFootNoteThrowingWhenSearchingInvalidNumber()
+        {
+            book.AddFootnote(1, "SomeFootNote");
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                Assert.That(book.FindFootnote(3) == "Footnote doesn't exists!");
+            });
+        }
+
+        [Test]
+        public void Test_IsAlterFootNoteWorkingProperly()
+        {
+            book.AddFootnote(1, "SomeFootNote");
+            book.AlterFootnote(1, "NewFootNote");
+
+            Assert.That(book.FindFootnote(1) == $"Footnote #1: NewFootNote");
+        }
+
+        [Test]
+        public void Test_IsAlterFootNoteThrowingWhenNonExistentNoteIsSearched()
+        {
+            book.AddFootnote(1, "SomeFootNote");
+            
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                book.AlterFootnote(10, "NewFootNote");
+            });
         }
     }
 }
