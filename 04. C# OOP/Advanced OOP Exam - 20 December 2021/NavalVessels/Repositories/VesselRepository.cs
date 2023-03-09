c7c33496beefef8b11074455db5b176f4032fe95
Kristian <krisvl5000@gmail.com>
2023-03-09 11:12:21 +0200

    Update VesselRepository.cs

--- 04. C# OOP/Advanced OOP Exam - 20 December 2021/NavalVessels/Repositories/VesselRepository.cs
+++ 04. C# OOP/Advanced OOP Exam - 20 December 2021/NavalVessels/Repositories/VesselRepository.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NavalVessels.Models.Contracts;
+using NavalVessels.Repositories.Contracts;
 
 namespace NavalVessels.Repositories
 {
-    internal class VesselRepository
+    public class VesselRepository : IRepository<IVessel>
     {
+        public VesselRepository()
+        {
+            models = new List<IVessel>();
+        }
+
+        private List<IVessel> models;
+
+        public IReadOnlyCollection<IVessel> Models => models.AsReadOnly();
+
+        public void Add(IVessel model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IVessel model)
+        {
+            var vesselToRemove = models.FirstOrDefault(x => x.Name == model.Name);
+
+            if (vesselToRemove == null)
+            {
+                return false;
+            }
+
+            models.Remove(vesselToRemove);
+            return true;
+        }
+
+        public IVessel FindByName(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
     }
 }
