49e4aa60fc9ff6a8da2fbdb74dab0e0cf7ae420f
Kristian <krisvl5000@gmail.com>
2023-03-09 20:34:28 +0200

    Update Tests.cs

--- 04. C# OOP/Advanced OOP Exam - 20 December 2021/Second Part/Book.Tests/Tests.cs
+++ 04. C# OOP/Advanced OOP Exam - 20 December 2021/Second Part/Book.Tests/Tests.cs
@@ -6,5 +6,18 @@
 
     public class Tests
     {
+        private Book book;
+
+        [SetUp]
+        public void SetUp()
+        {
+            book = new Book("Name", "Author");
+        }
+
+        [Test]
+        public void Test_IsConstructorWorkingProperly()
+        {
+            Assert.That(book.BookName == "Name" && book.Author == "Author");
+        }
     }
 }
