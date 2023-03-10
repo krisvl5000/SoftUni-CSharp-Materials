9245e7b3702ab805e5ea8f69c61be4a4182011b6
Kristian <krisvl5000@gmail.com>
2023-03-10 11:01:36 +0200

    Create HotelRepository.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2022/Repositories/HotelRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Repositories.Contracts;
+
+namespace BookingApp.Repositories
+{
+    public class HotelRepository : IRepository<IHotel>
+    {
+        private List<IHotel> hotels;
+
+        public HotelRepository()
+        {
+            hotels = new List<IHotel>();
+        }
+
+        public void AddNew(IHotel model)
+        {
+            hotels.Add(model);
+        }
+
+        public IHotel Select(string criteria)
+        {
+            return hotels.FirstOrDefault(x => x.FullName == criteria);
+        }
+
+        public IReadOnlyCollection<IHotel> All() => hotels.AsReadOnly();
+    }
+}
