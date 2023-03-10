0baf54ee7e5e39b0637b67aca87ef631a5550385
Kristian <krisvl5000@gmail.com>
2023-03-10 10:23:29 +0200

    Update Booking.cs

--- 04. C# OOP/Advanced OOP Exam - 22 August 2022/Models/Bookings/Booking.cs
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2022/Models/Bookings/Booking.cs
@@ -1,12 +1,91 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Intrinsics.X86;
 using System.Text;
 using System.Threading.Tasks;
+using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Models.Rooms;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Utilities.Messages;
 
 namespace BookingApp.Models.Bookings
 {
-    internal class Booking
+    public class Booking : IBooking
     {
+        public Booking(IRoom room, int residenceDuration, int adultsCount, int childrenCount, int bookingNumber)
+        {
+            Room = room;
+            ResidenceDuration = residenceDuration;
+            AdultsCount = adultsCount;
+            ChildrenCount = childrenCount;
+            BookingNumber = bookingNumber;
+        }
+
+        public IRoom Room { get; private set; }
+
+        private int residenceDuration;
+
+        public int ResidenceDuration
+        {
+            get { return residenceDuration; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.DurationZeroOrLess));
+                }
+                residenceDuration = value;
+            }
+        }
+
+        private int adultsCount;
+
+        public int AdultsCount
+        {
+            get { return adultsCount; }
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.AdultsZeroOrLess));
+                }
+                adultsCount = value;
+            }
+        }
+
+        private int childrenCount;
+
+        public int ChildrenCount
+        {
+            get { return childrenCount; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.ChildrenNegative));
+                }
+                childrenCount = value;
+            }
+        }
+
+        public int BookingNumber { get; private set; }
+
+        public string BookingSummary()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Booking number: {BookingNumber}");
+            sb.AppendLine($"Room type: {Room.GetType().Name}");
+            sb.AppendLine($"Adults: {AdultsCount} Children: {ChildrenCount}");
+            sb.AppendLine($"Total amount paid: {CalculateTotalAmountPaid():F2}");
+
+            return sb.ToString().Trim();
+        }
+
+        internal double CalculateTotalAmountPaid()
+        {
+            return Math.Round(ResidenceDuration * Room.PricePerNight, 2);
+        }
     }
 }
