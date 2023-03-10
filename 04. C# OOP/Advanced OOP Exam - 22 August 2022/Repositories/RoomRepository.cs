b33610911871ea647a2d0f4b4caebf2ee5ea4140
Kristian <krisvl5000@gmail.com>
2023-03-10 11:01:35 +0200

    Update RoomRepository.cs

--- 04. C# OOP/Advanced OOP Exam - 22 August 2022/Repositories/RoomRepository.cs
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2022/Repositories/RoomRepository.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Repositories.Contracts;
 
 namespace BookingApp.Repositories
 {
-    internal class RoomRepository
+    public class RoomRepository : IRepository<IRoom>
     {
+        private List<IRoom> rooms;
+        public RoomRepository()
+        {
+            rooms = new List<IRoom>();
+        }
+
+        public void AddNew(IRoom model)
+        {
+            rooms.Add(model);
+        }
+
+        public IRoom Select(string criteria)
+        {
+            return rooms.FirstOrDefault(x => x.GetType().Name == criteria);
+        }
+
+        public IReadOnlyCollection<IRoom> All() => rooms.AsReadOnly();
     }
 }
