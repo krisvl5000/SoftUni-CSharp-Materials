c8dc381442bd8ef0e8992ba4aea1c8c672227f0a
Kristian <krisvl5000@gmail.com>
2023-03-10 11:15:33 +0200

    Create Controller.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2022/Core/Controller.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingApp.Core.Contracts;
+
+namespace BookingApp.Core
+{
+    public class Controller : IController
+    {
+        public Controller()
+        {
+            
+        }
+
+        public string AddHotel(string hotelName, int category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UploadRoomTypes(string hotelName, string roomTypeName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SetRoomPrices(string hotelName, string roomTypeName, double price)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string BookAvailableRoom(int adults, int children, int duration, int category)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string HotelReport(string hotelName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
