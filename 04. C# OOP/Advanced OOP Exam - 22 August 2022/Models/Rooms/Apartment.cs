4fa476941c6daafbdd958819965476ad8498d9cd
Kristian <krisvl5000@gmail.com>
2023-03-10 10:23:34 +0200

    Update Apartment.cs

--- 04. C# OOP/Advanced OOP Exam - 22 August 2022/Models/Rooms/Apartment.cs
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2022/Models/Rooms/Apartment.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookingApp.Models.Rooms.Contracts;
 
 namespace BookingApp.Models.Rooms
 {
-    internal class Apartment
+    public class Apartment : Room
     {
+        private const int BED_CAPACITY = 6;
+        public Apartment() : base(BED_CAPACITY)
+        {
+
+        }
     }
 }
