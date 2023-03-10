d3cb1045c3647af33de4313c0b9a78620775cb04
Kristian <krisvl5000@gmail.com>
2023-03-10 15:35:27 +0200

    Update Controller.cs

--- 04. C# OOP/Advanced OOP Exam - 22 August 2022/Core/Controller.cs
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2022/Core/Controller.cs
@@ -4,24 +4,48 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using BookingApp.Core.Contracts;
+using BookingApp.Models.Hotels;
+using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Repositories;
+using BookingApp.Utilities.Messages;
 
 namespace BookingApp.Core
 {
     public class Controller : IController
     {
+        private HotelRepository hotels;
         public Controller()
         {
-            
+            hotels = new HotelRepository();
         }
 
         public string AddHotel(string hotelName, int category)
         {
-            throw new NotImplementedException();
+            var hotel = new Hotel(hotelName, category);
+
+            if (hotels.All().Any(x => x.FullName == hotelName))
+            {
+                return String.Format(OutputMessages.HotelAlreadyRegistered, hotelName);
+            }
+
+            hotels.AddNew(hotel);
+
+            return String.Format(OutputMessages.HotelSuccessfullyRegistered, category, hotelName);
         }
 
         public string UploadRoomTypes(string hotelName, string roomTypeName)
         {
-            throw new NotImplementedException();
+            if (hotels.All().All(x => x.FullName != hotelName))
+            {
+                return String.Format(OutputMessages.HotelNameInvalid, hotelName);
+            }
+
+            var hotel = hotels.All().FirstOrDefault(x => x.FullName == hotelName);
+
+            if (hotel.Rooms.All().Any(x => x.GetType().Name == roomTypeName))
+            {
+                
+            }
         }
 
         public string SetRoomPrices(string hotelName, string roomTypeName, double price)
