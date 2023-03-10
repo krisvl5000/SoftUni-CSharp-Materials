3966220e193a44fed12f4d7a3ee824c9d9d8578a
Kristian <krisvl5000@gmail.com>
2023-03-10 11:01:38 +0200

    Create BookingRepository.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2022/Repositories/BookingRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Repositories.Contracts;
+
+namespace BookingApp.Repositories
+{
+    public class BookingRepository : IRepository<IBooking>
+    {
+        private List<IBooking> bookings;
+        public BookingRepository()
+        {
+            bookings = new List<IBooking>();
+        }
+
+        public void AddNew(IBooking model)
+        {
+            bookings.Add(model);
+        }
+
+        public IBooking Select(string criteria)
+        {
+            return bookings.FirstOrDefault(x => x.BookingNumber == int.Parse(criteria));
+        }
+
+        public IReadOnlyCollection<IBooking> All() => bookings.ToList();
+    }
+}
