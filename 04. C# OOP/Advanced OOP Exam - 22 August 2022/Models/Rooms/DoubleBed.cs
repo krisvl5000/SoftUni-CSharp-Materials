b39a2582af67dd60a8007fac25ea8bc3f8d26190
Kristian <krisvl5000@gmail.com>
2023-03-10 09:41:20 +0200

    Create DoubleBed.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2022/Models/Rooms/DoubleBed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Models.Rooms
+{
+    internal class DoubleBed
+    {
+    }
+}
