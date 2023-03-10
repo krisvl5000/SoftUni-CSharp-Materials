619f1bba4066deffdbdcfcb0dd97f1caaa0ef2b2
Kristian <krisvl5000@gmail.com>
2023-03-10 10:23:31 +0200

    Update Room.cs

--- 04. C# OOP/Advanced OOP Exam - 22 August 2022/Models/Rooms/Room.cs
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2022/Models/Rooms/Room.cs
@@ -3,10 +3,40 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Utilities.Messages;
 
 namespace BookingApp.Models.Rooms
 {
-    internal class Room
+    public abstract class Room : IRoom
     {
+        public Room(int bedCapacity)
+        {
+            BedCapacity = bedCapacity;
+            PricePerNight = 0;
+        }
+
+        public int BedCapacity { get; }
+
+        private double pricePerNight;
+
+        public double PricePerNight
+        {
+            get { return pricePerNight; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.PricePerNightNegative));
+                }
+
+                pricePerNight = value;
+            }
+        }
+
+        public void SetPrice(double price)
+        {
+            PricePerNight = price;
+        }
     }
 }
