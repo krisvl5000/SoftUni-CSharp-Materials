afe3f53bfa7e767cc42a2121724382591f866fdb
Kristian <krisvl5000@gmail.com>
2023-03-10 10:23:36 +0200

    Update DoubleBed.cs

--- 04. C# OOP/Advanced OOP Exam - 22 August 2022/Models/Rooms/DoubleBed.cs
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2022/Models/Rooms/DoubleBed.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace BookingApp.Models.Rooms
 {
-    internal class DoubleBed
+    public class DoubleBed : Room
     {
+        private const int BED_CAPACITY = 2;
+        public DoubleBed() : base(BED_CAPACITY)
+        {
+
+        }
     }
 }
