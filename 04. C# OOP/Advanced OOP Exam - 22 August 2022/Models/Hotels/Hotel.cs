695f04c104567743e1640ea6f767b6271f4b4afb
Kristian <krisvl5000@gmail.com>
2023-03-10 10:23:38 +0200

    Create Hotel.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2022/Models/Hotels/Hotel.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using BookingApp.Models.Bookings;
+using BookingApp.Models.Bookings.Contracts;
+using BookingApp.Models.Hotels.Contacts;
+using BookingApp.Models.Rooms;
+using BookingApp.Models.Rooms.Contracts;
+using BookingApp.Repositories.Contracts;
+using BookingApp.Utilities.Messages;
+
+namespace BookingApp.Models.Hotels
+{
+    public class Hotel : IHotel
+    {
+        private ICollection<IRoom> rooms;
+        private ICollection<IBooking> bookings;
+
+        public Hotel(string fullName, int category)
+        {
+            FullName = fullName;
+            Category = category;
+
+            rooms = new List<IRoom>();
+            bookings = new List<IBooking>();
+        }
+
+        private string fullName;
+
+        public string FullName
+        {
+            get { return fullName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(String.Format(ExceptionMessages.HotelNameNullOrEmpty));
+                }
+                fullName = value;
+            }
+        }
+
+        private int category;
+
+        public int Category
+        {
+            get { return category; }
+            private set
+            {
+                if (value < 1 || value > 5)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidCategory));
+                }
+                category = value;
+            }
+        }
+
+        public double Turnover
+        {
+            get;
+        }
+
+        public IRepository<IRoom> Rooms { get; }
+        public IRepository<IBooking> Bookings { get; }
+    }
+}
