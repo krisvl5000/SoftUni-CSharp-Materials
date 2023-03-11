4abfbce73d4096e56160ff4508963180ab08eb4a
Kristian <krisvl5000@gmail.com>
2023-03-11 12:22:03 +0200

    Update Map.cs

--- 04. C# OOP/Advanced OOP Exam - 18 April 2022/First Part/Skeleton/Heroes/Models/Map/Map.cs
+++ 04. C# OOP/Advanced OOP Exam - 18 April 2022/First Part/Skeleton/Heroes/Models/Map/Map.cs
@@ -1,10 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Heroes.Models.Contracts;
 
 namespace Heroes.Models.Map
 {
-    internal class Map
+    public class Map : IMap
     {
+        public string Fight(ICollection<IHero> players)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
