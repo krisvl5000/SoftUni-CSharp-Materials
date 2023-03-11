2f35bfa3ae54f2df51e94af544de95406d14a7a0
Kristian <krisvl5000@gmail.com>
2023-03-11 13:30:56 +0200

    Update RepairsShopTests.cs

--- 04. C# OOP/Advanced OOP Exam - 18 April 2022/Second Part/RepairShop/RepairShop.Tests/RepairsShopTests.cs
+++ 04. C# OOP/Advanced OOP Exam - 18 April 2022/Second Part/RepairShop/RepairShop.Tests/RepairsShopTests.cs
@@ -1,3 +1,4 @@
+using System;
 using NUnit.Framework;
 
 namespace RepairShop.Tests
@@ -6,16 +7,62 @@ namespace RepairShop.Tests
     {
         public class RepairsShopTests
         {
+            private Car car;
+            private Garage garage;
+
             [SetUp]
             public void SetUp()
             {
-
+                car = new Car("Model", 5);
+                garage = new Garage("Garage", 1);
             }
 
             [Test]
-            public void Test_IsTheConstructorWorking()
+            public void Test_IsTheGarageConstructorWorking()
             {
+                Assert.That(garage.Name == "Garage" && garage.MechanicsAvailable == 5);
+            }
 
+            [Test]
+            public void Test_IsTheCarListInstantiated()
+            {
+                Assert.That(garage.CarsInGarage == 0);
+            }
+
+            [Test]
+            public void Test_AreTheCarsProperlyAdded()
+            {
+                garage.AddCar(car);
+                Assert.That(garage.CarsInGarage == 1);
+            }
+
+            [Test]
+            public void Test_DoesItThrowForInvalidName()
+            {
+                Assert.Throws<ArgumentNullException>(() =>
+                {
+                    garage = new Garage("", 5);
+                });
+            }
+
+            [Test]
+            public void Test_DoesItThrowForInvalidMechanics()
+            {
+                Assert.Throws<ArgumentException>(() =>
+                {
+                    garage = new Garage("Name", -5);
+                });
+            }
+
+            [Test]
+            public void Test_DoesItThrowWhenThereAreNoAvailableMechanics()
+            {
+                garage.AddCar(car);
+
+                Assert.Throws<InvalidOperationException>(() =>
+                {
+                    garage.AddCar(car);
+                });
             }
         }
     }
