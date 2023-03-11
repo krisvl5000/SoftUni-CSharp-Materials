bbe3a133990f7e2b99aad6a208804c94f022fca2
Kristian <krisvl5000@gmail.com>
2023-03-11 12:51:26 +0200

    Update Map.cs

--- 04. C# OOP/Advanced OOP Exam - 18 April 2022/First Part/Skeleton/Heroes/Models/Map/Map.cs
+++ 04. C# OOP/Advanced OOP Exam - 18 April 2022/First Part/Skeleton/Heroes/Models/Map/Map.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using Heroes.Models.Contracts;
 
@@ -9,7 +10,48 @@ namespace Heroes.Models.Map
     {
         public string Fight(ICollection<IHero> players)
         {
-            throw new NotImplementedException();
+            var barbarians = players.Where(x => x.GetType().Name == "Barbarian");
+
+            var knights = players.Where(x => x.GetType().Name == "Knight");
+
+            int knightCasultyCounter = 0;
+            int barbarianCasualtyCounter = 0;
+
+            while (barbarians.Any(x => x.IsAlive) && knights.Any(x => x.IsAlive))
+            {
+                foreach (var knight in knights.Where(x => x.IsAlive))
+                {
+                    foreach (var barbarian in barbarians)
+                    {
+                        barbarian.TakeDamage(knight.Weapon.DoDamage());
+
+                        if (!barbarian.IsAlive)
+                        {
+                            barbarianCasualtyCounter++;
+                        }
+                    }
+                }
+
+                foreach (var barbarian1 in barbarians.Where(x => x.IsAlive))
+                {
+                    foreach (var knight1 in knights)
+                    {
+                        knight1.TakeDamage(barbarian1.Weapon.DoDamage());
+
+                        if (!knight1.IsAlive)
+                        {
+                            knightCasultyCounter++;
+                        }
+                    }
+                }
+            }
+
+            if (!barbarians.Any())
+            {
+                return $"The knights took {knightCasultyCounter} casualties but won the battle.";
+            }
+
+            return $"The barbarians took {barbarianCasualtyCounter} casualties but won the battle.";
         }
     }
 }
