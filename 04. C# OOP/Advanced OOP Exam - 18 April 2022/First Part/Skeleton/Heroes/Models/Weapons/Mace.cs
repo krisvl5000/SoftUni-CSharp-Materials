6e5df851601f5613933363748aaa35d3ba4a224a
Kristian <krisvl5000@gmail.com>
2023-03-11 12:11:36 +0200

    Create Mace.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 18 April 2022/First Part/Skeleton/Heroes/Models/Weapons/Mace.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Heroes.Models.Weapons
+{
+    public class Mace : Weapon
+    {
+        public Mace(string name, int durability) : base(name, durability)
+        {
+
+        }
+
+        public override int DoDamage()
+        {
+            Durability -= 1;
+
+            if (Durability <= 0)
+            {
+                return 0;
+            }
+
+            return 25;
+        }
+    }
+}
