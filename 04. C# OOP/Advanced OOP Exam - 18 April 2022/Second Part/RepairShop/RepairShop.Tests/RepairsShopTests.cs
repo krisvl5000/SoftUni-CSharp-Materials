bea78e6fb4a2af2ec68bc41ff148af15efbf8a52
Kristian <krisvl5000@gmail.com>
2023-03-11 13:16:17 +0200

    Update RepairsShopTests.cs

--- 04. C# OOP/Advanced OOP Exam - 18 April 2022/Second Part/RepairShop/RepairShop.Tests/RepairsShopTests.cs
+++ 04. C# OOP/Advanced OOP Exam - 18 April 2022/Second Part/RepairShop/RepairShop.Tests/RepairsShopTests.cs
@@ -6,7 +6,17 @@ namespace RepairShop.Tests
     {
         public class RepairsShopTests
         {
-           
+            [SetUp]
+            public void SetUp()
+            {
+
+            }
+
+            [Test]
+            public void Test_IsTheConstructorWorking()
+            {
+
+            }
         }
     }
 }
