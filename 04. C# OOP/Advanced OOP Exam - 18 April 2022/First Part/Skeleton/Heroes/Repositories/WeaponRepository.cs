adcfb8a3106ea4493d551c0d678bd9bc64bf1559
Kristian <krisvl5000@gmail.com>
2023-03-11 12:51:28 +0200

    Create WeaponRepository.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 18 April 2022/First Part/Skeleton/Heroes/Repositories/WeaponRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
+
+namespace Heroes.Repositories
+{
+    internal class WeaponRepository : IRepository<IWeapon>
+    {
+        private List<IWeapon> models;
+
+        public WeaponRepository()
+        {
+            models = new List<IWeapon>();
+        }
+
+        public IReadOnlyCollection<IWeapon> Models => models.AsReadOnly();
+
+        public void Add(IWeapon model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IWeapon model)
+        {
+            var weaponToRemove = models.FirstOrDefault(x => x.Name == model.Name);
+
+            if (weaponToRemove == null)
+            {
+                return false;
+            }
+
+            models.Remove(weaponToRemove);
+            return true;
+        }
+
+        public IWeapon FindByName(string name) => models.FirstOrDefault(x => x.Name == name);
+    }
+}
