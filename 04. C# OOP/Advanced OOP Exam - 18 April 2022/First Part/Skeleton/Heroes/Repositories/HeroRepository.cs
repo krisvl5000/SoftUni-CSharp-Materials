994e40f0dccf0702c3f2f08d284608f1ab781062
Kristian <krisvl5000@gmail.com>
2023-03-11 12:22:05 +0200

    Update HeroRepository.cs

--- 04. C# OOP/Advanced OOP Exam - 18 April 2022/First Part/Skeleton/Heroes/Repositories/HeroRepository.cs
+++ 04. C# OOP/Advanced OOP Exam - 18 April 2022/First Part/Skeleton/Heroes/Repositories/HeroRepository.cs
@@ -1,10 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Heroes.Models.Contracts;
+using Heroes.Repositories.Contracts;
 
 namespace Heroes.Repositories
 {
-    internal class HeroRepository
+    public class HeroRepository : IRepository<IHero>
     {
+        private List<IHero> models;
+
+        public HeroRepository()
+        {
+            models = new List<IHero>();
+        }
+
+        public IReadOnlyCollection<IHero> Models => models.AsReadOnly();
+
+        public void Add(IHero model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IHero model)
+        {
+            var heroToRemove = models.FirstOrDefault(x => x.Name == model.Name);
+
+            if (heroToRemove == null)
+            {
+                return false;
+            }
+
+            models.Remove(heroToRemove);
+            return true;
+        }
+
+        public IHero FindByName(string name) => models.FirstOrDefault(x => x.Name == name);
     }
 }
