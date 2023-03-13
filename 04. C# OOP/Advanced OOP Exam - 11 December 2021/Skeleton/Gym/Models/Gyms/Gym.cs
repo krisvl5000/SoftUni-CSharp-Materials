31de052d7261c06aa0990b7fb5ed18dbdb04ae67
Kristian <krisvl5000@gmail.com>
2023-03-13 10:56:52 +0200

    Update Gym.cs

--- 04. C# OOP/Advanced OOP Exam - 11 December 2021/Skeleton/Gym/Models/Gyms/Gym.cs
+++ 04. C# OOP/Advanced OOP Exam - 11 December 2021/Skeleton/Gym/Models/Gyms/Gym.cs
@@ -1,10 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Gym.Models.Athletes.Contracts;
+using Gym.Models.Equipment.Contracts;
+using Gym.Models.Gyms.Contracts;
+using Gym.Repositories;
+using Gym.Repositories.Contracts;
+using Gym.Utilities.Messages;
 
 namespace Gym.Models.Gyms
 {
-    internal class Gym
+    public abstract class Gym : IGym
     {
+        public Gym()
+        {
+            athletes = new List<IAthlete>();
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidGymName));
+                }
+                name = value;
+            }
+        }
+
+        public int Capacity { get; }
+
+        public double EquipmentWeight => equipment.Sum(x => x.Weight);
+
+        private List<IEquipment> equipment;
+        public ICollection<IEquipment> Equipment => equipment;
+
+        private List<IAthlete> athletes;
+        public ICollection<IAthlete> Athletes => athletes;
+
+        public void AddAthlete(IAthlete athlete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool RemoveAthlete(IAthlete athlete)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddEquipment(IEquipment equipment)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Exercise()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GymInfo()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
