7f58e90361ad790b23ac9482fea6cff7baa5f4c3
Kristian <krisvl5000@gmail.com>
2023-03-13 10:56:54 +0200

    Update Boxer.cs

--- 04. C# OOP/Advanced OOP Exam - 11 December 2021/Skeleton/Gym/Models/Athletes/Boxer.cs
+++ 04. C# OOP/Advanced OOP Exam - 11 December 2021/Skeleton/Gym/Models/Athletes/Boxer.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Gym.Utilities.Messages;
 
 namespace Gym.Models.Athletes
 {
-    internal class Boxer
+    public class Boxer : Athlete
     {
+        private const int INITIAL_STAMINA = 60;
+        public Boxer(string fullName, string motivation, int numberOfMedals) : base(fullName, motivation, numberOfMedals, INITIAL_STAMINA)
+        {
+        }
+
+        public override void Exercise()
+        {
+            Stamina += 15;
+
+            if (Stamina > 100)
+            {
+                Stamina = 100;
+
+                throw new ArgumentException(String.Format(ExceptionMessages.InvalidStamina));
+            }
+        }
     }
 }
