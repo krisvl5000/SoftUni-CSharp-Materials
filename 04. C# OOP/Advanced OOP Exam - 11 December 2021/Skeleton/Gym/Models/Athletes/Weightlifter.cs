5a6944e758bf37cda19e8b083e9557cd3dca4e41
Kristian <krisvl5000@gmail.com>
2023-03-13 10:56:48 +0200

    Update Weightlifter.cs

--- 04. C# OOP/Advanced OOP Exam - 11 December 2021/Skeleton/Gym/Models/Athletes/Weightlifter.cs
+++ 04. C# OOP/Advanced OOP Exam - 11 December 2021/Skeleton/Gym/Models/Athletes/Weightlifter.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Gym.Utilities.Messages;
 
 namespace Gym.Models.Athletes
 {
-    internal class Weightlifter
+    public class Weightlifter : Athlete
     {
+        private const int INITIAL_STAMINA = 50;
+
+        public Weightlifter(string fullName, string motivation, int numberOfMedals) : base(fullName, motivation, numberOfMedals, INITIAL_STAMINA)
+        {
+        }
+
+        public override void Exercise() // only in weightlifting gym
+        {
+            Stamina += 10;
+
+            if (Stamina > 100)
+            {
+                Stamina = 100;
+
+                throw new ArgumentException(String.Format(ExceptionMessages.InvalidStamina));
+            }
+        }
     }
 }
