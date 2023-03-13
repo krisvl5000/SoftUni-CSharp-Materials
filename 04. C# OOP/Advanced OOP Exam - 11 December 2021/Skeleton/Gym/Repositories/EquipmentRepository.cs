c3916b27b32918d6112f918a68af5b62f6c28f16
Kristian <krisvl5000@gmail.com>
2023-03-13 10:56:56 +0200

    Update EquipmentRepository.cs

--- 04. C# OOP/Advanced OOP Exam - 11 December 2021/Skeleton/Gym/Repositories/EquipmentRepository.cs
+++ 04. C# OOP/Advanced OOP Exam - 11 December 2021/Skeleton/Gym/Repositories/EquipmentRepository.cs
@@ -1,10 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Gym.Models.Equipment.Contracts;
+using Gym.Repositories.Contracts;
 
 namespace Gym.Repositories
 {
-    internal class EquipmentRepository
+    public class EquipmentRepository : IRepository<IEquipment>
     {
+        private List<IEquipment> models;
+        public EquipmentRepository()
+        {
+            models = new List<IEquipment>();
+        }
+
+        public IReadOnlyCollection<IEquipment> Models => models.AsReadOnly();
+
+        public void Add(IEquipment model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IEquipment model)
+        {
+            if (models.Contains(model))
+            {
+                models.Remove(model);
+                return true;
+            }
+
+            return false;
+        }
+
+        public IEquipment FindByType(string type) => models.FirstOrDefault(x => x.GetType().Name == type);
     }
 }
