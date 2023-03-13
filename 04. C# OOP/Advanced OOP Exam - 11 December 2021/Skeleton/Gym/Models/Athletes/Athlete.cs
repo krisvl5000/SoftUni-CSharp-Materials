9ba6fd7b6ca73fddc7b8ed0684c046bba99c872f
Kristian <krisvl5000@gmail.com>
2023-03-13 10:56:43 +0200

    Update Athlete.cs

--- 04. C# OOP/Advanced OOP Exam - 11 December 2021/Skeleton/Gym/Models/Athletes/Athlete.cs
+++ 04. C# OOP/Advanced OOP Exam - 11 December 2021/Skeleton/Gym/Models/Athletes/Athlete.cs
@@ -1,10 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Gym.Models.Athletes.Contracts;
+using Gym.Utilities.Messages;
 
 namespace Gym.Models.Athletes
 {
-    internal class Athlete
+    public abstract class Athlete : IAthlete
     {
+        public Athlete(string fullName, string motivation, int numberOfMedals, int stamina)
+        {
+            FullName = fullName;
+            Motivation = motivation;
+            NumberOfMedals = numberOfMedals;
+            Stamina = stamina;
+        }
+
+        private string fullName;
+
+        public string FullName
+        {
+            get { return fullName; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidAthleteName));
+                }
+                fullName = value;
+            }
+        }
+
+        private string motivation;
+
+        public string Motivation
+        {
+            get { return motivation; }
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidAthleteMotivation));
+                }
+                motivation = value;
+            }
+        }
+
+        public int Stamina { get; protected set; }
+
+        private int numberOfMedals;
+
+        public int NumberOfMedals
+        {
+            get { return numberOfMedals; }
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidAthleteMedals));
+                }
+                numberOfMedals = value;
+            }
+        }
+
+        public abstract void Exercise();
     }
 }
