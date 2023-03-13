5527a387e457beb18a6a83f92b05c0cdd5ddea37
Kristian <krisvl5000@gmail.com>
2023-03-13 17:09:23 +0200

    Update GymsTests.cs

--- 04. C# OOP/Advanced OOP Exam - 11 December 2021/Second Part/UnitTests-Skeleton/Gyms.Tests/GymsTests.cs
+++ 04. C# OOP/Advanced OOP Exam - 11 December 2021/Second Part/UnitTests-Skeleton/Gyms.Tests/GymsTests.cs
@@ -1,7 +1,116 @@
-﻿namespace Gyms.Tests
+﻿using System;
+using NUnit.Framework;
+
+namespace Gyms.Tests
 {
     public class GymsTests
     {
-        // Implement unit tests here
+        private Athlete athlete;
+        private Gym gym;
+
+        [SetUp]
+        public void SetUp()
+        {
+            athlete = new Athlete("Athlete");
+            gym = new Gym("Gym", 3);
+        }
+
+        [Test]
+        public void Test_IsConstructorWorking()
+        {
+            Assert.That(gym.Name == "Gym" && gym.Capacity == 3);
+        }
+
+        [Test]
+        public void Test_IsGymThrowingWhenInvalidNameIsGiven()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                gym = new Gym("", 3);
+            });
+        }
+
+        [Test]
+        public void Test_IsGymThrowingWhenInvalidCapacityIsGiven()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                gym = new Gym("Gym", -1);
+            });
+        }
+
+        [Test]
+        public void Test_CountWorking()
+        {
+            Assert.That(gym.Count == 0);
+        }
+
+        [Test]
+        public void Test_IsAddAthleteWorking()
+        {
+            gym.AddAthlete(athlete);
+            Assert.That(gym.Count == 1);
+        }
+
+        [Test]
+        public void Test_IsAddAthleteThrowingWhenMoreThanCapAreAddedWorking()
+        {
+            gym.AddAthlete(athlete);
+            gym.AddAthlete(athlete);
+            gym.AddAthlete(athlete);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                gym.AddAthlete(athlete);
+            });
+        }
+
+        [Test]
+        public void Test_IsRemoveAthleteWorking()
+        {
+            gym.AddAthlete(athlete);
+            gym.RemoveAthlete("Athlete");
+
+            Assert.That(gym.Count == 0);
+        }
+
+        [Test]
+        public void Test_IsRemoveAthleteThrowingWhenNonExistentIsSearched()
+        {
+            gym.AddAthlete(athlete);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                gym.RemoveAthlete("Name");
+            });
+        }
+
+        [Test]
+        public void Test_IsInjureAthleteWorking()
+        {
+            gym.AddAthlete(athlete);
+            gym.InjureAthlete("Athlete");
+
+            Assert.That(athlete.IsInjured);
+        }
+
+        [Test]
+        public void Test_IsInjureAthleteThrowingWhenNonExistentIsSearched()
+        {
+            gym.AddAthlete(athlete);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                gym.InjureAthlete("Name");
+            });
+        }
+
+        [Test]
+        public void Test_IsReportWorking()
+        {
+            gym.AddAthlete(athlete);
+
+            Assert.That(gym.Report() == $"Active athletes at Gym: Athlete");
+        }
     }
 }
