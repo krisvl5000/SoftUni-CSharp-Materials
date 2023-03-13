1a8b3e4f5007cef50cfa373a6867c03c1900146a
Kristian <krisvl5000@gmail.com>
2023-03-13 11:19:24 +0200

    Create Controller.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 11 December 2021/Skeleton/Gym/Core/Controller.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Gym.Core.Contracts;
+
+namespace Gym.Core
+{
+    public class Controller : IController
+    {
+        public Controller()
+        {
+            
+        }
+
+        public string AddGym(string gymType, string gymName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddEquipment(string equipmentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string InsertEquipment(string gymName, string equipmentType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddAthlete(string gymName, string athleteType, string athleteName, string motivation, int numberOfMedals)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TrainAthletes(string gymName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string EquipmentWeight(string gymName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Report()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
