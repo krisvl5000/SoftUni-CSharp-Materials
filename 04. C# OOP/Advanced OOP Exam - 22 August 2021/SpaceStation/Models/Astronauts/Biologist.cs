9db4585028eb77925052e0ea4af64399cce04528
Kristian <krisvl5000@gmail.com>
2023-03-14 09:25:47 +0200

    Create Biologist.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2021/SpaceStation/Models/Astronauts/Biologist.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SpaceStation.Models.Astronauts
+{
+    public class Biologist : Astronaut
+    {
+        private const double INITIAL_OXYGEN = 70;
+        public Biologist(string name) : base(name, INITIAL_OXYGEN)
+        {
+        }
+
+        public override void Breath()
+        {
+            Oxygen -= 5;
+
+            if (Oxygen < 0)
+            {
+                Oxygen = 0;
+            }
+        }
+    }
+}
