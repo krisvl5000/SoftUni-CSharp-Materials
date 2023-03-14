0c8d789bb1674fd2788294330ed22d87b8428308
Kristian <krisvl5000@gmail.com>
2023-03-14 09:55:05 +0200

    Update Backpack.cs

--- 04. C# OOP/Advanced OOP Exam - 22 August 2021/SpaceStation/Models/Bags/Backpack.cs
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2021/SpaceStation/Models/Bags/Backpack.cs
@@ -1,10 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SpaceStation.Models.Bags.Contracts;
 
 namespace SpaceStation.Models.Bags
 {
-    internal class Backpack
+    public class Backpack : IBag
     {
+        public Backpack()
+        {
+            Items = new List<string>();
+        }
+
+        public ICollection<string> Items { get; }
     }
 }
