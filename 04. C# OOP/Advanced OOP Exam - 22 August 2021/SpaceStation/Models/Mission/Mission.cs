47d64e4e552fa38fed22c789638c17857bdb744a
Kristian <krisvl5000@gmail.com>
2023-03-14 09:55:11 +0200

    Update Mission.cs

--- 04. C# OOP/Advanced OOP Exam - 22 August 2021/SpaceStation/Models/Mission/Mission.cs
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2021/SpaceStation/Models/Mission/Mission.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Mission.Contracts;
+using SpaceStation.Models.Planets.Contracts;
 
 namespace SpaceStation.Models.Mission
 {
-    internal class Mission
+    public class Mission : IMission
     {
+        public void Explore(IPlanet planet, ICollection<IAstronaut> astronauts)
+        {
+            foreach (var astronaut in astronauts.Where(x => x.Oxygen > 0))
+            {
+                while (true)
+                {
+                    var item = planet.Items.FirstOrDefault(); 
+                    // we don't know what happens if the planet runs out of items while there are still remaining astronauts
+
+                    astronaut.Breath();
+
+                    astronaut.Bag.Items.Add(item);
+                    planet.Items.Remove(item);
+
+                    if (astronaut.Oxygen < 0) // the specific order of operations hasn't been specified, might need to be moved up
+                    {
+                        break;
+                    }
+                }
+            }
+        }
     }
 }
