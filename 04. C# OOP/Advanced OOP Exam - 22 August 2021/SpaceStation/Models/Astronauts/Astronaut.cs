b0ec111a0bfb31d24ad91dff5d2c08de9ec6bc4f
Kristian <krisvl5000@gmail.com>
2023-03-14 09:25:42 +0200

    Create Astronaut.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2021/SpaceStation/Models/Astronauts/Astronaut.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Bags.Contracts;
+using SpaceStation.Utilities.Messages;
+
+namespace SpaceStation.Models.Astronauts
+{
+    public abstract class Astronaut : IAstronaut
+    {
+        public Astronaut(string name, double oxygen)
+        {
+            Name = name;
+            Oxygen = oxygen;
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(String.Format(ExceptionMessages.InvalidAstronautName));
+                }
+                name = value;
+            }
+        }
+
+        private double oxygen;
+
+        public double Oxygen
+        {
+            get { return oxygen; }
+            protected set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidOxygen));
+                }
+                oxygen = value;
+            }
+        }
+
+        public bool CanBreath { get; }
+
+        public IBag Bag { get; }
+
+        public virtual void Breath()
+        {
+            Oxygen -= 10;
+
+            if (Oxygen < 0)
+            {
+                Oxygen = 0;
+            }
+        }
+    }
+}
