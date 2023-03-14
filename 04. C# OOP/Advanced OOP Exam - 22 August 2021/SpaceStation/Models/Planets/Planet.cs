0e2de1b0772fdd6c605a6d57f2fc0c42ddd76519
Kristian <krisvl5000@gmail.com>
2023-03-14 09:55:06 +0200

    Update Planet.cs

--- 04. C# OOP/Advanced OOP Exam - 22 August 2021/SpaceStation/Models/Planets/Planet.cs
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2021/SpaceStation/Models/Planets/Planet.cs
@@ -1,10 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using SpaceStation.Models.Planets.Contracts;
+using SpaceStation.Utilities.Messages;
 
 namespace SpaceStation.Models.Planets
 {
-    internal class Planet
+    public class Planet : IPlanet
     {
+        public Planet(string name)
+        {
+            Name = name;
+            Items = new List<string>();
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException(String.Format(ExceptionMessages.InvalidPlanetName));
+                }
+                name = value;
+            }
+        }
+
+        public ICollection<string> Items { get; }
     }
 }
