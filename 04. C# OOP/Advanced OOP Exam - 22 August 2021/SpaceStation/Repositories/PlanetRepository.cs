0a6fd40356bb03606cb31e74a4c63f93a99a1106
Kristian <krisvl5000@gmail.com>
2023-03-14 09:55:10 +0200

    Create PlanetRepository.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2021/SpaceStation/Repositories/PlanetRepository.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SpaceStation.Models.Planets.Contracts;
+using SpaceStation.Repositories.Contracts;
+
+namespace SpaceStation.Repositories
+{
+    public class PlanetRepository : IRepository<IPlanet>
+    {
+        public PlanetRepository()
+        {
+            models = new List<IPlanet>();
+        }
+
+        private List<IPlanet> models;
+
+        public IReadOnlyCollection<IPlanet> Models => models.AsReadOnly();
+
+        public void Add(IPlanet model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IPlanet model)
+        {
+            var modelToRemove = models.FirstOrDefault(x => x.Name == model.Name);
+
+            if (modelToRemove == null)
+            {
+                return false;
+            }
+
+            models.Remove(modelToRemove);
+            return true;
+        }
+
+        public IPlanet FindByName(string name) => models.FirstOrDefault(x => x.Name == name);
+    }
+}
