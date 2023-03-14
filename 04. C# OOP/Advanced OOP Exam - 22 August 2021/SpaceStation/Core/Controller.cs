f277a7790ef44dab132d68ee74cee90e4f4e278a
Kristian <krisvl5000@gmail.com>
2023-03-14 10:52:24 +0200

    Update Controller.cs

--- 04. C# OOP/Advanced OOP Exam - 22 August 2021/SpaceStation/Core/Controller.cs
+++ 04. C# OOP/Advanced OOP Exam - 22 August 2021/SpaceStation/Core/Controller.cs
@@ -1,10 +1,82 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
+using SpaceStation.Core.Contracts;
+using SpaceStation.Models.Astronauts;
+using SpaceStation.Models.Astronauts.Contracts;
+using SpaceStation.Models.Planets;
+using SpaceStation.Models.Planets.Contracts;
+using SpaceStation.Repositories;
+using SpaceStation.Repositories.Contracts;
+using SpaceStation.Utilities.Messages;
 
 namespace SpaceStation.Core
 {
-    internal class Controller
+    public class Controller : IController
     {
+        private IRepository<IAstronaut> astronauts;
+        private IRepository<IPlanet> planets;
+        public Controller()
+        {
+            astronauts = new AstronautRepository();
+            planets = new PlanetRepository();
+        }
+
+        public string AddAstronaut(string type, string astronautName)
+        {
+            if (type != nameof(Biologist) && type != nameof(Geodesist) && type != nameof(Meteorologist))
+            {
+                throw new InvalidOperationException(String.Format(ExceptionMessages.InvalidAstronautType));
+            }
+
+            IAstronaut astronaut = null;
+
+            if (type == nameof(Biologist))
+            {
+                astronaut = new Biologist(astronautName);
+            }
+            else if (type == nameof(Geodesist))
+            {
+                astronaut = new Geodesist(astronautName);
+            }
+            else
+            {
+                astronaut = new Meteorologist(astronautName);
+            }
+
+            astronauts.Add(astronaut);
+
+            return String.Format(OutputMessages.AstronautAdded, type, astronautName);
+        }
+
+        public string AddPlanet(string planetName, params string[] items)
+        {
+            IPlanet planet = new Planet(planetName);
+
+            foreach (var item in items)
+            {
+                planet.Items.Add(item);
+            }
+
+            planets.Add(planet);
+
+            return String.Format(OutputMessages.PlanetAdded, planetName);
+        }
+
+        public string RetireAstronaut(string astronautName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ExplorePlanet(string planetName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Report()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
