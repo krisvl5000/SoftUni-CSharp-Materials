2200ac214ba9c233584deded325c179fa4e7acab
Kristian <krisvl5000@gmail.com>
2023-03-15 10:10:28 +0200

    Create ProfessionalRacer.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 15 August 2021/CarRacing/Models/Racers/ProfessionalRacer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarRacing.Models.Cars.Contracts;
+
+namespace CarRacing.Models.Racers
+{
+    public class ProfessionalRacer : Racer
+    {
+        private const int DRIVING_EXPERIENCE = 30;
+        private const string RACING_BEHAVIOR = "strict";
+        public ProfessionalRacer(string username, ICar car) : base(username, RACING_BEHAVIOR, DRIVING_EXPERIENCE, car)
+        {
+        }
+    }
+}
