d052d8464452b64b334d3787bf76528d87f743b0
Kristian <krisvl5000@gmail.com>
2023-03-15 09:37:00 +0200

    Create SuperCar.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 15 August 2021/CarRacing/Models/Cars/SuperCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Cars
+{
+    public class SuperCar : Car
+    {
+        private const double FUEL_AVAILABLE = 80;
+        private const double FUEL_CONSUMPTION_PER_RACE = 10;
+        public SuperCar(string make, string model, string vin, int horsePower) : base(make, model, vin, horsePower, FUEL_AVAILABLE, FUEL_CONSUMPTION_PER_RACE)
+        {
+        }
+    }
+}
