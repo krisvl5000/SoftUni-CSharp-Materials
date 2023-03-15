15de0d279f1c267e04f113f2dadd5c5fdec1f3e8
Kristian <krisvl5000@gmail.com>
2023-03-15 10:10:30 +0200

    Create StreetRacer.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 15 August 2021/CarRacing/Models/Racers/StreetRacer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using CarRacing.Models.Cars.Contracts;
+
+namespace CarRacing.Models.Racers
+{
+    internal class StreetRacer : Racer
+    {
+        private const int DRIVING_EXPERIENCE = 10;
+        private const string RACING_BEHAVIOR = "aggressive";
+        public StreetRacer(string username, ICar car) : base(username, RACING_BEHAVIOR, DRIVING_EXPERIENCE, car)
+        {
+        }
+    }
+}
