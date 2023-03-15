19ea38aaecccd8d07c40f343bf23bbe1d4a0db53
Kristian <krisvl5000@gmail.com>
2023-03-15 09:36:59 +0200

    Create TunedCar.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 15 August 2021/CarRacing/Models/Cars/TunedCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarRacing.Models.Cars
+{
+    public class TunedCar : Car
+    {
+        private const double FUEL_AVAILABLE = 65;
+        private const double FUEL_CONSUMPTION_PER_RACE = 7.5;
+        public TunedCar(string make, string model, string vin, int horsePower) : base(make, model, vin, horsePower, FUEL_AVAILABLE, FUEL_CONSUMPTION_PER_RACE)
+        {
+        }
+    }
+}
