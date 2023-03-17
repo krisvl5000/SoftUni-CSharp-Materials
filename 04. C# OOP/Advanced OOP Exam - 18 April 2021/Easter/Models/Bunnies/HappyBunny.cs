b173d81715ab31b75a72c7ade2efb807dc574c6e
Kristian <krisvl5000@gmail.com>
2023-03-17 10:31:03 +0200

    Create HappyBunny.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 18 April 2021/Easter/Models/Bunnies/HappyBunny.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easter.Models.Bunnies
+{
+    public class HappyBunny : Bunny
+    {
+        private const int INITIAL_ENERGY = 100;
+        public HappyBunny(string name) : base(name, INITIAL_ENERGY)
+        {
+        }
+    }
+}
