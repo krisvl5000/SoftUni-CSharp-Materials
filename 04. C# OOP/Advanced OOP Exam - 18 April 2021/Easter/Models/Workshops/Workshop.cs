70bec87fb60513cd35e06142f6990d23d32839c9
Kristian <krisvl5000@gmail.com>
2023-03-17 10:31:07 +0200

    Create Workshop.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 18 April 2021/Easter/Models/Workshops/Workshop.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Easter.Models.Bunnies.Contracts;
+using Easter.Models.Eggs.Contracts;
+using Easter.Models.Workshops.Contracts;
+
+namespace Easter.Models.Workshops
+{
+    public class Workshop : IWorkshop
+    {
+        public Workshop()
+        {
+
+        }
+
+        public void Color(IEgg egg, IBunny bunny)
+        {
+            while (!egg.IsDone() && bunny.Energy > 0 && bunny.Dyes.Any(x => x.IsFinished() == false))
+            {
+                var dye = bunny.Dyes.FirstOrDefault(x => !x.IsFinished());
+                dye.Use();
+                bunny.Work();
+                egg.GetColored();
+            }
+
+        }
+    }
+}
