165cca29511bf35042a274f5531ff33cfd679baa
Kristian <krisvl5000@gmail.com>
2023-03-17 10:31:01 +0200

    Create SleepyBunny.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 18 April 2021/Easter/Models/Bunnies/SleepyBunny.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Easter.Models.Bunnies
+{
+    public class SleepyBunny : Bunny
+    {
+        private const int INITIAL_ENERGY = 50;
+        public SleepyBunny(string name) : base(name, INITIAL_ENERGY)
+        {
+        }
+
+        public override void Work()
+        {
+            base.Work();
+            Energy -= 5;
+
+            if (Energy < 0)
+            {
+                Energy = 0;
+            }
+        }
+    }
+}
