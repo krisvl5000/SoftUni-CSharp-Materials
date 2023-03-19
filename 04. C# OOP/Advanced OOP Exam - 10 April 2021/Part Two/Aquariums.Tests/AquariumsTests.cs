fdab30e773c0f37ca1a790f71766cf02d048c45f
Kristian <krisvl5000@gmail.com>
2023-03-19 15:57:11 +0200

    Update AquariumsTests.cs

--- 04. C# OOP/Advanced OOP Exam - 10 April 2021/Part Two/Aquariums.Tests/AquariumsTests.cs
+++ 04. C# OOP/Advanced OOP Exam - 10 April 2021/Part Two/Aquariums.Tests/AquariumsTests.cs
@@ -1,8 +1,34 @@
-﻿namespace Aquariums.Tests
+﻿using NUnit.Framework;
+
+namespace Aquariums.Tests
 {
     using System;
 
     public class AquariumsTests
     {
+        private Fish fish;
+        private Aquarium aquarium;
+
+        [SetUp]
+        public void SetUp()
+        {
+            fish = new Fish("Fish");
+            aquarium = new Aquarium("Aquarium", 3);
+        }
+
+        [Test]
+        public void Test_IsTheConstructorWorking()
+        {
+            Assert.That( aquarium.Name == "Aquarium" &&aquarium.Capacity == 3 && aquarium.Count == 0);
+        }
+
+        [Test]
+        public void Test_IsTheConstructorThrowingForAnEmptyName()
+        {
+            Assert.Throws<ArgumentNullException>(() =>
+            {
+                aquarium = new Aquarium("", 5);
+            });
+        }
     }
 }
