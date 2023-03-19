88fcfede8e82296d7c11f36963ed16c9c2e9fb4b
Kristian <krisvl5000@gmail.com>
2023-03-19 16:15:30 +0200

    Update AquariumsTests.cs

--- 04. C# OOP/Advanced OOP Exam - 10 April 2021/Part Two/Aquariums.Tests/AquariumsTests.cs
+++ 04. C# OOP/Advanced OOP Exam - 10 April 2021/Part Two/Aquariums.Tests/AquariumsTests.cs
@@ -19,7 +19,7 @@ namespace Aquariums.Tests
         [Test]
         public void Test_IsTheConstructorWorking()
         {
-            Assert.That( aquarium.Name == "Aquarium" &&aquarium.Capacity == 3 && aquarium.Count == 0);
+            Assert.That(aquarium.Name == "Aquarium" && aquarium.Capacity == 3 && aquarium.Count == 0);
         }
 
         [Test]
@@ -29,6 +29,75 @@ namespace Aquariums.Tests
             {
                 aquarium = new Aquarium("", 5);
             });
+        }
+
+        [Test]
+        public void Test_IsTheConstructorThrowingForNegativeCapacity()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                aquarium = new Aquarium("Name", -1);
+            });
+        }
+
+        [Test]
+        public void Test_IsAddWorking()
+        {
+            aquarium.Add(fish);
+            Assert.That(aquarium.Count == 1);
+        }
+
+        [Test]
+        public void Test_IsAddThrowingWhenWeSurpassTheCapacity()
+        {
+            aquarium.Add(fish);
+            aquarium.Add(fish);
+            aquarium.Add(fish);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                aquarium.Add(fish);
+            });
+        }
+
+        [Test]
+        public void Test_IsRemoveFishWorking()
+        {
+            aquarium.Add(fish);
+            aquarium.RemoveFish("Fish");
+            Assert.That(aquarium.Count == 0);
+        }
+
+        [Test]
+        public void Test_IsRemoveFishThrowingIfWePassANameForANonExistentFish()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                aquarium.RemoveFish("Some Other Name");
+            });
+        }
+
+        [Test]
+        public void Test_IsSellFishWorking()
+        {
+            aquarium.Add(fish);
+            Assert.That(fish == aquarium.SellFish("Fish") && fish.Available == false);
+        }
+
+        [Test]
+        public void Test_IsSellFishThrowingIfWePassANameForANonExistentFish()
+        {
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                aquarium.SellFish("Some Other Name");
+            });
+        }
+
+        [Test]
+        public void Test_IsReportWorking()
+        {
+            aquarium.Add(fish);
+            Assert.That(aquarium.Report() == $"Fish available at Aquarium: Fish");
         }
     }
 }
