a239a42a5d520a07a775f1f3c8db3f5f76d23f48
Kristian <krisvl5000@gmail.com>
2023-03-26 19:51:04 +0300

    Update CommandInterpreter.cs

--- 04. C# OOP/Reflection and Attributes Exercise/01. Command Pattern 1/Core/CommandInterpreter.cs
+++ 04. C# OOP/Reflection and Attributes Exercise/01. Command Pattern 1/Core/CommandInterpreter.cs
@@ -1,12 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Reflection;
 using _01._Command_Pattern_1.Core.Contracts;
 
-namespace CommandPattern
+namespace _01._Command_Pattern_1.Core
 {
     public class CommandInterpreter : ICommandInterpreter
     {
