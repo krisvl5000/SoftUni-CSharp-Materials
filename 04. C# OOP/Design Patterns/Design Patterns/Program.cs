22880aa53b9ed98d5c7caed7335623c15656bd11
Kristian <krisvl5000@gmail.com>
2023-03-27 15:51:00 +0300

    Adding Design Patterns Lab

---
+++ 04. C# OOP/Design Patterns/Design Patterns/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Hello_Softuni
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+                //Design Patterns : 3 Types
+
+            // 1. Creational - Deal with initialization and configuration of classes and objects
+
+            // Examples - Singleton, Simple Factory, Factory Method, Abstract Factory, Builder, Prototype, Fluent Interface, Object Pool, Lazy Initialization
+
+            // 2. Structural - Describe ways to assemble objects to implement new functionality
+
+            // 3. Behavioral - Deal with dynamic interactions among societies of classes
+
+
+        }
+    }
+}
