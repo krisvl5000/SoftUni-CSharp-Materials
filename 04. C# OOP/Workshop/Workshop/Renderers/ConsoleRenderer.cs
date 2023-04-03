6e88653ea8d190223a7df666139483144aab8a2b
Kristian <krisvl5000@gmail.com>
2023-04-03 12:47:23 +0300

    Create ConsoleRenderer.cs

---
+++ 04. C# OOP/Workshop/Workshop/Renderers/ConsoleRenderer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop.Renderers
+{
+    public class ConsoleRenderer : IRenderer
+    {
+        public void Write(object obj)
+        {
+            Console.Write(obj);
+        }
+
+        public void WriteLine(object obj)
+        {
+            Console.WriteLine(obj);
+        }
+    }
+}
