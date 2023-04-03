0781d961cab35d061473ed2ee2879680c766a93e
Kristian <krisvl5000@gmail.com>
2023-04-03 12:27:16 +0300

    Create AdvancedShapeDrawer.cs

---
+++ 04. C# OOP/Workshop/Workshop/Drawers/AdvancedShapeDrawer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Workshop.Drawers.Contracts;
+using Workshop.Shapes;
+
+namespace Workshop.Drawers
+{
+    public class AdvancedShapeDrawer : IShapeDrawer
+    {
+        public void DrawCircle(Circle circle)
+        {
+            Console.WriteLine("  @@");
+            Console.WriteLine("@    @");
+            Console.WriteLine("@    @");
+            Console.WriteLine("  @@");
+        }
+
+        public void DrawRectangle(Rectangle rectangle)
+        {
+            Console.WriteLine("@@@@");
+            Console.WriteLine("@  @");
+            Console.WriteLine("@  @");
+            Console.WriteLine("@@@@");
+        }
+    }
+}
