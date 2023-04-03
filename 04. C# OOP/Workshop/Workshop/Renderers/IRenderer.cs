2b080856e357273d002536900b875d1c720733aa
Kristian <krisvl5000@gmail.com>
2023-04-03 12:47:28 +0300

    Create IRenderer.cs

---
+++ 04. C# OOP/Workshop/Workshop/Renderers/IRenderer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Workshop.Renderers
+{
+    public interface IRenderer
+    {
+        void Write(object obj);
+
+        void WriteLine(object obj);
+    }
+}
