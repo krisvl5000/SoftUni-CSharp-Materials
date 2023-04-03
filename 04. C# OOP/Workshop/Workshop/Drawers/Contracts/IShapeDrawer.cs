044132bf5ac0e246a264c6158846f1e568292784
Kristian <krisvl5000@gmail.com>
2023-04-03 12:20:48 +0300

    Update IShapeDrawer.cs

--- 04. C# OOP/Workshop/Workshop/Drawers/Contracts/IShapeDrawer.cs
+++ 04. C# OOP/Workshop/Workshop/Drawers/Contracts/IShapeDrawer.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Workshop.Shapes;
 
 namespace Workshop.Drawers.Contracts
 {
-    internal interface IShapeDrawer
+    public interface IShapeDrawer
     {
+        public void DrawCircle(Circle circle);
+
+        public void DrawRectangle(Rectangle rectangle);
     }
 }
