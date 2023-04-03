6ee1b777ee4177f818aa31ed34141b08e8752501
Kristian <krisvl5000@gmail.com>
2023-04-03 12:40:33 +0300

    Create DependencyInjectionService.cs

---
+++ 04. C# OOP/Workshop/Workshop/DI/DependencyInjectionService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Workshop.Drawers;
+using Workshop.Drawers.Contracts;
+
+namespace Workshop.DI
+{
+    internal class DependencyInjectionService
+    {
+        public static IServiceProvider ConfigureServices()
+        {
+            IServiceCollection serviceCollection = new ServiceCollection();
+
+            // custom code
+            serviceCollection.AddTransient<IShapeDrawer, BasicShapeDrawer>();
+            serviceCollection.AddTransient<Engine, Engine>();
+
+            return serviceCollection.BuildServiceProvider();
+        }
+    }
+}
