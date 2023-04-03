a758364fd1643a77bca8b8fdcfd7ed11f3d7cbd5
Kristian <krisvl5000@gmail.com>
2023-04-03 12:20:47 +0300

    Update Engine.cs

--- 04. C# OOP/Workshop/Workshop/Engine.cs
+++ 04. C# OOP/Workshop/Workshop/Engine.cs
@@ -3,10 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Workshop.Drawers.Contracts;
+using Workshop.Shapes;
 
 namespace Workshop
 {
-    internal class Engine
+    public class Engine
     {
+        private IShapeDrawer drawer;
+        private List<Shape> shapes; 
+        public Engine(IShapeDrawer drawer)
+        {
+            this.drawer = drawer;
+
+            shapes = new List<Shape>
+            {
+                new Circle(),
+                new Rectangle()
+            };
+        }
+
+        public void Run()
+        {
+            foreach (var shape in shapes)
+            {
+                if (shape is Circle circle)
+                {
+                    drawer.DrawCircle(circle);
+                }
+                if (shape is Rectangle rectangle)
+                {
+                    drawer.DrawRectangle(rectangle);
+                }
+            }
+        }
     }
 }
