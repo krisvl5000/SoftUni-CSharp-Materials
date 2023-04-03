4742bb73e4f7a38dd7eff1a15dac01359110315f
Kristian <krisvl5000@gmail.com>
2023-04-03 12:47:25 +0300

    Update BasicShapeDrawer.cs

--- 04. C# OOP/Workshop/Workshop/Drawers/BasicShapeDrawer.cs
+++ 04. C# OOP/Workshop/Workshop/Drawers/BasicShapeDrawer.cs
@@ -4,27 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Workshop.Drawers.Contracts;
+using Workshop.Renderers;
 using Workshop.Shapes;
 
 namespace Workshop.Drawers
 {
     public class BasicShapeDrawer : IShapeDrawer
-
     {
+        private readonly IRenderer renderer;
+        public BasicShapeDrawer(IRenderer renderer)
+        {
+            this.renderer = renderer;
+        }
+
         public void DrawCircle(Circle circle)
         {
-            Console.WriteLine("  **");
-            Console.WriteLine("*    *");
-            Console.WriteLine("*    *");
-            Console.WriteLine("  **");
+            renderer.WriteLine("  **");
+            renderer.WriteLine("*    *");
+            renderer.WriteLine("*    *");
+            renderer.WriteLine("  **");
         }
 
         public void DrawRectangle(Rectangle rectangle)
         {
-            Console.WriteLine("****");
-            Console.WriteLine("*  *");
-            Console.WriteLine("*  *");
-            Console.WriteLine("****");
+            renderer.WriteLine("****");
+            renderer.WriteLine("*  *");
+            renderer.WriteLine("*  *");
+            renderer.WriteLine("****");
         }
     }
 }
