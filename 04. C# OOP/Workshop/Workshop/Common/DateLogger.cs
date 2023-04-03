85d6d00e56069964a884480d4c6cc9d493cd2360
Kristian <krisvl5000@gmail.com>
2023-04-03 12:58:34 +0300

    Update DateLogger.cs

--- 04. C# OOP/Workshop/Workshop/Common/DateLogger.cs
+++ 04. C# OOP/Workshop/Workshop/Common/DateLogger.cs
@@ -6,7 +6,18 @@ using System.Threading.Tasks;
 
 namespace Workshop.Common
 {
-    internal class DateLogger
+    public class DateLogger : ILogger
     {
+        private readonly DateTime date;
+
+        public DateLogger(int day, int month, int year)
+        {
+            date = new DateTime(day, month, year);
+        }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"DATE: {date:dd/mm/yyyy}");
+        }
     }
 }
