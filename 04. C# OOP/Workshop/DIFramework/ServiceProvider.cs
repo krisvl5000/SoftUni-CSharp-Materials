9779e4b36763368fc1407182ed9aacd11c391a4e
Kristian <krisvl5000@gmail.com>
2023-04-04 13:00:00 +0300

    Create ServiceProvider.cs

---
+++ 04. C# OOP/Workshop/DIFramework/ServiceProvider.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DIFramework.Contracts;
+using IServiceProvider = System.IServiceProvider;
+
+namespace DIFramework
+{
+    public class ServiceProvider : IServiceProvider
+    {
+        private IServiceCollection serviceCollection;
+        public ServiceProvider(IServiceCollection serviceCollection)
+        {
+            this.serviceCollection = serviceCollection;
+        }
+
+        public object? GetService(Type serviceType)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
