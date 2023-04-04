3fd439df25506d528840aa16dc21bfe36e62631f
Kristian <krisvl5000@gmail.com>
2023-04-04 12:55:06 +0300

    Update ServiceCollection.cs

--- 04. C# OOP/Workshop/DIFramework/ServiceCollection.cs
+++ 04. C# OOP/Workshop/DIFramework/ServiceCollection.cs
@@ -4,9 +4,25 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DIFramework
+namespace DIFramework.Contracts
 {
-    internal class ServiceCollection
+    public class ServiceCollection : IServiceCollection
     {
+        private Dictionary<Type, Type> mappings;
+
+        public ServiceCollection()
+        {
+            mappings = new Dictionary<Type, Type>();
+        }
+
+        public void AddTransient<TInterface, TImplementation>()
+        {
+            mappings.Add(typeof(TInterface), typeof(TImplementation));
+        }
+
+        public void AddTransient<TInterface, TImplementation>(Func<IServiceProvider, TImplementation> implementationFactory)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
