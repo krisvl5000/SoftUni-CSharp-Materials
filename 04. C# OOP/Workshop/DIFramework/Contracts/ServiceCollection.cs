1716ed3a8445541ae54728fc669a589a94482700
Kristian <krisvl5000@gmail.com>
2023-04-04 12:53:56 +0300

    Create ServiceCollection.cs

---
+++ 04. C# OOP/Workshop/DIFramework/Contracts/ServiceCollection.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DIFramework.Contracts
+{
+    internal class ServiceCollection
+    {
+    }
+}
