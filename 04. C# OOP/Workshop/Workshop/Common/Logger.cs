b23446c230ecf95d8fdcb6da3a66256ad384fc79
Kristian <krisvl5000@gmail.com>
2023-04-04 12:38:30 +0300

    Update Logger.cs

--- 04. C# OOP/Workshop/Workshop/Common/Logger.cs
+++ 04. C# OOP/Workshop/Workshop/Common/Logger.cs
@@ -8,6 +8,13 @@ namespace Workshop.Common
 {
     public class Logger : ILogger
     {
+        public Logger()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("A new Logger instance was created!");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
         public void Log(string message)
         {
             Console.WriteLine($"Logging {message}");
