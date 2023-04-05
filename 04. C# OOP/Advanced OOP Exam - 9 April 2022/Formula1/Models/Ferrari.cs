a938ca35b942bdc10e9617208c3c6a44025b1677
Kristian <krisvl5000@gmail.com>
2023-04-05 12:00:07 +0300

    Update Ferrari.cs

--- 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Models/Ferrari.cs
+++ 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Models/Ferrari.cs
@@ -6,7 +6,10 @@ using System.Threading.Tasks;
 
 namespace Formula1.Models
 {
-    internal class Ferrari
+    public class Ferrari : FormulaOneCar
     {
+        public Ferrari(string model, int horsepower, double engineDisplacement) : base(model, horsepower, engineDisplacement)
+        {
+        }
     }
 }
