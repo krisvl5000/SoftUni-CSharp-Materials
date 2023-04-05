da250accc015d0487e6ded9b32254b25b43f15c0
Kristian <krisvl5000@gmail.com>
2023-04-05 18:29:54 +0300

    Create Engine.cs

---
+++ 04. C# OOP/Workshop - Snake/SimpleSnake/Core/Engine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleSnake.Core.Interfaces;
+
+namespace SimpleSnake.Core
+{
+    public class Engine : IEngine
+    {
+        public Engine()
+        {
+            
+        }
+
+        public void Run()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
