f73a674ea7d8dd4d9219c1b4570c47214cf32dde
Kristian <krisvl5000@gmail.com>
2023-04-05 20:38:25 +0300

    Create Wall.cs

---
+++ 04. C# OOP/Workshop - Snake/SimpleSnake/GameObjects/Wall.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SimpleSnake.GameObjects
+{
+    public class Wall : Point
+    {
+        private const char wallSymbol = '\u25A0';
+
+        public Wall(int leftX, int topY) : base(leftX, topY)
+        {
+        }
+        
+        private void SetHorizontalLine(int topY)
+        {
+            for (int leftX = 0; leftX < LeftX; leftX++)
+            {
+                Draw(leftX, topY, wallSymbol);
+            }
+        }
+    }
+}
