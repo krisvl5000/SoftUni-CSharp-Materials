44495809d04bc8261ab83927d7b7859ed5d2fde3
Kristian <krisvl5000@gmail.com>
2023-04-05 12:00:14 +0300

    Update FormulaOneCar.cs

--- 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Models/FormulaOneCar.cs
+++ 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Models/FormulaOneCar.cs
@@ -3,10 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Formula1.Models.Contracts;
+using Formula1.Utilities;
 
 namespace Formula1.Models
 {
-    internal class FormulaOneCar
+    public abstract class FormulaOneCar : IFormulaOneCar
     {
+        public FormulaOneCar(string model, int horsepower, double engineDisplacement)
+        {
+            Model = model;
+            Horsepower = horsepower;
+            EngineDisplacement = engineDisplacement;
+        }
+
+        private string model;
+
+        public string Model
+        {
+            get { return model; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 3)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidF1CarModel, value));
+                }
+                model = value;
+            }
+        }
+
+        private int horsepower;
+
+        public int Horsepower
+        {
+            get { return horsepower; }
+            private set
+            {
+                if (value < 900 || value > 1050)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidF1HorsePower, horsepower));
+                }
+                horsepower = value;
+            }
+        }
+
+        private double engineDisplacement;
+
+        public double EngineDisplacement
+        {
+            get { return engineDisplacement; }
+            private set
+            {
+                if (value < 1.6 || value > 2.00)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidF1EngineDisplacement, value));
+                }
+                engineDisplacement = value;
+            }
+        }
+
+        public double RaceScoreCalculator(int laps) => engineDisplacement / horsepower * laps;
     }
 }
