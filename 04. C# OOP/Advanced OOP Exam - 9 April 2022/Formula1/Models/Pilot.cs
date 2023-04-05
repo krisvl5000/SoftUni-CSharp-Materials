e828a000cd1a8e2c4a91f024b7995da95b541de4
Kristian <krisvl5000@gmail.com>
2023-04-05 12:00:12 +0300

    Update Pilot.cs

--- 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Models/Pilot.cs
+++ 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Models/Pilot.cs
@@ -3,10 +3,66 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Formula1.Models.Contracts;
+using Formula1.Utilities;
 
 namespace Formula1.Models
 {
-    internal class Pilot
+    public class Pilot : IPilot
     {
+        public Pilot(string fullName)
+        {
+            FullName = fullName;
+
+            CanRace = false;
+        }
+
+        private string fullName;
+
+        public string FullName
+        {
+            get { return fullName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidPilot, value));
+                }
+                fullName = value;
+            }
+        }
+
+        private IFormulaOneCar car;
+
+        public IFormulaOneCar Car
+        {
+            get { return car; }
+            private set
+            {
+                if (value == null)
+                {
+                    throw new NullReferenceException(String.Format(ExceptionMessages.InvalidCarForPilot));
+                }
+                car = value;
+            }
+        }
+
+
+        public int NumberOfWins { get; private set; }
+
+        public bool CanRace { get; private set; }
+
+        public void AddCar(IFormulaOneCar car)
+        {
+            Car = car;
+            CanRace = true;
+        }
+
+        public void WinRace()
+        {
+            NumberOfWins++;
+        }
+
+        public override string ToString() => $"Pilot {FullName} has {NumberOfWins} wins.";
     }
 }
