253a672969f87a1ef61d83bfc5d1c7c929b95e4b
Kristian <krisvl5000@gmail.com>
2023-04-05 12:09:22 +0300

    Create RaceRepository.cs

---
+++ 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Repositories/RaceRepository.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Formula1.Models.Contracts;
+using Formula1.Repositories.Contracts;
+
+namespace Formula1.Repositories
+{
+    public class RaceRepository : IRepository<IRace>
+    {
+        private List<IRace> models;
+
+        public RaceRepository()
+        {
+            models = new List<IRace>();
+        }
+
+        public IReadOnlyCollection<IRace> Models => models.AsReadOnly();
+
+        public void Add(IRace model)
+        {
+            models.Add(model);
+        }
+
+        public bool Remove(IRace model)
+        {
+            var raceToRemove = models.FirstOrDefault(x => x.RaceName == model.RaceName);
+
+            if (raceToRemove == null)
+            {
+                return false;
+            }
+
+            models.Remove(model);
+            return true;
+        }
+
+        public IRace FindByName(string name) => models.FirstOrDefault(x => x.RaceName == name);
+    }
+}
