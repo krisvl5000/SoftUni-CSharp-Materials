42a46f38a1cecc29dae89813c5aa23a120761193
Kristian <krisvl5000@gmail.com>
2023-04-05 12:00:11 +0300

    Update Race.cs

--- 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Models/Race.cs
+++ 04. C# OOP/Advanced OOP Exam - 9 April 2022/Formula1/Models/Race.cs
@@ -3,10 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Formula1.Models.Contracts;
+using Formula1.Utilities;
 
 namespace Formula1.Models
 {
-    internal class Race
+    public class Race : IRace
     {
+        public Race(string raceName, int numberOfLaps)
+        {
+            RaceName = raceName;
+            NumberOfLaps = numberOfLaps;
+
+            TookPlace = false;
+            Pilots = new List<IPilot>();
+        }
+
+        private string raceName;
+
+        public string RaceName
+        {
+            get { return raceName; }
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value) || value.Length < 5)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidRaceName, value));
+                }
+                raceName = value;
+            }
+        }
+
+        private int numberOfLaps;
+
+        public int NumberOfLaps
+        {
+            get { return numberOfLaps; }
+            private set
+            {
+                if (value < 1)
+                {
+                    throw new ArgumentException(String.Format(ExceptionMessages.InvalidLapNumbers, value));
+                }
+                numberOfLaps = value;
+            }
+        }
+
+        public bool TookPlace { get; set; }
+
+        public ICollection<IPilot> Pilots { get; }
+
+        public void AddPilot(IPilot pilot)
+        {
+            Pilots.Add(pilot);
+        }
+
+        public string RaceInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"The {RaceName} race has:");
+            sb.AppendLine($"Participants: {Pilots.Count}");
+            sb.AppendLine($"Number of laps: {numberOfLaps}");
+            sb.AppendLine(TookPlace ? "Took place: Yes" : "Took place: No");
+
+            return sb.ToString().Trim();
+        }
     }
 }
