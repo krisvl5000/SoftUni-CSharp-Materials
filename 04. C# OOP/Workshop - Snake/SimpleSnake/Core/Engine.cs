e0a1899e4a5d62e7737b3b5fb32468877722023e
Kristian <krisvl5000@gmail.com>
2023-04-06 11:06:31 +0300

    Update Engine.cs

--- 04. C# OOP/Workshop - Snake/SimpleSnake/Core/Engine.cs
+++ 04. C# OOP/Workshop - Snake/SimpleSnake/Core/Engine.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using SimpleSnake.Core.Interfaces;
 using SimpleSnake.GameObjects;
+using SimpleSnake.GameObjects.Foods;
 
 namespace SimpleSnake.Core
 {
@@ -18,6 +19,10 @@ namespace SimpleSnake.Core
         public void Run()
         {
             Wall wall = new Wall(60, 20);
+
+            Food food = new FoodHash(wall);
+
+            food.SetRandomPosition(new Queue<Point>());
 
             while (true)
             {
