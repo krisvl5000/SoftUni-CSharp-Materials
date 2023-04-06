68b482ccd727b71ca34796f76d9cffdc06f45845
Kristian <krisvl5000@gmail.com>
2023-04-06 10:57:11 +0300

    Update Food.cs

--- 04. C# OOP/Workshop - Snake/SimpleSnake/GameObjects/Foods/Food.cs
+++ 04. C# OOP/Workshop - Snake/SimpleSnake/GameObjects/Foods/Food.cs
@@ -8,10 +8,37 @@ namespace SimpleSnake.GameObjects.Foods
 {
     public abstract class Food : Point
     {
-        protected Food(int leftX, int topY) : base(leftX, topY)
+        private char foodSymbol;
+        private Random random;
+        private Wall wall;
+
+        protected Food(Wall wall, int leftX, int topY) : base(leftX, topY)
         {
+            random = new Random();
         }
 
         public int FoodPoints { get; private set; }
+
+        public void SetRandomPosition(Queue<Point> snakeElements)
+        {
+            LeftX = random.Next(2, wall.LeftX - 2);
+            TopY = random.Next(2, wall.TopY - 2);
+
+            bool isPointOfSnake = snakeElements
+                .Any(x => x.LeftX == LeftX && x.TopY == TopY);
+
+            while (isPointOfSnake)
+            {
+                LeftX = random.Next(2, wall.LeftX - 2);
+                TopY = random.Next(2, wall.TopY - 2);
+
+                isPointOfSnake = snakeElements
+                    .Any(x => x.LeftX == LeftX && x.TopY == TopY);
+            }
+
+            Console.BackgroundColor = ConsoleColor.Red;
+            Draw(foodSymbol);
+            Console.ForegroundColor = ConsoleColor.White;
+        }
     }
 }
