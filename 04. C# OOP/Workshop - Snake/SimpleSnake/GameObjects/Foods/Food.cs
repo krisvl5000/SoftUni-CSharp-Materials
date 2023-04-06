fe69208c622a74f00ce0bfb3e7c42066182aff89
Kristian <krisvl5000@gmail.com>
2023-04-06 10:14:29 +0300

    Create Food.cs

---
+++ 04. C# OOP/Workshop - Snake/SimpleSnake/GameObjects/Foods/Food.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSnake.GameObjects.Foods
+{
+    public abstract class Food : Point
+    {
+        protected Food(int leftX, int topY) : base(leftX, topY)
+        {
+        }
+
+        public int FoodPoints { get; private set; }
+    }
+}
