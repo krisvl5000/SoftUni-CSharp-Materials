ef97059119f9d33e254435c21c005aeaec1f6382
Kristian <krisvl5000@gmail.com>
2023-09-30 12:46:50 +0300

    Update Databases_Introduction.sql

---
+++ 05. MS SQL/01. Databases Introduction/Databases_Introduction.sql
@@ -0,0 +1,47 @@
+-- 01. Create Database
+CREATE DATABASE [Minions]
+
+-- 02. Create Tables
+
+CREATE TABLE [Minions] (
+    [Id] INT PRIMARY KEY,
+    [Name] NVARCHAR(50) NOT NULL,
+    [Age] INT NOT NULL
+);
+
+CREATE TABLE [Towns](
+    [Id] INT PRIMARY KEY,
+    [Name] NVARCHAR(70) NOT NULL
+);
+
+-- 03. Alter Minions Table
+
+ALTER TABLE [Minions]
+ADD [TownId] INT FOREIGN KEY REFERENCES [Towns]([Id]) NOT NULL
+
+ALTER TABLE [Minions]
+ALTER COLUMN [Age] INT
+
+GO
+
+-- 04. Insert Records in Both Tables
+
+INSERT INTO [Towns] ([Id], [Name])
+VALUES
+	(1, 'Sofia'),
+	(2, 'Plovdiv'),
+	(3, 'Varna')
+
+INSERT INTO [Minions] ([Id], [Name], [Age], [TownId])
+VALUES
+(1, 'Kevin', 22, 1),
+(2, 'Bob', 15, 3),
+(3, 'Steward', NULL, 2)
+
+-- 05. Truncate Table Minions
+
+TRUNCATE TABLE [Minions]
+
+-- 06. Drop All Tables
+
+DROP TABLE 
