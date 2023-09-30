baa9aecd2a68c22133936eaae7d455470b25033a
Kristian <krisvl5000@gmail.com>
2023-09-30 12:50:58 +0300

    Update Databases_Introduction.sql
    Added 07

--- 05. MS SQL/01. Databases Introduction/Databases_Introduction.sql
+++ 05. MS SQL/01. Databases Introduction/Databases_Introduction.sql
@@ -49,3 +49,26 @@ DROP TABLE [Minions]
 GO
 
 DROP TABLE [Towns]
+
+-- 07. Create Table People
+
+CREATE TABLE [People](
+    [Id] INT PRIMARY KEY IDENTITY,
+    [Name] NVARCHAR(200) NOT NULL,
+    [Picture] VARBINARY(MAX),
+    CHECK (DATALENGTH([Picture]) <= 2000000),
+    [Height] DECIMAL(3, 2),
+    [Weight] DECIMAL(5,2),
+    [Gender] CHAR(1) NOT NULL,
+    CHECK ([Gender] = 'm' OR [Gender] = 'f'),
+    [Birthdate] DATE NOT NULL,
+    [Biography] NVARCHAR(MAX)
+    )
+
+	INSERT INTO [People]([Name], [Height], [Weight], [Gender], [Birthdate])
+    VALUES
+    ('Pesho', 1.77, 75.2, 'm', '1998-05-25'),
+	('Gosho', NULL, NULL, 'm', '1977-11-05'),
+	('Maria', 1.65, 42.2, 'f', '1998-06-27'),
+	('Viki', NULL, NULL, 'f', '1986-02-02'),
+	('Vancho', 1.69, 77.8, 'm', '1999-03-03')
