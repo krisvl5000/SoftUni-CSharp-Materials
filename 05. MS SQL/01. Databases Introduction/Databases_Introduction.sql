064eb9de6c1b9e0654795f0c16e822f8118ea3ac
Kristian <krisvl5000@gmail.com>
2023-10-02 12:14:42 +0300

    Update Databases_Introduction.sql

--- 05. MS SQL/01. Databases Introduction/Databases_Introduction.sql
+++ 05. MS SQL/01. Databases Introduction/Databases_Introduction.sql
@@ -122,3 +122,166 @@ DROP CONSTRAINT PK_Combination
 ALTER TABLE [Users]
 ADD CONSTRAINT PK_Id
 PRIMARY KEY([Id])
+
+ALTER TABLE [Users]
+ADD CHECK(LEN(Username) >= 3)
+
+-- 13. Movies Database
+
+CREATE DATABASE [Movies]
+
+CREATE TABLE [Directors](
+	[Id] INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	[DirectorName] VARCHAR(50) NOT NULL,
+	[Notes] VARCHAR (150),
+)
+
+INSERT INTO [Directors] ([DirectorName], [Notes])
+VALUES
+	('Stephen Spielberg', 'Something'),
+	('Ivan Stoyanov', 'Something else'),
+	('Gosho Ivanov', NULL),
+	('Petur Zahariev', 'Some notes'),
+	('Evtim Petrov', NULL)
+
+
+CREATE TABLE Genres(
+	[Id] INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	[GenreName] VARCHAR(50) NOT NULL,
+	[Notes] VARCHAR(150),
+)
+
+INSERT INTO [Genres] ([GenreName], [Notes])
+VALUES
+	('Horror', 'Very scary'),
+	('Comedy', 'Very funny'),
+	('Thriller', 'Very tense'),
+	('Action', 'Very fast-paced'),
+	('Documentary', 'Very interesting')
+
+CREATE TABLE Categories(
+	[Id] INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	[CategoryName] VARCHAR(50) NOT NULL,
+	[Notes] VARCHAR(150),
+)
+
+INSERT INTO [Categories] ([CategoryName], [Notes])
+VALUES
+	('I dont know what is meant by category', 'Something'),
+	('They didnt say what it is supposed to be', 'Something else'),
+	('But whatever', NULL),
+	('I will just write something', 'Some notes'),
+	('So it isnt empty', NULL)
+
+CREATE TABLE Movies(
+	[Id] INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	[Title] VARCHAR(50) NOT NULL,
+	[DirectorId] INT FOREIGN KEY
+	REFERENCES Directors(Id),
+	[CopyrightYear] INT,
+	[Length] INT
+)
+
+INSERT INTO [Movies] ([Title], [DirectorId], [CopyrightYear], [Length])
+VALUES
+	('American Pie', 2, 1999, 110),
+	('Scary Movie', 3, 2001, 120),
+	('Die Hard', 1, 1998, 130),
+	('Die Hard 3', 4, 2003, 100),
+	('The Matrix', 5, 1999, 120)
+
+-- 14. Car Rental Database
+
+CREATE DATABASE CarRental
+
+CREATE TABLE Categories(
+	[Id] INT PRIMARY KEY IDENTITY(1, 1) NOT NULL,
+	[CategoryName] NVARCHAR(50) NOT NULL,
+	[DailyRate] DECIMAL(10, 2) NOT NULL,
+	[WeeklyRate] DECIMAL(10, 2) NOT NULL,
+	[MonthlyRate] DECIMAL(10, 2) NOT NULL,
+	[WeekendRate] DECIMAL(10, 2) NOT NULL
+)
+
+INSERT INTO [Categories] ([CategoryName], [DailyRate], [WeeklyRate], [MonthlyRate], [WeekendRate])
+VALUES
+    ('A', 1.00, 2.00, 3.00, 4.00),
+    ('B', 1.00, 2.00, 3.00, 4.00),
+    ('C', 1.00, 2.00, 3.00, 4.00)
+
+CREATE TABLE Cars(
+	[Id] INT IDENTITY(1, 1) PRIMARY KEY NOT NULL,
+	[PlateNumber] NVARCHAR(50) NOT NULL,
+	[Manufacturer] NVARCHAR(50) NOT NULL,
+	[Model] NVARCHAR(50) NOT NULL,
+	[CarYear] DATE NOT NULL,
+	[CategoryId] INT FOREIGN KEY REFERENCES [Categories](Id) NOT NULL,
+	[Doors] INT,
+	[Picture] IMAGE,
+	[Condition] NVARCHAR(50) NOT NULL,
+	[Available] VARCHAR(50) NOT NULL
+		CHECK([Available] = 'true' OR [Available] = 'false')
+)
+
+INSERT INTO [Cars]
+    ([PlateNumber], [Manufacturer], [Model], [CarYear], [CategoryId], [Condition], [Available])
+VALUES
+    ('123AAA', 'BMW', 'X6', '1993-04-01', 1, 'Used', 'true'),
+    ('456BBB', 'Audi', 'RS7', '2000-05-06', 2, 'New', 'false'),
+    ('789CCC', 'Mercedes', 'S350', '2012-01-04', 3, 'Broken', 'false')
+
+CREATE TABLE Employees(
+	[Id] INT IDENTITY(1, 1) PRIMARY KEY,
+	[FirstName] NVARCHAR(50) NOT NULL,
+	[LastName] NVARCHAR(50) NOT NULL,
+	[Title] NVARCHAR(50) NOT NULL,
+	[Notes] NVARCHAR(MAX)
+)
+
+INSERT INTO [Employees] ([FirstName], [LastName], [Title])
+VALUES
+    ('Nekoi', 'Si', 'CEO'),
+    ('Nekoi', 'Drug', 'CTO'),
+    ('Nekoi', 'Treti', 'MafiaBoss')
+
+CREATE TABLE [Customers]
+(
+    [Id] INT IDENTITY(1, 1) PRIMARY KEY,
+    [DriverLicenseNumber] INT NOT NULL,
+    [FullName] NVARCHAR(100) NOT NULL,
+    [Address] NVARCHAR(250) NOT NULL,
+    [City] NVARCHAR(100) NOT NULL,
+    [ZIPCode] INT NOT NULL,
+    [Notes] NVARCHAR(MAX)
+)
+
+INSERT INTO [Customers] ([DriverLicenseNumber], [FullName], [Address], [City], [ZIPCode])
+VALUES
+    (123456789, 'Stefcho Popov', 'Ivan Vazov 21', 'Sofia', 1000),
+    (987654321, 'Kris Velkov', 'Pangaris Boy 45', 'Budeshte', 1495),
+    (767676767, 'Stefan Zhivkov', 'Preslav 5', 'Bojurishte', 4321)
+
+CREATE TABLE [RentalOrders]
+(
+    [Id] INT IDENTITY(1, 1) PRIMARY KEY,
+    [EmployeeId] INT FOREIGN KEY REFERENCES [Employees]([Id]) NOT NULL,
+    [CustomerId] INT FOREIGN KEY REFERENCES [Customers]([Id]) NOT NULL,
+    [CarId] INT FOREIGN KEY REFERENCES [Cars]([Id]) NOT NULL,
+    [TankLevel] NVARCHAR(50) NOT NULL,
+    [KilometrageStart] INT NOT NULL,
+    [KilometrageEnd] INT NOT NULL,
+    [TotalKilometrage] INT NOT NULL,
+    [StartDate] DATE NOT NULL,
+    [EndDate] DATE NOT NULL,
+    [TotalDays] INT NOT NULL,
+    [RateApplied] DECIMAL(18, 2) NOT NULL,
+    [TaxRate] DECIMAL(18, 2) NOT NULL,
+    [OrderStatus] NVARCHAR(50) NOT NULL,
+    [Notes] NVARCHAR(MAX)
+)
+
+INSERT INTO [RentalOrders] ([EmployeeId], [CustomerId], [CarId], [TankLevel], [KilometrageStart], [KilometrageEnd], [TotalKilometrage], [StartDate], [EndDate], [TotalDays], [RateApplied], [TaxRate], [OrderStatus])
+VALUES
+    (1, 3, 2, 100, 190000, 190500, 500, '1994-01-01', '1994-01-21', 20, 50.0, 10.0, 'Pending'),
+    (2, 1, 1, 90, 190000, 190500, 500, '1994-01-01', '1994-01-21', 20, 50.0, 10.0, 'Pending'),
+    (3, 2, 3, 50, 20000, 25000, 5000, '2020-10-01', '2022-12-01', 100, 50.0, 10.0, 'Approved')
