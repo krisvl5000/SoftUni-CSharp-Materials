cc24ecfe4805a3cb34206c1467268dbe54a501ae
Kristian <krisvl5000@gmail.com>
2023-10-04 11:46:49 +0300

    Update SQLQuery1.sql

--- 05. MS SQL/02. Crud/SQLQuery1.sql
+++ 05. MS SQL/02. Crud/SQLQuery1.sql
@@ -1,2 +1,11 @@
 -- 02. Find All Infromation About Departments
 
+SELECT * FROM [Departments]
+
+-- 03. Find all Department Names
+
+SELECT [Name]
+  FROM [Departments]
+
+-- 04. Find Salary of Each Employee
+
